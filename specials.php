95907356d334ef36571c59eda153d8783e2ce4e3
gburton <oscshops@gmail.com>
2014-02-17 13:39:57 +0000

    Use Product Listing Module
    In specials.php and products_new.php

--- specials.php
+++ specials.php
@@ -19,73 +19,93 @@
   require(DIR_WS_INCLUDES . 'template_top.php');
 ?>
 
-<div class="page-header">
-  <h1><?php echo HEADING_TITLE; ?></h1>
-</div>
-
-<div class="contentContainer">
-  <div class="contentText">
+<h1><?php echo HEADING_TITLE; ?></h1>
 
 <?php
-  $specials_query_raw = "select p.products_id, pd.products_name, p.products_price, p.products_tax_class_id, p.products_image, s.specials_new_products_price from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_SPECIALS . " s where p.products_status = '1' and s.products_id = p.products_id and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' and s.status = '1' order by s.specials_date_added DESC";
-  $specials_split = new splitPageResults($specials_query_raw, MAX_DISPLAY_SPECIAL_PRODUCTS);
+  // create column list
+  $define_list = array('PRODUCT_LIST_MODEL' => PRODUCT_LIST_MODEL,
+                       'PRODUCT_LIST_NAME' => PRODUCT_LIST_NAME,
+                       'PRODUCT_LIST_MANUFACTURER' => PRODUCT_LIST_MANUFACTURER,
+                       'PRODUCT_LIST_PRICE' => PRODUCT_LIST_PRICE,
+                       'PRODUCT_LIST_QUANTITY' => PRODUCT_LIST_QUANTITY,
+                       'PRODUCT_LIST_WEIGHT' => PRODUCT_LIST_WEIGHT,
+                       'PRODUCT_LIST_IMAGE' => PRODUCT_LIST_IMAGE,
+                       'PRODUCT_LIST_BUY_NOW' => PRODUCT_LIST_BUY_NOW);
 
-  if (($specials_split->number_of_rows > 0) && ((PREV_NEXT_BAR_LOCATION == '1') || (PREV_NEXT_BAR_LOCATION == '3'))) {
-?>
+  asort($define_list);
 
-    <div>
-      <span style="float: right;"><?php echo TEXT_RESULT_PAGE . ' ' . $specials_split->display_links(MAX_DISPLAY_PAGE_LINKS, tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></span>
-
-      <span><?php echo $specials_split->display_count(TEXT_DISPLAY_NUMBER_OF_SPECIALS); ?></span>
-    </div>
-
-    <br />
-
-<?php
+  $column_list = array();
+  reset($define_list);
+  while (list($key, $value) = each($define_list)) {
+    if ($value > 0) $column_list[] = $key;
   }
-?>
 
-    <table border="0" width="100%" cellspacing="0" cellpadding="2">
-      <tr>
-<?php
-    $row = 0;
-    $specials_query = tep_db_query($specials_split->sql_query);
-    while ($specials = tep_db_fetch_array($specials_query)) {
-      $row++;
+  $select_column_list = '';
 
-      echo '        <td align="center" width="33%"><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $specials['products_id']) . '">' . tep_image(DIR_WS_IMAGES . $specials['products_image'], $specials['products_name'], SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT) . '</a><br /><a href="' . tep_href_link(FILENAME_PRODUCT_INFO, 'products_id=' . $specials['products_id']) . '">' . $specials['products_name'] . '</a><br /><del>' . $currencies->display_price($specials['products_price'], tep_get_tax_rate($specials['products_tax_class_id'])) . '</del><br /><span class="productSpecialPrice">' . $currencies->display_price($specials['specials_new_products_price'], tep_get_tax_rate($specials['products_tax_class_id'])) . '</span></td>' . "\n";
+  for ($i=0, $n=sizeof($column_list); $i<$n; $i++) {
+    switch ($column_list[$i]) {
+      case 'PRODUCT_LIST_MODEL':
+        $select_column_list .= 'p.products_model, ';
+        break;
+      case 'PRODUCT_LIST_NAME':
+        $select_column_list .= 'pd.products_name, ';
+        break;
+      case 'PRODUCT_LIST_MANUFACTURER':
+        $select_column_list .= 'm.manufacturers_name, ';
+        break;
+      case 'PRODUCT_LIST_QUANTITY':
+        $select_column_list .= 'p.products_quantity, ';
+        break;
+      case 'PRODUCT_LIST_IMAGE':
+        $select_column_list .= 'p.products_image, ';
+        break;
+      case 'PRODUCT_LIST_WEIGHT':
+        $select_column_list .= 'p.products_weight, ';
+        break;
+    }
+  }
 
-      if ((($row / 3) == floor($row / 3))) {
-?>
-      </tr>
-      <tr>
-<?php
+  $listing_sql = "select " . $select_column_list . " p.products_id, SUBSTRING_INDEX(pd.products_description, ' ', 20) as products_description, p.manufacturers_id, p.products_price, p.products_tax_class_id, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price from " . TABLE_PRODUCTS_DESCRIPTION . " pd, " . TABLE_PRODUCTS . " p left join " . TABLE_MANUFACTURERS . " m on p.manufacturers_id = m.manufacturers_id left join " . TABLE_SPECIALS . " s on p.products_id = s.products_id where p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' and s.status = '1'";
+
+  if ( (!isset($HTTP_GET_VARS['sort'])) || (!preg_match('/^[1-8][ad]$/', $HTTP_GET_VARS['sort'])) || (substr($HTTP_GET_VARS['sort'], 0, 1) > sizeof($column_list)) ) {
+    for ($i=0, $n=sizeof($column_list); $i<$n; $i++) {
+      if ($column_list[$i] == 'PRODUCT_LIST_NAME') {
+        $HTTP_GET_VARS['sort'] = $i+1 . 'a';
+        $listing_sql .= " order by pd.products_name";
+        break;
       }
     }
-?>
-      </tr>
-    </table>
+  } else {
+    $sort_col = substr($HTTP_GET_VARS['sort'], 0 , 1);
+    $sort_order = substr($HTTP_GET_VARS['sort'], 1);
 
-<?php
-  if (($specials_split->number_of_rows > 0) && ((PREV_NEXT_BAR_LOCATION == '2') || (PREV_NEXT_BAR_LOCATION == '3'))) {
-?>
-
-    <br />
-
-    <div>
-      <span style="float: right;"><?php echo TEXT_RESULT_PAGE . ' ' . $specials_split->display_links(MAX_DISPLAY_PAGE_LINKS, tep_get_all_get_params(array('page', 'info', 'x', 'y'))); ?></span>
-
-      <span><?php echo $specials_split->display_count(TEXT_DISPLAY_NUMBER_OF_SPECIALS); ?></span>
-    </div>
-
-<?php
+    switch ($column_list[$sort_col-1]) {
+      case 'PRODUCT_LIST_MODEL':
+        $listing_sql .= " order by p.products_model " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_NAME':
+        $listing_sql .= " order by pd.products_name " . ($sort_order == 'd' ? 'desc' : '');
+        break;
+      case 'PRODUCT_LIST_MANUFACTURER':
+        $listing_sql .= " order by m.manufacturers_name " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_QUANTITY':
+        $listing_sql .= " order by p.products_quantity " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_IMAGE':
+        $listing_sql .= " order by pd.products_name";
+        break;
+      case 'PRODUCT_LIST_WEIGHT':
+        $listing_sql .= " order by p.products_weight " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+      case 'PRODUCT_LIST_PRICE':
+        $listing_sql .= " order by final_price " . ($sort_order == 'd' ? 'desc' : '') . ", pd.products_name";
+        break;
+    }
   }
-?>
 
-  </div>
-</div>
+  include(DIR_WS_MODULES . FILENAME_PRODUCT_LISTING);
 
-<?php
   require(DIR_WS_INCLUDES . 'template_bottom.php');
   require(DIR_WS_INCLUDES . 'application_bottom.php');
 ?>
