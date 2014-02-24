fc266bde7023e100d7306ad47331c7645fceafbc
gburton <oscshops@gmail.com>
2014-02-24 13:43:42 +0000

    Address Book Clean Up
    Need to amend the tep_address_label function at some point...

--- address_book.php
+++ address_book.php
@@ -38,41 +38,48 @@
 <div class="contentContainer">
   <h2><?php echo PRIMARY_ADDRESS_TITLE; ?></h2>
 
-  <div class="contentText">
-    <div class="ui-widget infoBoxContainer" style="float: right;">
-      <div class="ui-widget-header infoBoxHeading"><?php echo PRIMARY_ADDRESS_TITLE; ?></div>
+  <div class="contentText row">
+    <div class="col-sm-8">
+      <div class="alert alert-warning"><?php echo PRIMARY_ADDRESS_DESCRIPTION; ?></div>
+    </div>
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading"><?php echo PRIMARY_ADDRESS_TITLE; ?></div>
 
-      <div class="ui-widget-content infoBoxContents">
-        <?php echo tep_address_label($customer_id, $customer_default_address_id, true, ' ', '<br />'); ?>
+        <div class="panel-body">
+          <?php echo tep_address_label($customer_id, $customer_default_address_id, true, ' ', '<br />'); ?>
+        </div>
       </div>
     </div>
-
-    <?php echo PRIMARY_ADDRESS_DESCRIPTION; ?>
   </div>
 
-  <div style="clear: both;"></div>
+  <div class="clearfix"></div>
 
   <h2><?php echo ADDRESS_BOOK_TITLE; ?></h2>
+  
+  <div class="alert alert-warning"><?php echo sprintf(TEXT_MAXIMUM_ENTRIES, MAX_ADDRESS_BOOK_ENTRIES); ?></div>
 
-  <div class="contentText">
-
+  <div class="contentText row">
 <?php
   $addresses_query = tep_db_query("select address_book_id, entry_firstname as firstname, entry_lastname as lastname, entry_company as company, entry_street_address as street_address, entry_suburb as suburb, entry_city as city, entry_postcode as postcode, entry_state as state, entry_zone_id as zone_id, entry_country_id as country_id from " . TABLE_ADDRESS_BOOK . " where customers_id = '" . (int)$customer_id . "' order by firstname, lastname");
   while ($addresses = tep_db_fetch_array($addresses_query)) {
     $format_id = tep_get_address_format_id($addresses['country_id']);
 ?>
-
-    <div>
-      <span style="float: right;"><?php echo tep_draw_button(SMALL_IMAGE_BUTTON_EDIT, 'glyphicon-file', tep_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'edit=' . $addresses['address_book_id'], 'SSL')) . ' ' . tep_draw_button(SMALL_IMAGE_BUTTON_DELETE, 'glyphicon-trash', tep_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'delete=' . $addresses['address_book_id'], 'SSL')); ?></span>
-      <p><strong><?php echo tep_output_string_protected($addresses['firstname'] . ' ' . $addresses['lastname']); ?></strong><?php if ($addresses['address_book_id'] == $customer_default_address_id) echo '&nbsp;<small><i>' . PRIMARY_ADDRESS . '</i></small>'; ?></p>
-      <p style="padding-left: 20px;"><?php echo tep_address_format($format_id, $addresses, true, ' ', '<br />'); ?></p>
-    </div>
-
+      <div class="col-sm-4">
+        <div class="panel panel-<?php echo ($addresses['address_book_id'] == $customer_default_address_id) ? 'primary' : 'default'; ?>">
+          <div class="panel-heading"><?php echo tep_output_string_protected($addresses['firstname'] . ' ' . $addresses['lastname']); ?></strong><?php if ($addresses['address_book_id'] == $customer_default_address_id) echo '&nbsp;<small><i>' . PRIMARY_ADDRESS . '</i></small>'; ?></div>
+          <div class="panel-body">
+            <?php echo tep_address_format($format_id, $addresses, true, ' ', '<br />'); ?>
+          </div>
+          <div class="panel-footer text-center"><?php echo tep_draw_button(SMALL_IMAGE_BUTTON_EDIT, 'glyphicon-file', tep_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'edit=' . $addresses['address_book_id'], 'SSL')) . ' ' . tep_draw_button(SMALL_IMAGE_BUTTON_DELETE, 'glyphicon-trash', tep_href_link(FILENAME_ADDRESS_BOOK_PROCESS, 'delete=' . $addresses['address_book_id'], 'SSL')); ?></div>
+        </div>
+      </div>
 <?php
   }
 ?>
-
   </div>
+  
+  <div class="clearfix"></div>
 
   <div class="buttonSet">
 
@@ -88,8 +95,6 @@
 
     <?php echo tep_draw_button(IMAGE_BUTTON_BACK, 'glyphicon-chevron-left', tep_href_link(FILENAME_ACCOUNT, '', 'SSL')); ?>
   </div>
-
-  <p><?php echo sprintf(TEXT_MAXIMUM_ENTRIES, MAX_ADDRESS_BOOK_ENTRIES); ?></p>
 </div>
 
 <?php
