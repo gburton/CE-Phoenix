a8d95534d6bad8e0ea0096f96c246d644b2a4bbd
gburton <oscshops@gmail.com>
2014-03-20 21:00:33 +0000

    Remove stylesheet.css
    css is now defined elsewhere

--- ext/modules/payment/sage_pay/redirect.php
+++ ext/modules/payment/sage_pay/redirect.php
@@ -40,7 +40,6 @@
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
 <title><?php echo TITLE; ?></title>
 <base href="<?php echo (($request_type == 'SSL') ? HTTPS_SERVER : HTTP_SERVER) . DIR_WS_CATALOG; ?>">
-<link rel="stylesheet" href="stylesheet.css">
 </head>
 <body>
 <form name="redirect" action="<?php echo $redirect_url; ?>" method="post" target="_top"><?php echo $hidden_params; ?>
