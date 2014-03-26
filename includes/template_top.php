cd0dba937fdff2904f233e2c3d2ebc0a996714e9
mattjt83 <mattjt83@gmail.com>
2014-03-26 06:49:52 -0700

    Changed link to respond.js to be local
    Respond js is much simpler to use as a local file

--- includes/template_top.php
+++ includes/template_top.php
@@ -36,7 +36,7 @@
  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
  <!--[if lt IE 9]>
    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+   <script src="ext/bootstrap/js/respond.min.js"></script>
  <![endif]-->
  
  <!-- Custom -->
