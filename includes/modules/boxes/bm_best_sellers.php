587aa18e969e77acf1722d7db0e79f9afdf45517
gburton <oscshops@gmail.com>
2014-03-29 08:17:16 +0000

    INFOBOX - Best Sellers to Footer

--- includes/modules/boxes/bm_best_sellers.php
+++ includes/modules/boxes/bm_best_sellers.php
@@ -26,7 +26,16 @@
         $this->sort_order = MODULE_BOXES_BEST_SELLERS_SORT_ORDER;
         $this->enabled = (MODULE_BOXES_BEST_SELLERS_STATUS == 'True');
 
-        $this->group = ((MODULE_BOXES_BEST_SELLERS_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
+        switch (MODULE_BOXES_BEST_SELLERS_CONTENT_PLACEMENT) {
+          case 'Left Column':
+          $this->group = 'boxes_column_left';
+          break;
+          case 'Footer':
+          $this->group = 'boxes_footer';
+          break;
+          default:
+          $this->group = 'boxes_column_right';
+        }
       }
     }
 
@@ -47,10 +56,23 @@
           }
           $bestsellers_list .= '</ol>';
 
-          $data = '<div class="panel panel-default">' .
-                  '  <div class="panel-heading">' . MODULE_BOXES_BEST_SELLERS_BOX_TITLE . '</div>' .
-                  '  <div class="panel-body">' . $bestsellers_list . '</div>' .
-                  '</div>';
+          if ($this->group == 'boxes_footer') {
+            $data =  '<div class="col-sm-3 col-lg-2">' .
+                     '  <div class="footerbox best-sellers">' .
+                     '    <h2>' . MODULE_BOXES_BEST_SELLERS_BOX_TITLE . '</h2>';
+          }
+          else {
+            $data =  '<div class="panel panel-default">' .
+                     '  <div class="panel-heading">' . MODULE_BOXES_BEST_SELLERS_BOX_TITLE . '</div>';
+          }
+
+          $data .=   '  <div class="panel-body">' . $bestsellers_list . '</div>';
+
+          $data .=   '</div>';
+
+          if ($this->group == 'boxes_footer') {
+            $data .=   '</div>';
+          }
 
           $oscTemplate->addBlock($data, $this->group);
         }
@@ -67,7 +89,7 @@
 
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Best Sellers Module', 'MODULE_BOXES_BEST_SELLERS_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_BEST_SELLERS_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_BEST_SELLERS_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column or directly in the footer?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\', \'Footer\'), ', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_BEST_SELLERS_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
@@ -79,4 +101,4 @@
       return array('MODULE_BOXES_BEST_SELLERS_STATUS', 'MODULE_BOXES_BEST_SELLERS_CONTENT_PLACEMENT', 'MODULE_BOXES_BEST_SELLERS_SORT_ORDER');
     }
   }
-?>
+
