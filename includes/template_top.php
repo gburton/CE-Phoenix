232456a2c28ab59ca6bf54a3aa7b0de6d0a516b2
gburton <oscshops@gmail.com>
2014-04-03 20:15:14 +0100

    Call to jquery move to template_bottom

--- includes/template_top.php
+++ includes/template_top.php
@@ -42,9 +42,7 @@
    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
    <script src="ext/bootstrap/js/respond.min.js"></script>
  <![endif]-->
-
- <script src="ext/jquery/jquery-1.11.0.min.js"></script>
-
+ 
  <?php echo $oscTemplate->getBlocks('header_tags'); ?>
 </head>
 
