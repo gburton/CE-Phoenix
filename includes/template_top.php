45ce60c1788fe7852e446da18ba8b12e8a6028e7
gburton <oscshops@gmail.com>
2014-04-07 12:48:38 +0100

    HTML Messages
    We don't need no stinkin' messages

--- includes/template_top.php
+++ includes/template_top.php
@@ -36,8 +36,6 @@
  <!-- User -->
  <link href="user.css" rel="stylesheet">
 
- <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
- <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
  <!--[if lt IE 9]>
    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
    <script src="ext/bootstrap/js/respond.min.js"></script>
