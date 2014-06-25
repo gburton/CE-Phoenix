534bc0f5722b9cb71ff56527aa5a0fae15426e98
gburton <oscshops@gmail.com>
2014-06-25 11:13:58 +0100

    Remove language constants
    already defined in main language file.  Thanks @gergely

--- includes/languages/english/advanced_search.php
+++ includes/languages/english/advanced_search.php
@@ -36,15 +36,6 @@ define('HEADING_SEARCH_HELP', 'Search Help');
 define('TEXT_SEARCH_HELP', 'Keywords may be separated by AND and/or OR statements for greater control of the search results.<br /><br />For example, <u>Microsoft AND mouse</u> would generate a result set that contain both words. However, for <u>mouse OR keyboard</u>, the result set returned would contain both or either words.<br /><br />Exact matches can be searched for by enclosing keywords in double-quotes.<br /><br />For example, <u>"notebook computer"</u> would generate a result set which match the exact string.<br /><br />Brackets can be used for further control on the result set.<br /><br />For example, <u>Microsoft and (keyboard or mouse or "visual basic")</u>.');
 define('TEXT_CLOSE_WINDOW', '<u>Close Window</u> [x]');
 
-define('TABLE_HEADING_IMAGE', '');
-define('TABLE_HEADING_MODEL', 'Model');
-define('TABLE_HEADING_PRODUCTS', 'Product Name');
-define('TABLE_HEADING_MANUFACTURER', 'Manufacturer');
-define('TABLE_HEADING_QUANTITY', 'Quantity');
-define('TABLE_HEADING_PRICE', 'Price');
-define('TABLE_HEADING_WEIGHT', 'Weight');
-define('TABLE_HEADING_BUY_NOW', 'Buy Now');
-
 define('TEXT_NO_PRODUCTS', 'There is no product that matches the search criteria.');
 
 define('ERROR_AT_LEAST_ONE_INPUT', 'At least one of the fields in the search form must be entered.');
