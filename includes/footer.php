dec702723e3c3b0e90c39f3234c9dd2bce3131d1
John Barounis <jbqwerty@gmail.com>
2014-11-07 21:25:33 +0200

    Changed code to comply with bts v3.x
    "row-fluid" has been removed in favor of row. See http://getbootstrap.com/migration/

--- includes/footer.php
+++ includes/footer.php
@@ -17,17 +17,21 @@
   if ($oscTemplate->hasBlocks('boxes_footer')) {
     ?>
     <div class="footer">
-      <div class="container-fluid row-fluid">
-        <?php echo $oscTemplate->getBlocks('boxes_footer'); ?>
+      <div class="container-fluid">
+        <div class="row">
+          <?php echo $oscTemplate->getBlocks('boxes_footer'); ?>
+        </div>
       </div>
     </div>
     <?php
   }
   ?>
   <div class="footer-extra">
-    <div class="container-fluid row-fluid">
-      <div class="col-sm-6 text-center-xs"><?php echo FOOTER_TEXT_BODY; ?></div>
-      <div class="col-sm-6 text-right text-center-xs"><?php echo FOOTER_TEXT_PAYMENT_ICONS; ?></div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-6 text-center-xs"><?php echo FOOTER_TEXT_BODY; ?></div>
+        <div class="col-sm-6 text-right text-center-xs"><?php echo FOOTER_TEXT_PAYMENT_ICONS; ?></div>
+      </div>
     </div>
   </div>
 </footer>
