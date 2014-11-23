3bf36da3e3916755ec450c62018c830d5ff7cbad
gburton <oscshops@gmail.com>
2014-11-23 15:49:09 +0000

    Remove Header Boxes System

--- includes/modules/boxes/bm_search.php
+++ includes/modules/boxes/bm_search.php
@@ -25,40 +25,25 @@
       if ( defined('MODULE_BOXES_SEARCH_STATUS') ) {
         $this->sort_order = MODULE_BOXES_SEARCH_SORT_ORDER;
         $this->enabled = (MODULE_BOXES_SEARCH_STATUS == 'True');
-
-        switch (MODULE_BOXES_SEARCH_CONTENT_PLACEMENT) {
-          case 'Left Column':
-          $this->group = 'boxes_column_left';
-          break;
-          case 'Header':
-          $this->group = 'boxes_header';
-          break;
-          default:
-          $this->group = 'boxes_column_right';
-        }
         
+        $this->group = ((MODULE_BOXES_SEARCH_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
       }
     }
 
     function execute() {
       global $request_type, $oscTemplate;
 
-      if ($this->group == 'boxes_header') {
-        $data = '<div class="col-sm-6">' . tep_navbar_search('btn-info', false) . '</div>';
-      }
-      else {
-        $data = '<div class="panel panel-default">' .
-                '  <div class="panel-heading">' . MODULE_BOXES_SEARCH_BOX_TITLE . '</div>' .
-                '  <div class="panel-body text-center">' .
-                '    ' . tep_draw_form('quick_find', tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', $request_type, false), 'get') .
-                '    <div class="input-group">' .
-                '    ' . tep_draw_input_field('keywords', '', 'required placeholder="' . TEXT_SEARCH_PLACEHOLDER . '"') .
-                '      <span class="input-group-btn"><button type="submit" class="btn btn-search"><i class="glyphicon glyphicon-search"></i></button></span>' .
-                '    </div>' . tep_draw_hidden_field('search_in_description', '0') . tep_hide_session_id() . '<br />' . MODULE_BOXES_SEARCH_BOX_TEXT . '<br /><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH) . '"><strong>' . MODULE_BOXES_SEARCH_BOX_ADVANCED_SEARCH . '</strong></a>' .
-                '    </form>' .
-                '  </div>' .
-                '</div>';
-      }
+      $data = '<div class="panel panel-default">' .
+              '  <div class="panel-heading">' . MODULE_BOXES_SEARCH_BOX_TITLE . '</div>' .
+              '  <div class="panel-body text-center">' .
+              '    ' . tep_draw_form('quick_find', tep_href_link(FILENAME_ADVANCED_SEARCH_RESULT, '', $request_type, false), 'get') .
+              '    <div class="input-group">' .
+              '    ' . tep_draw_input_field('keywords', '', 'required placeholder="' . TEXT_SEARCH_PLACEHOLDER . '"') .
+              '      <span class="input-group-btn"><button type="submit" class="btn btn-search"><i class="glyphicon glyphicon-search"></i></button></span>' .
+              '    </div>' . tep_draw_hidden_field('search_in_description', '0') . tep_hide_session_id() . '<br />' . MODULE_BOXES_SEARCH_BOX_TEXT . '<br /><a href="' . tep_href_link(FILENAME_ADVANCED_SEARCH) . '"><strong>' . MODULE_BOXES_SEARCH_BOX_ADVANCED_SEARCH . '</strong></a>' .
+              '    </form>' .
+              '  </div>' .
+              '</div>';
 
       $oscTemplate->addBlock($data, $this->group);
     }
@@ -73,7 +58,7 @@
 
     function install() {
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Search Module', 'MODULE_BOXES_SEARCH_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_SEARCH_CONTENT_PLACEMENT', 'Header', 'Should the module be loaded in the left or right column or directly in the header?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\', \'Header\'), ', now())");
+      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_SEARCH_CONTENT_PLACEMENT', 'Left Column', 'Should the module be loaded in the left or right column?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
       tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_SEARCH_SORT_ORDER', '', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
