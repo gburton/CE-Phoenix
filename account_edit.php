534737d40176cc7296ff424ad6f0f8e49685f95f
raiwa <info@sarplataygemas.com>
2014-12-15 16:04:13 +0100

    Update input types
    Added input type e-mail to e-mail
    Added input type tel to telephone
    Added input type tel to fax

--- account_edit.php
+++ account_edit.php
@@ -196,21 +196,21 @@
   <div class="form-group has-feedback">
     <label for="inputEmail" class="control-label col-xs-3"><?php echo ENTRY_EMAIL_ADDRESS; ?></label>
     <div class="col-xs-9">
-      <?php echo tep_draw_input_field('email_address', $account['customers_email_address'], 'required aria-required="true" id="inputEmail" placeholder="' . ENTRY_EMAIL_ADDRESS . '"'); ?>
+      <?php echo tep_draw_input_field('email_address', $account['customers_email_address'], 'required aria-required="true" id="inputEmail" placeholder="' . ENTRY_EMAIL_ADDRESS . '"','email'); ?>
       <?php echo FORM_REQUIRED_INPUT; ?>
     </div>
   </div>
   <div class="form-group has-feedback">
     <label for="inputTelephone" class="control-label col-xs-3"><?php echo ENTRY_TELEPHONE_NUMBER; ?></label>
     <div class="col-xs-9">
-      <?php echo tep_draw_input_field('telephone', $account['customers_telephone'], 'required aria-required="true" id="inputTelephone" placeholder="' . ENTRY_TELEPHONE_NUMBER . '"'); ?>
+      <?php echo tep_draw_input_field('telephone', $account['customers_telephone'], 'required aria-required="true" id="inputTelephone" placeholder="' . ENTRY_TELEPHONE_NUMBER . '"', 'tel'); ?>
       <?php echo FORM_REQUIRED_INPUT; ?>
     </div>
   </div>
   <div class="form-group">
     <label for="inputFax" class="control-label col-xs-3"><?php echo ENTRY_FAX_NUMBER; ?></label>
     <div class="col-xs-9">
-      <?php echo tep_draw_input_field('fax', $account['customers_fax'], 'id="inputFax" placeholder="' . ENTRY_FAX_NUMBER . '"'); ?>
+      <?php echo tep_draw_input_field('fax', $account['customers_fax'], 'id="inputFax" placeholder="' . ENTRY_FAX_NUMBER . '"', 'tel'); ?>
     </div>
   </div>
 
