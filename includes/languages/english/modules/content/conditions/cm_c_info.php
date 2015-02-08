68f02ad5dcee5e803a655344428b75855e43723e
Matt <matt@sweetnessandlight.com>
2015-02-08 11:00:05 -0800

    Modularize the conditions of use page

---
+++ includes/languages/english/modules/content/conditions/cm_c_info.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_CONDITIONS_INFO_TITLE', 'Conditions of Use info');
+  define('MODULE_CONTENT_CONDITIONS_INFO_DESCRIPTION', 'Show Conditions of Use info block on the Conditions of Use info page.');
+  
+  define('MODULE_CONTENT_CONDITIONS_INFO_INFORMATION', '<p>Put your Conditions of Use information here.</p>');
