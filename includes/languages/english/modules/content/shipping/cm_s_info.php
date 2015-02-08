c14e74430c08b8f3bf461c745214ba3ecb2401d5
Matt <matt@sweetnessandlight.com>
2015-02-08 09:59:59 -0800

    Modularize shipping & returns page

---
+++ includes/languages/english/modules/content/shipping/cm_s_info.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_SHIPPING_INFO_TITLE', 'Shipping info');
+  define('MODULE_CONTENT_SHIPPING_INFO_DESCRIPTION', 'Show shipping info block on the shipping info page.');
+  
+  define('MODULE_CONTENT_SHIPPING_INFO_INFORMATION', '<p>Put your Shipping &amp; Returns information here.</p>');
---
+++ includes/modules/content/shipping/templates/info.php
@@ -0,0 +1,4 @@
+<div class="col-sm-<?php echo $content_width; ?> shippinginfo">
+  <?php echo MODULE_CONTENT_SHIPPING_INFO_INFORMATION; ?>
+</div>
+
