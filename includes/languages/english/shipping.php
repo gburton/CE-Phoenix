706b645b2653317f9e621a83d32e4b290a1fb580
Matt <matt@sweetnessandlight.com>
2015-02-08 10:41:05 -0800

    remove unused define

--- includes/languages/english/shipping.php
+++ includes/languages/english/shipping.php
@@ -13,5 +13,4 @@
 define('NAVBAR_TITLE', 'Shipping &amp; Returns');
 define('HEADING_TITLE', 'Shipping &amp; Returns');
 
-define('TEXT_INFORMATION', 'Put here your Shipping &amp; Returns information.');
 ?>
