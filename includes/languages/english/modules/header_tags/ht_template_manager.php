9800384fb840f73a012fdd479f09603bca24e554
osc2nuke <osc2nuke@gmail.com>
2015-02-09 14:24:58 +0100

    template manager header tag module

---
+++ includes/languages/english/modules/header_tags/ht_template_manager.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2015 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_TEMPLATE_MANAGER_TITLE', 'Template Manager');
+  define('MODULE_HEADER_TAGS_TEMPLATE_MANAGER_DESCRIPTION', 'With this module you can choose your shop template.');
+?>
