faab3ed9e85492900bb9ea61bac1d50839ab08b9
sweetnessandlightcandy <matt@sweetnessandlight.com>
2015-02-11 05:51:03 -0800

    Prettify account page

--- includes/modules/pages/tp_account.php
+++ includes/modules/pages/tp_account.php
@@ -19,49 +19,61 @@
       $oscTemplate->_data[$this->group] = array('account' => array('title' => MY_ACCOUNT_TITLE,
                                                                    'links' => array('edit' => array('title' => MY_ACCOUNT_INFORMATION,
                                                                                                     'link' => tep_href_link(FILENAME_ACCOUNT_EDIT, '', 'SSL'),
-                                                                                                    'icon' => 'glyphicon glyphicon-user'),
+                                                                                                    'icon' => 'fa fa-user fa-3x'),
                                                                                     'address_book' => array('title' => MY_ACCOUNT_ADDRESS_BOOK,
                                                                                                             'link' => tep_href_link(FILENAME_ADDRESS_BOOK, '', 'SSL'),
-                                                                                                            'icon' => 'glyphicon glyphicon-home'),
+                                                                                                            'icon' => 'fa fa-home fa-3x'),
                                                                                     'password' => array('title' => MY_ACCOUNT_PASSWORD,
                                                                                                         'link' => tep_href_link(FILENAME_ACCOUNT_PASSWORD, '', 'SSL'),
-                                                                                                        'icon' => 'glyphicon glyphicon-cog'))),
+                                                                                                        'icon' => 'fa fa-cog fa-3x'))),
                                                 'orders' => array('title' => MY_ORDERS_TITLE,
                                                                   'links' => array('history' => array('title' => MY_ORDERS_VIEW,
                                                                                                       'link' => tep_href_link(FILENAME_ACCOUNT_HISTORY, '', 'SSL'),
-                                                                                                      'icon' => 'glyphicon glyphicon-shopping-cart'))),
+                                                                                                      'icon' => 'fa fa-shopping-cart fa-3x'))),
                                                 'notifications' => array('title' => EMAIL_NOTIFICATIONS_TITLE,
                                                                          'links' => array('newsletters' => array('title' => EMAIL_NOTIFICATIONS_NEWSLETTERS,
                                                                                                                  'link' => tep_href_link(FILENAME_ACCOUNT_NEWSLETTERS, '', 'SSL'),
-                                                                                                                 'icon' => 'glyphicon glyphicon-envelope'),
+                                                                                                                 'icon' => 'fa fa-envelope fa-3x'),
                                                                                           'products' => array('title' => EMAIL_NOTIFICATIONS_PRODUCTS,
                                                                                                               'link' => tep_href_link(FILENAME_ACCOUNT_NOTIFICATIONS, '', 'SSL'),
-                                                                                                              'icon' => 'glyphicon glyphicon-send'))));
+                                                                                                              'icon' => 'fa fa-send fa-3x'))));
     }
 
     function build() {
       global $oscTemplate;
-
-      $output = '';
+      
+      $count              = 0;
+      $output             = '<div role="tabpanel">';
+      $output_tab_list    = '<ul class="nav nav-tabs">';
+      $output_tab_content = '<div class="tab-content">';
 
       foreach ( $oscTemplate->_data[$this->group] as $group ) {
-        $output .= '<h2>' . $group['title'] . '</h2>' .
-                   '<div class="contentText">' .
-                   '  <ul class="accountLinkList">';
+        
+        $output_tab_list .= '<li role="presentation"' . (( $count == 0 ) ? ' class="active"':'') . '><a href="#' . str_replace(' ', '', strtolower($group['title'])) . '" aria-controls="home" role="tab" data-toggle="tab">' . $group['title'] . '</a></li>';
+        
+        $output_tab_content .= '<div role="tabpanel" class="tab-pane fade' . (( $count == 0 ) ? ' active in':'') . '" id="' . str_replace(' ', '', strtolower($group['title'])) . '"><ul class="accountLinkList">';
 
         foreach ( $group['links'] as $entry ) {
-          $output .= '    <li>';
+          $output_tab_content .= '  <li>';
 
           if ( isset($entry['icon']) ) {
-            $output .= '<i class="' . $entry['icon'] . '"></i> ';
+            $output_tab_content .= '<i class="' . $entry['icon'] . '"></i> ';
           }
 
-          $output .= '<a href="' . $entry['link'] . '">' . $entry['title'] . '</a></li>';
+          $output_tab_content .= '<a href="' . $entry['link'] . '">' . $entry['title'] . '</a></li>';
         }
 
-        $output .= '  </ul>' .
-                   '</div>';
+        $output_tab_content .= '</ul></div>';
+        
+        $count++;
       }
+      
+      $output_tab_content .= '</div>';
+      $output_tab_list .= '</ul>';
+      
+      $output .= $output_tab_list;
+      $output .= $output_tab_content;
+      $output .= '</div>';
 
       $oscTemplate->addContent($output, $this->group);
     }
