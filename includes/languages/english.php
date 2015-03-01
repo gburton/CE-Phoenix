9b45d36cb7d854762f25360bf72a48a0170fb971
kolingua <antonio.xyz@hotmail.com>
2015-03-01 01:05:28 +0100

    Update english.php
    Update the comment with the appropriate documentation link

--- includes/languages/english.php
+++ includes/languages/english.php
@@ -22,7 +22,7 @@ define('DATE_FORMAT_SHORT', '%m/%d/%Y');  // this is used for strftime()
 define('DATE_FORMAT_LONG', '%A %d %B, %Y'); // this is used for strftime()
 define('DATE_FORMAT', 'm/d/Y'); // this is used for date()
 define('DATE_TIME_FORMAT', DATE_FORMAT_SHORT . ' %H:%M:%S');
-define('JQUERY_DATEPICKER_I18N_CODE', ''); // leave empty for en_US; see http://jqueryui.com/demos/datepicker/#localization
+define('JQUERY_DATEPICKER_I18N_CODE', ''); // leave empty for en_US; see http://bootstrap-datepicker.readthedocs.org/en/release/options.html#language
 define('BOOTSTRAP_DATEPICKER_FORMAT', 'mm/dd/yyyy'); // see http://bootstrap-datepicker.readthedocs.org/en/release/options.html#format
 
 ////
