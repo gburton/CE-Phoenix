e70433eafa39b458600674e1a94be0bf94415716
Steve <sakleiner@yahoo.com>
2015-03-07 14:56:21 -0500

    Meta Title Separator Update

--- includes/modules/header_tags/ht_product_title.php
+++ includes/modules/header_tags/ht_product_title.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2010 osCommerce
+  Copyright (c) 2015 osCommerce
 
   Released under the GNU General Public License
 */
@@ -31,13 +31,18 @@
     function execute() {
       global $PHP_SELF, $oscTemplate, $HTTP_GET_VARS, $languages_id, $product_check;
 
-      if ( (basename($PHP_SELF) == FILENAME_PRODUCT_INFO) || (basename($PHP_SELF) == 'product_reviews.php') ) {
+      if (basename($PHP_SELF) == FILENAME_PRODUCT_INFO) {
         if (isset($HTTP_GET_VARS['products_id'])) {
           if ($product_check['total'] > 0) {
-            $product_info_query = tep_db_query("select pd.products_name from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where p.products_status = '1' and p.products_id = '" . (int)$HTTP_GET_VARS['products_id'] . "' and pd.products_id = p.products_id and pd.language_id = '" . (int)$languages_id . "'");
+            $product_info_query = tep_db_query("select pd.products_name, pd.products_seo_title from " . TABLE_PRODUCTS . " p, " . TABLE_PRODUCTS_DESCRIPTION . " pd where p.products_status = '1' and p.products_id = '" . (int)$HTTP_GET_VARS['products_id'] . "' and pd.products_id = p.products_id and pd.language_id = '" . (int)$languages_id . "'");
             $product_info = tep_db_fetch_array($product_info_query);
 
-            $oscTemplate->setTitle($product_info['products_name'] . ', ' . $oscTemplate->getTitle());
+            if (tep_not_null($product_info['products_seo_title'])) {
+              $oscTemplate->setTitle($product_info['products_seo_title'] . MODULE_HEADER_TAGS_PAGES_SEO_SEPARATOR . $oscTemplate->getTitle());
+            }
+            else {
+              $oscTemplate->setTitle($product_info['products_name'] . MODULE_HEADER_TAGS_PAGES_SEO_SEPARATOR . $oscTemplate->getTitle());
+            }
           }
         }
       }
