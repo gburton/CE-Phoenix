a181e674f49ed46f8ec923134336d58a2d704139
crazy-colorado <mr.marcello@gmail.com>
2015-07-06 15:36:33 -0700

    Update cm_footer_extra_copyright.php
    Added Bootstrap Row Description to content module that contains field.

--- includes/modules/content/footer_suffix/cm_footer_extra_copyright.php
+++ includes/modules/content/footer_suffix/cm_footer_extra_copyright.php
@@ -24,6 +24,7 @@
 
       $this->title = MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_TITLE;
       $this->description = MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_DESCRIPTION;
+      $this->description .= '<div class="secWarning">' . MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION . '</div>';
 
       if ( defined('MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_STATUS') ) {
         $this->sort_order = MODULE_CONTENT_FOOTER_EXTRA_COPYRIGHT_SORT_ORDER;
