66cd76a50fc1151bce2472867ba506304778074a
crazy-colorado <mr.marcello@gmail.com>
2015-07-08 10:56:07 -0700

    Update shopping_cart.php
    Added class="panel-heading-link" to header link.

--- includes/modules/boxes/templates/shopping_cart.php
+++ includes/modules/boxes/templates/shopping_cart.php
@@ -1,5 +1,5 @@
 <div class="panel panel-default">
-  <div class="panel-heading"><a href="<?php echo tep_href_link('shopping_cart.php'); ?>"><?php echo MODULE_BOXES_SHOPPING_CART_BOX_TITLE; ?></a></div>
+  <div class="panel-heading"><a class="panel-heading-link" href="<?php echo tep_href_link('shopping_cart.php'); ?>"><?php echo MODULE_BOXES_SHOPPING_CART_BOX_TITLE; ?></a></div>
   <div class="panel-body">
     <ul class="shoppingCartList">
       <?php echo $cart_contents_string; ?>
