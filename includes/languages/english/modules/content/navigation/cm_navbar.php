af86f42854759fb7d8daf29acecba1aa8b96d949
gburton <oscshops@gmail.com>
2016-02-22 12:36:45 +0000

    Introduce Navigation Bar Modularisation
    This will consist of the Navbar Content Module, and a bunch of Sub Modules

--- includes/languages/english/modules/content/navigation/cm_navbar.php
+++ includes/languages/english/modules/content/navigation/cm_navbar.php
@@ -5,36 +5,11 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2014 osCommerce
+  Copyright (c) 2016 osCommerce
 
   Released under the GNU General Public License
 */
 
   define('MODULE_CONTENT_NAVBAR_TITLE', 'Navigation Bar');
-  define('MODULE_CONTENT_NAVBAR_DESCRIPTION', 'Show the Navigation Bar on your site.');
+  define('MODULE_CONTENT_NAVBAR_DESCRIPTION', 'Show the Navigation Bar on your site. <div class="secWarning">This module has a number of Sub Modules which must also be installed.<br><br>Admin > Modules > Navbar Modules</div>');
   
-  //header titles
-  define('HEADER_CART_CONTENTS', '<i class="fa fa-shopping-cart"></i> %s item(s) <span class="caret"></span>');
-  define('HEADER_CART_NO_CONTENTS', '<i class="fa fa-shopping-cart"></i> 0 items');
-  define('HEADER_ACCOUNT_LOGGED_OUT', '<i class="fa fa-user"></i><span class="hidden-sm"> My Account</span> <span class="caret"></span>');
-  define('HEADER_ACCOUNT_LOGGED_IN', '<i class="fa fa-user"></i> %s <span class="caret"></span>');
-  define('HEADER_SITE_SETTINGS', '<i class="fa fa-cog"></i><span class="hidden-sm"> Site Settings</span> <span class="caret"></span>');
-  define('HEADER_TOGGLE_NAV', 'Toggle Navigation');
-  define('HEADER_HOME', '<i class="fa fa-home"></i><span class="hidden-sm"> Home</span>');
-  define('HEADER_WHATS_NEW', '<i class="fa fa-certificate"></i><span class="hidden-sm">  New Products</span>');
-  define('HEADER_SPECIALS', '<i class="fa fa-fire"></i><span class="hidden-sm"> Special Offers</span>');
-  define('HEADER_REVIEWS', '<i class="fa fa-comment"></i><span class="hidden-sm"> Reviews</span>');
-  // header dropdowns
-  define('HEADER_ACCOUNT_LOGIN', '<i class="fa fa-sign-in"></i> Log In');
-  define('HEADER_ACCOUNT_LOGOFF', '<i class="fa fa-sign-out"></i> Log Off');
-  define('HEADER_ACCOUNT', 'My Account');
-  define('HEADER_ACCOUNT_HISTORY', 'My Orders');
-  define('HEADER_ACCOUNT_EDIT', 'My Details');
-  define('HEADER_ACCOUNT_ADDRESS_BOOK', 'My Address Book');
-  define('HEADER_ACCOUNT_PASSWORD', 'My Password');
-  define('HEADER_ACCOUNT_REGISTER', '<i class="fa fa-pencil"></i> Register');
-  define('HEADER_CART_HAS_CONTENTS', '%s item(s), %s');
-  define('HEADER_CART_VIEW_CART', 'View Cart');
-  define('HEADER_CART_CHECKOUT', '<i class="fa fa-angle-right"></i> Checkout');
-  define('USER_LOCALIZATION', '<abbr title="Selected Language">L:</abbr> %s <abbr title="Selected Currency">C:</abbr> %s');
-
--- includes/modules/content/navigation/cm_navbar.php
+++ includes/modules/content/navigation/cm_navbar.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2014 osCommerce
+  Copyright (c) 2016 osCommerce
 
   Released under the GNU General Public License
 */
@@ -32,14 +32,36 @@
     }
 
     function execute() {
-      global $PHP_SELF, $cart, $lng, $language, $currencies, $HTTP_GET_VARS, $request_type, $currency, $oscTemplate;
-      global $customer_first_name;
+      global $language, $oscTemplate;
+      
+      if ( defined('MODULE_CONTENT_NAVBAR_INSTALLED') && tep_not_null(MODULE_CONTENT_NAVBAR_INSTALLED) ) {
+        $nav_array = explode(';', MODULE_CONTENT_NAVBAR_INSTALLED);
 
-      ob_start();
-      include(DIR_WS_MODULES . 'content/' . $this->group . '/templates/navbar.php');
-      $template = ob_get_clean();
+        $navbar_modules = array();
 
-      $oscTemplate->addContent($template, $this->group);
+        foreach ( $nav_array as $nbm ) {
+          $class = substr($nbm, 0, strrpos($nbm, '.'));
+
+          if ( !class_exists($class) ) {
+            include(DIR_WS_LANGUAGES . $language . '/modules/navbar_modules/' . $nbm);
+            require(DIR_WS_MODULES . 'navbar_modules/' . $class . '.php');
+          }
+
+          $nav = new $class();
+
+          if ( $nav->isEnabled() ) {
+            $navbar_modules[] = $nav->getOutput();
+          }
+        }
+
+        if ( !empty($navbar_modules) ) {          
+          ob_start();
+          include(DIR_WS_MODULES . 'content/' . $this->group . '/templates/navbar.php');
+          $template = ob_get_clean();
+
+          $oscTemplate->addContent($template, $this->group);
+        }
+      }      
     }
 
     function isEnabled() {
@@ -51,16 +73,15 @@
     }
 
     function install() {
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Navbar Module', 'MODULE_CONTENT_NAVBAR_STATUS', 'True', 'Should the Navbar be shown? ', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into " . TABLE_CONFIGURATION . " (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_NAVBAR_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Navbar Module', 'MODULE_CONTENT_NAVBAR_STATUS', 'True', 'Should the Navbar be shown? ', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_NAVBAR_SORT_ORDER', '10', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
     function remove() {
-      tep_db_query("delete from " . TABLE_CONFIGURATION . " where configuration_key in ('" . implode("', '", $this->keys()) . "')");
+      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
     }
 
     function keys() {
       return array('MODULE_CONTENT_NAVBAR_STATUS', 'MODULE_CONTENT_NAVBAR_SORT_ORDER');
     }
   }
-
