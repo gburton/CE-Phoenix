6fec2631cf1454baf2246301e33a933593bb026c
gburton <oscshops@gmail.com>
2016-02-23 13:07:18 +0000

    Navbar: Modular New Product Link
    Fix language module

---
+++ includes/languages/english/modules/navbar_modules/nb_new_products.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License 
+*/
+
+  define('MODULE_NAVBAR_NEW_PRODUCTS_TITLE', 'New Products');
+  define('MODULE_NAVBAR_NEW_PRODUCTS_DESCRIPTION', 'Show New Products Link in Navbar.');
+  
+  define('MODULE_NAVBAR_NEW_PRODUCTS_PUBLIC_TEXT', '<li><a href="' . tep_href_link('products_new.php') . '"><i class="fa fa-certificate"></i><span class="hidden-sm">  New Products</span></a></li>');
+  
