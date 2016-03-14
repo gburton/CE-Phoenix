9abdb3b8315e015b9354d9df8dcceb88f32537e0
gburton <oscshops@gmail.com>
2016-03-14 19:10:32 +0000

    GTIN Display
    Allow shopowner to choose how the GTIN is displayed.

--- includes/modules/content/product_info/cm_pi_gtin.php
+++ includes/modules/content/product_info/cm_pi_gtin.php
@@ -38,7 +38,7 @@
       $content_width = (int)MODULE_CONTENT_PRODUCT_INFO_GTIN_CONTENT_WIDTH;
 
       if (tep_not_null($product_info['products_gtin'])) {
-        $gtin = $product_info['products_gtin'];
+        $gtin = substr($product_info['products_gtin'], 0-MODULE_CONTENT_PRODUCT_INFO_GTIN_LENGTH);
         
         ob_start();
         include(DIR_WS_MODULES . 'content/' . $this->group . '/templates/gtin.php');
@@ -59,6 +59,7 @@
     function install() {
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable GTIN Module', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_STATUS', 'True', 'Should this module be shown on the product info page?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_CONTENT_WIDTH', '6', 'What width container should the content be shown in?', '6', '1', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Length of GTIN', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_LENGTH', '13', 'Length of GTIN. 14 (Industry Standard), 13 (eg ISBN codes and EAN UCC-13), 12 (UPC), 8 (EAN UCC-8)', '6', '0', 'tep_cfg_select_option(array(\'14\', \'13\', \'12\', \'8\'), ', now())");
       tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
@@ -67,7 +68,7 @@
     }
 
     function keys() {
-      return array('MODULE_CONTENT_PRODUCT_INFO_GTIN_STATUS', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_CONTENT_WIDTH', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_SORT_ORDER');
+      return array('MODULE_CONTENT_PRODUCT_INFO_GTIN_STATUS', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_CONTENT_WIDTH', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_LENGTH', 'MODULE_CONTENT_PRODUCT_INFO_GTIN_SORT_ORDER');
     }
   }
   
--- includes/modules/content/product_info/templates/gtin.php
+++ includes/modules/content/product_info/templates/gtin.php
@@ -1,7 +1,7 @@
 <div class="col-sm-<?php echo $content_width; ?> gtin">
   <ul class="list-group">
     <li class="list-group-item">
-      <span itemprop="gtin14" class="badge"><?php echo $gtin; ?></span>
+      <span itemprop="gtin<?php echo MODULE_CONTENT_PRODUCT_INFO_GTIN_LENGTH; ?>" class="badge"><?php echo $gtin; ?></span>
       <?php echo MODULE_CONTENT_PRODUCT_INFO_GTIN_PUBLIC_TITLE; ?>
     </li>
   </ul>
