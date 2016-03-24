0523ace75848ec83ddead07403d2efd5ecace618
Juanma <mazinfriki@gmail.com>
2016-03-24 08:38:04 +0100

    Modular Account history info
    All content moved to modules. Language files updated for each module and
    unneeded strings removed from account:history_info language file.

---
+++ includes/languages/english/modules/content/account_history_info/cm_ahi_details.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_ACCOUNT_HISTORY_INFO_DETAILS_TITLE', 'Order Details');
+  define('MODULE_CONTENT_ACCOUNT_HISTORY_INFO_DETAILS_DESCRIPTION', 'Show Order Details on the account history info page.');
+  define('HEADING_ORDER_NUMBER', 'Order #%s');
+  define('HEADING_PRODUCTS', 'Products');
+  define('HEADING_TAX', 'Tax');
+  define('HEADING_TOTAL', 'Total');
+  define('HEADING_ORDER_TOTAL', 'Order Total:');
+  define('HEADING_ORDER_DATE', 'Order Date:');
---
+++ includes/modules/content/account_history_info/templates/cm_ahi_title.php
@@ -0,0 +1,17 @@
+<?php
+/*
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+
+*/
+?>
+<!-- Start cm_ahi_title module -->
+    <div class="page-header col-sm-<?php echo (int)MODULE_CONTENT_ACCOUNT_HISTORY_INFO_TITLE_CONTENT_WIDTH; ?>">
+      <h1><?php echo HEADING_TITLE; ?></h1>
+    </div>
+<!-- End cm_ahi_title module -->
