41025c36c67f56bcd0ad2ec6d266e1294c00a5ec
gburton <oscshops@gmail.com>
2016-03-29 18:47:46 +0100

    Testimonials Link
    Navbar Module.
    Shopowner could also manually change (eg) the information sidebox/footerbox etc.

---
+++ includes/languages/english/modules/navbar_modules/nb_testimonials.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2016 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_NAVBAR_TESTIMONIALS_TITLE', 'Testimonials'); 
+  define('MODULE_NAVBAR_TESTIMONIALS_DESCRIPTION', 'Show Testimonials Link in Navbar.');
+  
+  define('MODULE_NAVBAR_TESTIMONIALS_PUBLIC_TEXT', '<li><a href="' . tep_href_link('testimonials.php') . '"><i class="fa fa-pencil-square-o"></i><span class="hidden-sm"> Testimonials</span></a></li>');
+  
