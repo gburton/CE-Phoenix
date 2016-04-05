0f7b69da62c545dca49092e6b46040f88eee80c2
gburton <oscshops@gmail.com>
2016-04-05 11:12:00 +0100

    Shop Side

--- includes/languages/english/modules/header_tags/ht_manufacturer_title.php
+++ includes/languages/english/modules/header_tags/ht_manufacturer_title.php
@@ -5,11 +5,13 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2010 osCommerce
+  Copyright (c) 2016 osCommerce
 
   Released under the GNU General Public License
 */
 
   define('MODULE_HEADER_TAGS_MANUFACTURER_TITLE_TITLE', 'Manufacturer Title');
   define('MODULE_HEADER_TAGS_MANUFACTURER_TITLE_DESCRIPTION', 'Add the title of the current manufacturer to the page title');
-?>
+
+  define('MODULE_HEADER_TAGS_MANUFACTURER_SEO_SEPARATOR', ' | ');
+  
