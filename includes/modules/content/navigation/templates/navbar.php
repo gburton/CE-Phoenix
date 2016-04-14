cbd73c1cc2d99c52e7f5e86e0d29c104dbe73bc7
Juanma <mazinfriki@gmail.com>
2016-04-14 15:00:24 +0200

    SQL install and CSS for navbar-fixed

--- includes/modules/content/navigation/templates/navbar.php
+++ includes/modules/content/navigation/templates/navbar.php
@@ -23,3 +23,22 @@
     </div>
   </div>
 </nav>
+<?php
+switch ($navbar_fixed) {
+  
+  case " navbar-fixed-top":
+?>
+<style scoped>
+body { padding-top: 50px; }
+</style>
+<?php
+  break;
+  
+  case " navbar-fixed-bottom":
+?>
+<style scoped>
+body { padding-bottom: 50px; }
+</style>
+<?php
+  break;
+}
