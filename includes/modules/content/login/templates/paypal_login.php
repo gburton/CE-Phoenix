e3a1de1ebbb0903144e3249c62fa730af16f15c2
Juanma <mazinfriki@gmail.com>
2016-04-15 19:43:20 +0200

    Paypal login module

--- includes/modules/content/login/templates/paypal_login.php
+++ includes/modules/content/login/templates/paypal_login.php
@@ -1,15 +1,15 @@
-<div class="paypal-login <?php echo (MODULE_CONTENT_PAYPAL_LOGIN_CONTENT_WIDTH == 'Half') ? 'col-sm-6' : 'col-sm-12'; ?>">
+<div class="paypal-login <?php echo (MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_CONTENT_WIDTH == 'Half') ? 'col-sm-6' : 'col-sm-12'; ?>">
   <div class="panel panel-success">
     <div class="panel-body">
-      <h2><?php echo MODULE_CONTENT_PAYPAL_LOGIN_TEMPLATE_TITLE; ?></h2>
+      <h2><?php echo MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_TEMPLATE_TITLE; ?></h2>
 
 <?php
-  if ( MODULE_CONTENT_PAYPAL_LOGIN_SERVER_TYPE == 'Sandbox' ) {
-    echo '    <p class="messageStackError">' . MODULE_CONTENT_PAYPAL_LOGIN_TEMPLATE_SANDBOX . '</p>';
+  if ( MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_SERVER_TYPE == 'Sandbox' ) {
+    echo '    <p class="messageStackError">' . MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_TEMPLATE_SANDBOX . '</p>';
   }
 ?>
 
-      <p class="alert alert-success"><?php echo MODULE_CONTENT_PAYPAL_LOGIN_TEMPLATE_CONTENT; ?></p>
+      <p class="alert alert-success"><?php echo MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_TEMPLATE_CONTENT; ?></p>
 
       <div id="PayPalLoginButton" class="text-right"></div>
     </div>
@@ -22,20 +22,20 @@ paypal.use( ["login"], function(login) {
   login.render ({
 
 <?php
-  if ( MODULE_CONTENT_PAYPAL_LOGIN_SERVER_TYPE == 'Sandbox' ) {
+  if ( MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_SERVER_TYPE == 'Sandbox' ) {
     echo '    "authend": "sandbox",';
   }
 
-  if ( MODULE_CONTENT_PAYPAL_LOGIN_THEME == 'Neutral' ) {
+  if ( MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_THEME == 'Neutral' ) {
     echo '    "theme": "neutral",';
   }
 
-  if ( defined('MODULE_CONTENT_PAYPAL_LOGIN_LANGUAGE_LOCALE') && tep_not_null(MODULE_CONTENT_PAYPAL_LOGIN_LANGUAGE_LOCALE) ) {
-    echo '    "locale": "' . MODULE_CONTENT_PAYPAL_LOGIN_LANGUAGE_LOCALE . '",';
+  if ( defined('MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_LANGUAGE_LOCALE') && tep_not_null(MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_LANGUAGE_LOCALE) ) {
+    echo '    "locale": "' . MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_LANGUAGE_LOCALE . '",';
   }
 ?>
 
-    "appid": "<?php echo MODULE_CONTENT_PAYPAL_LOGIN_CLIENT_ID; ?>",
+    "appid": "<?php echo MODULE_CONTENT_LOGIN_PAYPAL_LOGIN_CLIENT_ID; ?>",
     "scopes": "<?php echo implode(' ', $use_scopes); ?>",
     "containerid": "PayPalLoginButton",
     "returnurl": "<?php echo str_replace('&amp;', '&', tep_href_link(FILENAME_LOGIN, 'action=paypal_login', 'SSL', false)); ?>"
