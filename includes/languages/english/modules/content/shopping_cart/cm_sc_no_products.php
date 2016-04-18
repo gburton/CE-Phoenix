11463f6d6de08ec7f45966bf3962b7a03159de62
MOPS-DanCole <dancole@mops.ca>
2016-04-17 23:32:16 -0400

    Another language file addition.

--- includes/languages/english/modules/content/shopping_cart/cm_sc_no_products.php
+++ includes/languages/english/modules/content/shopping_cart/cm_sc_no_products.php
@@ -12,3 +12,5 @@
   
   define('MODULE_CONTENT_SC_NO_PRODUCTS_TITLE', 'No Products Message');
   define('MODULE_CONTENT_SC_NO_PRODUCTS_DESCRIPTION', 'Show the No Products message on the shopping cart page.');
+  
+  define('TEXT_CART_EMPTY', 'Your Shopping Cart is empty!');
