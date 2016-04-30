134d1ffc21486d1a94dd420e54bba4c5badef160
gburton <oscshops@gmail.com>
2016-04-30 11:21:19 +0100

    Main Text Module

--- includes/languages/english/index.php
+++ includes/languages/english/index.php
@@ -10,7 +10,6 @@
   Released under the GNU General Public License
 */
 
-define('TEXT_MAIN', '');
 define('TABLE_HEADING_NEW_PRODUCTS', 'New Products For %s');
 define('TABLE_HEADING_UPCOMING_PRODUCTS', 'Upcoming Products');
 define('TABLE_HEADING_DATE_EXPECTED', 'Date Expected');
