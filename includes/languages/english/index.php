e4ab7a797d718d99feff42e4d52b7cb6f0547ffc
gburton <oscshops@gmail.com>
2016-04-30 11:27:07 +0100

    Upcoming Products Module

--- includes/languages/english/index.php
+++ includes/languages/english/index.php
@@ -10,8 +10,6 @@
   Released under the GNU General Public License
 */
 
-define('TABLE_HEADING_UPCOMING_PRODUCTS', 'Upcoming Products');
-define('TABLE_HEADING_DATE_EXPECTED', 'Date Expected');
 define('HEADING_TITLE', 'Welcome to ' . STORE_NAME);
 
 define('TEXT_NO_PRODUCTS', 'There are no products available in this category.');
