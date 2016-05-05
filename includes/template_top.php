8e874adc151e393ddacc36e627f89dc5d417a84d
raiwa <info@sarplataygemas.com>
2016-05-05 16:57:24 +0200

    clean template top

--- includes/template_top.php
+++ includes/template_top.php
@@ -31,11 +31,7 @@
 
 <link href="ext/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <!-- font awesome -->
-<<<<<<< HEAD
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
-=======
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
->>>>>>> refs/remotes/origin/master
 
 <link href="custom.css" rel="stylesheet">
 <link href="user.css" rel="stylesheet">
