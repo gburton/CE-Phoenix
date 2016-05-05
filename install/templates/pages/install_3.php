c4856f7d2a899b143485f32c9b0e2155dee9a427
raiwa <info@sarplataygemas.com>
2016-05-05 18:34:04 +0200

    install_update

--- install/templates/pages/install_3.php
+++ install/templates/pages/install_3.php
@@ -23,20 +23,20 @@
 <div class="row">
   <div class="col-sm-9">
     <div class="alert alert-info">
-      <h1>New Installation</h1>
+      <h1>Instalación Nueva</h1>
 
-      <p>This web-based installation routine will correctly setup and configure osCommerce Online Merchant to run on this server.</p>
-      <p>Please follow the on-screen instructions that will take you through the database server, web server, and store configuration options. If help is needed at any stage, please consult the documentation or seek help at the community support forums.</p>
+      <p>Esta rutina de instalación basado en la web va a instalarar y configurar correctamente la tienda osCommerce para ejecutarla en este servidor.</p>
+      <p>Por favor, siga las instrucciones en pantalla que le guiarán a través del servidor de la base de datos, del servidor web, y las opciones de configuración de la tienda. Si necesita ayuda en cualquiera de lo s pasos, por favor consulte la documentación o busque ayuda en los foros de soporte de la comunidad.</p>
     </div>
   </div>
   <div class="col-sm-3">
     <div class="panel panel-default">
       <div class="panel-body">
         <ol>
-          <li class="text-muted">Database Server</li>
-          <li class="text-muted">Web Server</li>
-          <li class="text-success"><strong>Online Store Settings</strong></li>
-          <li class="text-muted">Finished!</li>
+          <li class="text-muted">Servidor de base de la datos</li>
+          <li class="text-muted">Servidor web</li>
+          <li class="text-success"><strong>Configuración de la Tienda Online</strong></li>
+          <li class="text-muted">Terminado!</li>
         </ol>
       </div>
     </div>
@@ -53,54 +53,54 @@
 
     <div class="page-header">
       <p class="inputRequirement pull-right text-right"><span class="fa fa-asterisk inputRequirement"></span> Required information</p>
-      <h2>Online Store Settings</h2>
+      <h2>Configuración de la Tienda Online</h2>
     </div>
 
     <form name="install" id="installForm" action="install.php?step=4" method="post" class="form-horizontal" role="form">
 
       <div class="form-group has-feedback">
-        <label for="storeName" class="control-label col-xs-3">Store Name</label>
+        <label for="storeName" class="control-label col-xs-3">Nombre de la Tienda</label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_STORE_NAME', NULL, 'required aria-required="true" id="storeName" placeholder="Your Store Name"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The name of the online store that is presented to the public.</span>
+          <span class="help-block">El nombre de la tienda online visible al público.</span>
         </div>
       </div>
       
 
       <div class="form-group has-feedback">
-        <label for="ownerName" class="control-label col-xs-3">Store Owner Name</label>
+        <label for="ownerName" class="control-label col-xs-3">Nombre del propietario de la tienda</label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_STORE_OWNER_NAME', NULL, 'required aria-required="true" id="ownerName" placeholder="Your Name"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The name of the store owner that is presented to the public.</span>
+          <span class="help-block">El nombre del dueño de la tienda visible al público.</span>
         </div>
       </div>
       
       <div class="form-group has-feedback">
-        <label for="ownerEmail" class="control-label col-xs-3">Store Owner E-Mail Address</label>
+        <label for="ownerEmail" class="control-label col-xs-3">La Dirección de Correo Electrónico del Propietario de la tienda </label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_STORE_OWNER_EMAIL_ADDRESS', NULL, 'required aria-required="true" id="ownerEmail" placeholder="you@yours.com"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The e-mail address of the store owner that is presented to the public.</span>
+          <span class="help-block">La dirección de correo electrónico del dueño de la tienda visible al público.</span>
         </div>
       </div>
       
       <div class="form-group has-feedback">
-        <label for="adminUsername" class="control-label col-xs-3">Administrator Username</label>
+        <label for="adminUsername" class="control-label col-xs-3">Nombre de usuario del administrador</label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_ADMINISTRATOR_USERNAME', NULL, 'required aria-required="true" id="adminUsername" placeholder="Username"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The administrator username to use for the administration tool.</span>
+          <span class="help-block">El nombre de usuario que el administrador utiliza para la herramienta de administración.</span>
         </div>
       </div>
       
       <div class="form-group has-feedback">
-        <label for="adminPassword" class="control-label col-xs-3">Administrator Password</label>
+        <label for="adminPassword" class="control-label col-xs-3">Contraseña de administrador</label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_ADMINISTRATOR_PASSWORD', NULL, 'required aria-required="true" id="adminPassword"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The password to use for the administrator account.</span>
+          <span class="help-block">La contraseña para la cuenta de administrador.</span>
         </div>
       </div>
 
@@ -108,11 +108,11 @@
   if (osc_is_writable($dir_fs_document_root) && osc_is_writable($dir_fs_document_root . 'admin')) {
 ?>
       <div class="form-group has-feedback">
-        <label for="adminDir" class="control-label col-xs-3">Administration Directory Name</label>
+        <label for="adminDir" class="control-label col-xs-3">Nombre del directorio de administración</label>
         <div class="col-xs-9">
           <?php echo osc_draw_input_field('CFG_ADMIN_DIRECTORY', 'admin', 'required aria-required="true" id="adminDir"'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">This is the directory where the administration section will be installed. You should change this for security reasons.</span>
+          <span class="help-block">Este es el directorio donde se instalará la sección de administración. Debe cambiarlo por razones de seguridad.</span>
         </div>
       </div>
 <?php
@@ -120,15 +120,15 @@
 
 ?>
       <div class="form-group has-feedback">
-        <label for="Zulu" class="control-label col-xs-3">Time Zone</label>
+        <label for="Zulu" class="control-label col-xs-3">Zona horaria</label>
         <div class="col-xs-9">
           <?php echo osc_draw_time_zone_select_menu('CFG_TIME_ZONE'); ?>
           <span class="fa fa-asterisk form-control-feedback inputRequirement"></span>
-          <span class="help-block">The time zone to base the date and time on.</span>
+          <span class="help-block">La zona horaria en la que se basa la fecha y la hora.</span>
         </div>
       </div>
 
-      <p><?php echo osc_draw_button('Continue To Step 4', 'triangle-1-e', null, 'primary', null, 'btn-success btn-block'); ?></p>
+      <p><?php echo osc_draw_button('Continuar con el paso 4', 'triangle-1-e', null, 'primary', null, 'btn-success btn-block'); ?></p>
 
       <?php
       foreach ( $_POST as $key => $value ) {
@@ -144,11 +144,11 @@
   <div class="col-xs-12 col-sm-pull-9 col-sm-3">
     <div class="panel panel-success">
       <div class="panel-heading">
-        Step 3: Online Store Settings
+        Paso 3: Configuración de la Tienda online
       </div>
       <div class="panel-body">
-        <p>Here you can define the name of your online store and the contact information for the store owner.</p>
-        <p>The administrator username and password are used to log into the protected administration tool section.</p>
+        <p>Aquí puede definir el nombre de su tienda online y la información de contacto del dueño de la tienda.</p>
+        <p>El nombre de usuario y contraseña de administrador se utilizan para iniciar sesión en la sección de herramientas de administración protegida.</p>
       </div>
     </div>
   </div>
