67f4c69b4c1f250b90b787e6bb8b82b6345ced49
gburton <oscshops@gmail.com>
2016-05-12 19:16:39 +0100

    Introduce Modularity to Index->Nested
    aka Categories and Sub Categories

--- index.php
+++ index.php
@@ -49,60 +49,9 @@
   }
 ?>
 
-<?php
-if (tep_not_null($category['categories_description'])) {
-  echo '<div class="well well-sm">' . $category['categories_description'] . '</div>';
-}  
-?>
-
 <div class="contentContainer">
-  <div class="contentText">
-    <div class="row">
-<?php
-    if (isset($cPath) && strpos('_', $cPath)) {
-// check to see if there are deeper categories within the current category
-      $category_links = array_reverse($cPath_array);
-      for($i=0, $n=sizeof($category_links); $i<$n; $i++) {
-        $categories_query = tep_db_query("select count(*) as total from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" . (int)$category_links[$i] . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "'");
-        $categories = tep_db_fetch_array($categories_query);
-        if ($categories['total'] < 1) {
-          // do nothing, go through the loop
-        } else {
-          $categories_query = tep_db_query("select c.categories_id, cd.categories_name, c.categories_image, c.parent_id from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" . (int)$category_links[$i] . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "' order by sort_order, cd.categories_name");
-          break; // we've found the deepest category the customer is in
-        }
-      }
-    } else {
-      $categories_query = tep_db_query("select c.categories_id, cd.categories_name, c.categories_image, c.parent_id from " . TABLE_CATEGORIES . " c, " . TABLE_CATEGORIES_DESCRIPTION . " cd where c.parent_id = '" . (int)$current_category_id . "' and c.categories_id = cd.categories_id and cd.language_id = '" . (int)$languages_id . "' order by sort_order, cd.categories_name");
-    }
-
-    echo '<div itemscope itemtype="http://schema.org/ItemList">';
-    echo '  <meta itemprop="itemListOrder" content="http://schema.org/ItemListUnordered" />';
-    echo '  <meta itemprop="name" content="' . $category['categories_name']  . '" />';
-
-    while ($categories = tep_db_fetch_array($categories_query)) {
-      $cPath_new = tep_get_path($categories['categories_id']);
-      echo '<div class="col-xs-6 col-sm-4">';
-      echo '  <div class="text-center">';
-      echo '    <a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '">' . tep_image(DIR_WS_IMAGES . $categories['categories_image'], $categories['categories_name'], SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '</a>';
-      echo '    <div class="caption text-center">';
-      echo '      <h5><a href="' . tep_href_link(FILENAME_DEFAULT, $cPath_new) . '"><span itemprop="itemListElement">' . $categories['categories_name'] . '</span></a></h5>';
-      echo '    </div>';
-      echo '  </div>';
-      echo '</div>';
-    }
-
-    echo '</div>';
-
-// needed for the new products module shown below
-    $new_products_category_id = $current_category_id;
-?>
-      </div>
-
-    <br />
-
-<?php include(DIR_WS_MODULES . FILENAME_NEW_PRODUCTS); ?>
-
+  <div class="row">
+    <?php echo $oscTemplate->getContent('index_nested'); ?>
   </div>
 </div>
 
