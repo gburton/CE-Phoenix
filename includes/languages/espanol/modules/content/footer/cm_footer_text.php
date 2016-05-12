bd83269405e784a0eb79a4d4bc52afb7b5dd3c26
raiwa <info@sarplataygemas.com>
2016-05-12 17:49:41 +0200

    Fix wrong constant in cm_footer_text

--- includes/languages/espanol/modules/content/footer/cm_footer_text.php
+++ includes/languages/espanol/modules/content/footer/cm_footer_text.php
@@ -13,7 +13,7 @@
   define('MODULE_CONTENT_FOOTER_TEXT_TITLE', 'Bloque de texto');
   define('MODULE_CONTENT_FOOTER_TEXT_DESCRIPTION', 'Añade un bloque de Texto al pie de página de su sitio web.');
  
-  define('MODULE_CONTENT_FOOTER_TEXT_DEVOLUTION', 'Acerca de nosotros');
+  define('MODULE_CONTENT_FOOTER_TEXT_HEADING_TITLE', 'Acerca de nosotros');
   
   
   define('MODULE_CONTENT_FOOTER_TEXT_TEXT', '<p>Lorem Ipsum es simplemente un texto de relleno usado por impresores y tipógrafos. Lorem Ipsum es el texto de relleno estándar de la industria desde el siglo XVI, cuando un impresor desconocido tomó una galerada de tipos y los combinó para hacer un libro de tipografías.</p>');
