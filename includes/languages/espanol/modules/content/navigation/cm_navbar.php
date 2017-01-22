610a489c8202f35280f90eb69a7c3f401ed19efb
gburton <oscshops@gmail.com>
2017-01-22 17:02:01 +0000

    Shop Modules Content Navigation

--- includes/languages/espanol/modules/content/navigation/cm_navbar.php
+++ includes/languages/espanol/modules/content/navigation/cm_navbar.php
@@ -5,11 +5,11 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2016 osCommerce
+  Copyright (c) 2017 osCommerce
 
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_NAVBAR_TITLE', 'Navigation Bar');
-  define('MODULE_CONTENT_NAVBAR_DESCRIPTION', 'Show the Navigation Bar on your site. <div class="secWarning">This module has a number of Sub Modules which must also be installed.<br><br>Admin > Modules > Navbar Modules</div>');
+  define('MODULE_CONTENT_NAVBAR_TITLE', 'Barra de Navegación');
+  define('MODULE_CONTENT_NAVBAR_DESCRIPTION', 'Mostrar la barra de navegación en su tienda.');
   
