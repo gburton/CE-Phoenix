7507acc8a52d4c377540a573919262665fec107c
katapofatico <katapofatico@gmail.com>
2017-02-01 14:16:28 +0100

    cm_in_new_products.php translated to Spanish

--- includes/languages/espanol/modules/content/index_nested/cm_in_new_products.php
+++ includes/languages/espanol/modules/content/index_nested/cm_in_new_products.php
@@ -10,11 +10,11 @@
   Released under the GNU General Public License
 */
 
-  const MODULE_CONTENT_IN_NEW_PRODUCTS_TITLE       = 'New Products';
-  const MODULE_CONTENT_IN_NEW_PRODUCTS_DESCRIPTION = 'Shows the "New Products" module on your Categories page.';
+  const MODULE_CONTENT_IN_NEW_PRODUCTS_TITLE       = 'Nuevos productos';
+  const MODULE_CONTENT_IN_NEW_PRODUCTS_DESCRIPTION = 'Muestra el módulo "Nuevos productos" en la página de categoría.';
   
-  const MODULE_CONTENT_IN_NEW_PRODUCTS_HEADING     = 'New Products For %s';
+  const MODULE_CONTENT_IN_NEW_PRODUCTS_HEADING     = 'Nuevos productos en %s';
   
-  const MODULE_CONTENT_IN_NEW_PRODUCTS_BUTTON_VIEW = 'View';
-  const MODULE_CONTENT_IN_NEW_PRODUCTS_BUTTON_BUY  = 'Buy';
+  const MODULE_CONTENT_IN_NEW_PRODUCTS_BUTTON_VIEW = 'Ver';
+  const MODULE_CONTENT_IN_NEW_PRODUCTS_BUTTON_BUY  = 'Comprar';
   
