c23179cacdc467b6d42ceb8d5c75d5f67782a59b
katapofatico <katapofatico@gmail.com>
2017-02-01 14:04:01 +0100

    cm_i_customer_greeting.php translated

--- includes/languages/espanol/modules/content/index/cm_i_customer_greeting.php
+++ includes/languages/espanol/modules/content/index/cm_i_customer_greeting.php
@@ -10,8 +10,8 @@
   Released under the GNU General Public License
 */
 
-  const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Customer Greeting';
-  const MODULE_CONTENT_CUSTOMER_GREETING_DESCRIPTION = 'Shows the "Customer Greeting" module on your Index page.';
+  const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Saludo al cliente';
+  const MODULE_CONTENT_CUSTOMER_GREETING_DESCRIPTION = 'Muestra el módulo "Saludo al cliente" en tu página index.';
 
-  const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = 'Welcome back <strong>%s!</strong> Would you like to see which <a role="button" class="btn btn-info btn-xs" href="%s">new products</a> are available to purchase?';
-  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = 'Welcome <strong>Guest!</strong> Would you like to <a role="button" class="btn btn-info btn-xs" href="%s">log yourself in</a> or would you prefer to <a role="button" class="btn btn-info btn-xs" href="%s">create an account</a>';
+  const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = '¡Bienvenid@ de nuevo <strong>%s</strong>! ¿Le gustaría ver qué <a role="button" class="btn btn-info btn-xs" href="%s">nuevos productos</a> están disponibles para comprar?';
+  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = '¡Bienvenido <strong>Invitado</strong>! ¿Le gustaría <a role="button" class="btn btn-info btn-xs" href="%s">iniciar su sesión</a> o prefiere <a role="button" class="btn btn-info btn-xs" href="%s">crear una cuenta</a>?';
