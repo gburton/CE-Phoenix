c96a9bd99be6b6fa061f8f69f66ae85445035736
Piernas <Piernas@users.noreply.github.com>
2017-02-02 01:36:58 +0100

    Update log.php

--- includes/apps/paypal/languages/espanol/admin/log.php
+++ includes/apps/paypal/languages/espanol/admin/log.php
@@ -1,18 +1,18 @@
-table_heading_action = Action
+table_heading_action = Acción
 table_heading_ip = IP
-table_heading_customer = Customer
-table_heading_date = Date
+table_heading_customer = Cliente
+table_heading_date = Fecha
 
-guest = Guest
+guest = Invitado
 
-no_entries = No log entries found.
+no_entries = No hay entradas en el registro.
 
-listing_number_of_log_entries = Displaying <strong>%d</strong> to <strong>%d</strong> (of <strong>%d</strong> entries)
+listing_number_of_log_entries = Mostrando de <strong>%d</strong> a <strong>%d</strong> (de <strong>%d</strong> entradas)
 
-dialog_delete_title = Delete All Log Entries?
-dialog_delete_body = Are you sure you want to delete all log entries?
+dialog_delete_title = ¿Eliminar todas las entradas del registro?
+dialog_delete_body = ¿Está seguro que quiere eliminar todas las entradas del registro?
 
-table_heading_entries_request = Request
-table_heading_entries_response = Response
+table_heading_entries_request = Solicitud
+table_heading_entries_response = Respuesta
 
-alert_delete_success = Log entries have been successfully deleted.
+alert_delete_success = Las entradas del registro han sido eliminadas con éxito.
