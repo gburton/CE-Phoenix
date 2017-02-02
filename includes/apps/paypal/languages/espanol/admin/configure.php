c645c9b279cf4efb64c4983f87554f11ece8b3ad
Piernas <Piernas@users.noreply.github.com>
2017-02-02 01:21:40 +0100

    Update configure.php

--- includes/apps/paypal/languages/espanol/admin/configure.php
+++ includes/apps/paypal/languages/espanol/admin/configure.php
@@ -1,9 +1,10 @@
 section_general = General
 section_more = +
 
-dialog_uninstall_title = Uninstall Module?
-dialog_uninstall_body = Are you sure you want to uninstall this module?
+dialog_uninstall_title = ¿Desinstalar el módulo?
+dialog_uninstall_body = ¿Está seguro que quiere desinstalar este módulo?
 
-alert_module_install_success = Module has been successfully installed.
-alert_module_uninstall_success = Module has been successfully uninstalled.
-alert_cfg_saved_success = Configuration parameters have been successfully saved.
+alert_module_install_success = El módulo ha sido instalado con éxito.
+alert_module_uninstall_success = El módulo ha sido desinstalado con éxito.
+alert_cfg_saved_success = Los parámetros de configuración han sido guardados con éxito.
+n
