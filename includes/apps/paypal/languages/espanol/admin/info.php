6ca3bc989cc8fce1c00cdd9832bd38237cda6f35
Piernas <Piernas@users.noreply.github.com>
2017-02-02 01:33:18 +0100

    Update info.php

--- includes/apps/paypal/languages/espanol/admin/info.php
+++ includes/apps/paypal/languages/espanol/admin/info.php
@@ -1,13 +1,13 @@
-online_documentation_title = Online Documentation
+online_documentation_title = Documentación en línea
 
-online_documentation_body = <p>Online documentation is available at the osCommerce Library website:</p>
+online_documentation_body = <p>La Documentación en línea está disponible en el sitio web osCommerce Library:</p>
 <p>:button_online_documentation</p>
 
-button_online_documentation = Online Documentation
+button_online_documentation = Documentación en línea
 
-online_forum_title = Online Forum
+online_forum_title = Foro en línea
 
-online_forum_body = <p>Support enquiries can be posted at the osCommerce Support Forum PayPal Channel:</p>
+online_forum_body = <p>Las peticiones de soporte pueden ser enviadas al canal de PayPal en el foro de Soporte de osCommerce:</p>
 <p>:button_online_forum</p>
 
-button_online_forum = osCommerce Support Forums
+button_online_forum = Foros en línea de soporte de osCommerce
