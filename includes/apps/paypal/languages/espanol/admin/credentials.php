50b4b12f0a77b6043786b63f1b70a0019f59fe9c
Piernas <Piernas@users.noreply.github.com>
2017-02-02 01:28:29 +0100

    Update credentials.php

--- includes/apps/paypal/languages/espanol/admin/credentials.php
+++ includes/apps/paypal/languages/espanol/admin/credentials.php
@@ -1,32 +1,32 @@
 section_paypal = PayPal
 section_payflow = PayFlow
 
-paypal_live_title = Live Credentials
-paypal_live_api_username = API Username
-paypal_live_api_password = API Password
-paypal_live_api_signature = API Signature
-paypal_live_email_address = PayPal E-Mail Address
-paypal_live_primary_email_address = Primary E-Mail Address
-paypal_live_primary_email_address_desc = The primary e-mail address only needs to be entered if it differs to the e-mail address entered above. This must be the e-mail address of the account marked as primary in your PayPal account settings.
+paypal_live_title = Credenciales Live
+paypal_live_api_username = Usuario API
+paypal_live_api_password = Contraseña API
+paypal_live_api_signature = Firma API
+paypal_live_email_address = Dirección de correo electrónico de PayPal
+paypal_live_primary_email_address = Dirección de correo Principal
+paypal_live_primary_email_address_desc = La dirección de correo electrónico proncipal sólo debe ser introducida si es diferente to a la dirección de correo introducida más arriba. Debe ser la dirección de correo de la cuenta marcada como primaria en la configuración de su cuenta de PayPal.
 
-paypal_sandbox_title = Sandbox Credentials
-paypal_sandbox_api_username = API Username
-paypal_sandbox_api_password = API Password
-paypal_sandbox_api_signature = API Signature
-paypal_sandbox_email_address = PayPal E-Mail Address
-paypal_sandbox_primary_email_address = Primary E-Mail Address
-paypal_sandbox_primary_email_address_desc = The primary e-mail address only needs to be entered if it differs to the e-mail address entered above. This must be the e-mail address of the account marked as primary in your PayPal account settings.
+paypal_sandbox_title = Credenciales Sandbox
+paypal_sandbox_api_username = Usuario API
+paypal_sandbox_api_password = Contraseña API
+paypal_sandbox_api_signature = Firma API
+paypal_sandbox_email_address = Dirección de correo electrónico de PayPal
+paypal_sandbox_primary_email_address = Dirección de correo electrónico Principal
+paypal_sandbox_primary_email_address_desc = La dirección de correo electrónico proncipal sólo debe ser introducida si es diferente to a la dirección de correo introducida más arriba. Debe ser la dirección de correo de la cuenta marcada como primaria en la configuración de su cuenta de PayPal.
 
-payflow_live_title = Live Credentials
+payflow_live_title = Credenciales Live
 payflow_live_partner = Partner
 payflow_live_merchant_login = Merchant Login
-payflow_live_user = User
-payflow_live_password = Password
+payflow_live_user = Usuario
+payflow_live_password = Contraseña
 
-payflow_sandbox_title = Sandbox Credentials
+payflow_sandbox_title = Credenciales Sandbox
 payflow_sandbox_partner = Partner
 payflow_sandbox_merchant_login = Merchant Login
-payflow_sandbox_user = User
-payflow_sandbox_password = Password
+payflow_sandbox_user = Usuario
+payflow_sandbox_password = Contraseña
 
-alert_credentials_saved_success = Account credentials have been successfully saved.
+alert_credentials_saved_success = Las credenciales de la Cuenta han sido almacenadas con éxito.
