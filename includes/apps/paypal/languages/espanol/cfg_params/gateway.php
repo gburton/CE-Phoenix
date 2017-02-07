75c7c1e916e48514f0e8d5c7019225c0e6dc878c
Piernas <Piernas@users.noreply.github.com>
2017-02-07 20:34:59 +0100

    Update gateway.php

--- includes/apps/paypal/languages/espanol/cfg_params/gateway.php
+++ includes/apps/paypal/languages/espanol/cfg_params/gateway.php
@@ -1,5 +1,6 @@
-cfg_gateway_title = Gateway
-cfg_gateway_desc = Set this to the gateway to use to process payments through.
+cfg_gateway_title = Puerta de enlace
+cfg_gateway_desc = Establezca la puerta de enlace a emplear para procesar pagos.
 
 cfg_gateway_paypal = PayPal
 cfg_gateway_payflow = Payflow
+ 
