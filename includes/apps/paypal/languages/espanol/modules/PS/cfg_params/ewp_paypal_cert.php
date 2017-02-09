34f83f58af7a18aa30b169af4a02bd0ae4950133
Piernas <Piernas@users.noreply.github.com>
2017-02-09 19:00:08 +0100

    Update ewp_paypal_cert.php

--- includes/apps/paypal/languages/espanol/modules/PS/cfg_params/ewp_paypal_cert.php
+++ includes/apps/paypal/languages/espanol/modules/PS/cfg_params/ewp_paypal_cert.php
@@ -1,2 +1,2 @@
-cfg_ps_ewp_paypal_cert_title = PayPal Public Certificate
-cfg_ps_ewp_paypal_cert_desc = The location and filename of the PayPal Public Certificate to use for encrypting the parameters.
+cfg_ps_ewp_paypal_cert_title = Certificado público de PayPal
+cfg_ps_ewp_paypal_cert_desc = Ubicación y nombre de archivo del Certificado público de PayPal a usar para encriptar los parámetros.
