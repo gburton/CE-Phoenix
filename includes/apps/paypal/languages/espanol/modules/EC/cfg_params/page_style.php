d2a2cb098deaede96973f191db9f6b554a3e6ac4
Lionel <admin@nukua.es>
2017-02-10 11:23:07 +0100

    Update page_style.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/EC/cfg_params/page_style.php
+++ includes/apps/paypal/languages/espanol/modules/EC/cfg_params/page_style.php
@@ -1,2 +1,2 @@
-cfg_ec_page_style_title = Page Style
-cfg_ec_page_style_desc = Add the Page Style defined in your PayPal account profile to apply the style to the checkout flow.
+cfg_ec_page_style_title = Estilo de Página
+cfg_ec_page_style_desc = Añada el estilo de página definido en su perfil de cuenta de PayPal para aplicar el estilo al flujo de "Checkout".
