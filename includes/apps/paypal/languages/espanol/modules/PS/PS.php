247aee6f454f2ddb3b36ac6a56775811731019b6
Lionel <admin@nukua.es>
2017-02-10 10:32:49 +0100

    Update PS.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/PS/PS.php
+++ includes/apps/paypal/languages/espanol/modules/PS/PS.php
@@ -1,22 +1,22 @@
-module_ps_title = PayPal Payments Standard
-module_ps_short_title = Payments Standard
-module_ps_public_title = PayPal (including Credit and Debit Cards)
+module_ps_title = PayPal Pago Standard
+module_ps_short_title = Pago Standard
+module_ps_public_title = PayPal (incl. Tarjetas de crédito y débito)
 
-module_ps_legacy_admin_app_button = Manage App
+module_ps_legacy_admin_app_button = Administrar App
 
 module_ps_introduction = <ul>
-  <li>Accept credit cards and PayPal on your online store.</li>
-  <li>Simplified PCI compliance standards.</li>
-  <li>Accept multiple currencies worldwide.</li>
-  <li>Optimized mobile checkout experience.</li>
+  <li>Acepte tarjetas de crédito y PayPal en su tienda online.</li>
+  <li>Normas de cumplimiento PCI simplificadas.</li>
+  <li>Aceptar múltiples monedas en todo el mundo.</li>
+  <li>Optimización de la experiencia de pago móvil.</li>
 </ul>
 
-<p>PayPal handles the payment acceptance experience and returns the customer to your store after payment has been made.</p>
-<p>For new buyers, signing up for a PayPal account is optional meaning customers can complete their payments first and then decide to save their information in a PayPal account for future purchases.</p>
+<p>PayPal gestiona la experiencia de aceptación de pagos y devuelve al cliente a su tienda después de haber realizado el pago.</p>
+<p>Para los nuevos compradores, la creación de una cuenta PayPal es opcional, lo que significa que los clientes pueden completar primero sus pagos y luego decidir si quieren guardar sus datos en una cuenta PayPal para futuras compras.</p>
 
-module_ps_error_curl = This module requires cURL to be enabled in PHP and will not load until it has been enabled on this webserver.
-module_ps_error_credentials = This module will not load until the Seller E-Mail Address parameter has been configured. Please edit and configure the settings of this module.
+module_ps_error_curl = Este módulo requiere que cURL esté habilitado en PHP y no se cargará hasta que se haya habilitado en este servidor web.
+module_ps_error_credentials = Este módulo no se cargará hasta que se haya configurado el parámetro dirección de correo electrónico de vendedor (Seller E-Mail Address). Por favor, edite y configure los prámetros de este módulo.
 
-module_ps_button_return_to_store = Back to :storename
+module_ps_button_return_to_store = Volver a :storename
 
-module_ps_error_invalid_transaction = Could not verify the PayPal transaction. Please try again.
+module_ps_error_invalid_transaction = No se ha podido verificar la transacción PayPal. Por favor, inténtelo de nuevo.
