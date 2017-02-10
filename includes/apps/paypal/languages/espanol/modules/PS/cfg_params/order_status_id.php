c28afb3bb55eb106a4500a6600f7eaa8e9460323
Lionel <admin@nukua.es>
2017-02-10 10:42:17 +0100

    Update order_status_id.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/PS/cfg_params/order_status_id.php
+++ includes/apps/paypal/languages/espanol/modules/PS/cfg_params/order_status_id.php
@@ -1,4 +1,4 @@
-cfg_ps_order_status_id_title = Order Status
-cfg_ps_order_status_id_desc = Set this to the order status level that is assigned to new orders.
+cfg_ps_order_status_id_title = Estado de Pedidos
+cfg_ps_order_status_id_desc = Establezca esto en el nivel de estado del pedido que se asigna a los nuevos pedidos.
 
 cfg_ps_order_status_id_default = -- Store Default Order Status --
