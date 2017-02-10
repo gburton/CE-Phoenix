d07b71ec524b10bd7e5b254b336d7413a3be375a
Lionel <admin@nukua.es>
2017-02-10 11:09:33 +0100

    Update tab.php
    spanish

--- includes/apps/paypal/languages/espanol/hooks/admin/orders/tab.php
+++ includes/apps/paypal/languages/espanol/hooks/admin/orders/tab.php
@@ -1,25 +1,25 @@
-button_dialog_capture = Capture &hellip;
-button_details = Details
-button_dialog_refund = Refund &hellip;
-button_view_at_paypal = View at PayPal
-button_dialog_void = Void &hellip;
+button_dialog_capture = Capturar &hellip;
+button_details = Detalles
+button_dialog_refund = Reembolsar &hellip;
+button_view_at_paypal = Ver en PayPal
+button_dialog_void = Anular &hellip;
 
 tab_title = PayPal
 
-dialog_capture_title = Capture Payment
-dialog_capture_body = The full or a partial amount can be captured.
-dialog_capture_amount_field_title = Amount:
-dialog_capture_last_capture_field_title = This is the last capture (release <span id="ppCaptureVoidedValue"></span> :currency back to the customer).
-dialog_capture_button_capture = Capture Amount
-dialog_capture_button_cancel = Cancel
+dialog_capture_title = Capturar Pago
+dialog_capture_body = Se puede capturar la cantidad total o parcial.
+dialog_capture_amount_field_title = Cantidad:
+dialog_capture_last_capture_field_title = Esta es la última captura (release <span id="ppCaptureVoidedValue"></span> :currency back to the customer).
+dialog_capture_button_capture = Capturar Cantidad
+dialog_capture_button_cancel = Cancelar
 
-dialog_void_title = Void Payment
-dialog_void_body = Are you sure you want to void the authorized payment?
-dialog_void_button_void = Void Payment
-dialog_void_button_cancel = Cancel
+dialog_void_title = Anular Pago
+dialog_void_body = ¿Seguro que desea anular el pago autorizado??
+dialog_void_button_void = Anular Pago
+dialog_void_button_cancel = Cancelar
 
-dialog_refund_title = Refund Payment
-dialog_refund_body = Please select which refunds to perform:
-dialog_refund_payment_title = Refund :amount to customer.
-dialog_refund_button_refund = Refund Payment
-dialog_refund_button_cancel = Cancel
+dialog_refund_title = Reembolsar Pago
+dialog_refund_body = Seleccione los reembolsos que desea realizar:
+dialog_refund_payment_title = Reembolsar :amount to customer.
+dialog_refund_button_refund = Reembolsar Pago
+dialog_refund_button_cancel = Cancelar
