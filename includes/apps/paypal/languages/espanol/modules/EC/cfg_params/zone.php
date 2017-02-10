e33a46166c29ca53e4d8f5a660781891e61c9d0a
Lionel <admin@nukua.es>
2017-02-10 11:26:05 +0100

    Update zone.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/EC/cfg_params/zone.php
+++ includes/apps/paypal/languages/espanol/modules/EC/cfg_params/zone.php
@@ -1,4 +1,4 @@
-cfg_ec_zone_title = Payment Zone
-cfg_ec_zone_desc = Enable this payment module globally or limit it to customers shipping to the selected zone only.
+cfg_ec_zone_title = Zona de Pago
+cfg_ec_zone_desc = Habilite este módulo de pago a nivel mundial (Global) o limite a clientes de la zona seleccionada.
 
 cfg_ec_zone_global = -- Global --
