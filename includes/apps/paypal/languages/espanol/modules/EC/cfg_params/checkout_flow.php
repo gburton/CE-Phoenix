6954ca35efc4288957d41479b6c299e7035a7712
Lionel <admin@nukua.es>
2017-02-10 11:19:00 +0100

    Update checkout_flow.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/EC/cfg_params/checkout_flow.php
+++ includes/apps/paypal/languages/espanol/modules/EC/cfg_params/checkout_flow.php
@@ -1,5 +1,5 @@
 cfg_ec_checkout_flow_title = Checkout Flow
-cfg_ec_checkout_flow_desc = Set this to Default to have PayPal automatically choose the best checkout flow to use, or to use the new In-Context checkout flow (beta).<br /><br /><em>In-Context only works in certain conditions and automatically disables Instant Update.</em>
+cfg_ec_checkout_flow_desc = Configure esto como Predeterminado para que PayPal elija automáticamente el mejor flujo de pago o para usar el nuevo flujo de pago "In-Context"(beta).<br /><br /><em>"In-Context" sólo funciona en ciertas condiciones y Desactiva automáticamente "Instant Update".</em>
 
 cfg_ec_checkout_flow_default = Default
 cfg_ec_checkout_flow_in_context = In-Context (Beta)
