ef99350d738056ed78a58026da5fc097c9b3102b
Lionel <admin@nukua.es>
2017-02-10 11:20:40 +0100

    Update checkout_image.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/EC/cfg_params/checkout_image.php
+++ includes/apps/paypal/languages/espanol/modules/EC/cfg_params/checkout_image.php
@@ -1,5 +1,5 @@
-cfg_ec_checkout_image_title = PayPal Express Checkout Button
-cfg_ec_checkout_image_desc = Set this to Dynamic if the Express Checkout button is used with a campaign.
+cfg_ec_checkout_image_title = Botón PayPal Express Checkout
+cfg_ec_checkout_image_desc = Establezca esta opción en "Dinámico" si el botón "Express Checkout" se utiliza con una campaña.
 
-cfg_ec_checkout_image_dynamic = Dynamic
-cfg_ec_checkout_image_static = Static
+cfg_ec_checkout_image_dynamic = Dinámico
+cfg_ec_checkout_image_static = Estático
