dc464c7e11f6b9cc7f4baa32e808deeecf8758f6
Lionel <admin@nukua.es>
2017-02-10 10:57:00 +0100

    Update LOGIN.php
    spanish

--- includes/apps/paypal/languages/espanol/modules/LOGIN/LOGIN.php
+++ includes/apps/paypal/languages/espanol/modules/LOGIN/LOGIN.php
@@ -1,22 +1,22 @@
-module_login_title = Log In with PayPal
-module_login_short_title = Log In
+module_login_title = Iniciar sesión con PayPal
+module_login_short_title = Iniciar sesión
 
-module_login_legacy_admin_app_button = Manage App
+module_login_legacy_admin_app_button = Administrar App
 
 module_login_introduction = <ul>
-  <li>Accept credit cards and PayPal on your online store.</li>
-  <li>Simplified PCI compliance standards.</li>
-  <li>Accept multiple currencies worldwide.</li>
-  <li>Optimized mobile checkout experience.</li>
+  <li>Acepte tarjetas de crédito y PayPal en su tienda online.</li>
+  <li>Normas de cumplimiento PCI simplificadas.</li>
+  <li>Aceptar múltiples monedas en todo el mundo.</li>
+  <li>Optimización de la experiencia de pago móvil.</li>
 </ul>
 
-<p>PayPal handles the payment acceptance experience and returns the customer to your store after payment has been made.</p>
-<p>For new buyers, signing up for a PayPal account is optional meaning customers can complete their payments first and then decide to save their information in a PayPal account for future purchases.</p>
+<p>PayPal gestiona la experiencia de aceptación de pagos y devuelve al cliente a su tienda después de haber realizado el pago.</p>
+<p>Para los nuevos compradores, la creación de una cuenta PayPal es opcional, lo que significa que los clientes pueden completar primero sus pagos y luego decidir si quieren guardar sus datos en una cuenta PayPal para futuras compras.</p>
 
-module_login_error_curl = This module requires cURL to be enabled in PHP and will not load until it has been enabled on this webserver.
-module_login_error_credentials = This module will not load until the Client ID and Secret parameters have been configured. Please edit and configure the settings of the PayPal App.
+module_login_error_curl = Este módulo requiere que cURL esté habilitado en PHP y no se cargará hasta que se haya habilitado en este servidor web.
+module_login_error_credentials = Este módulo no se cargará hasta que se haya configurado el parámetro "Id" de cliente y "Secret". Por favor, edite y configure los prámetros de este módulo.
 
-module_login_template_title = Log In with PayPal
-module_login_template_content = Have a PayPal account? Securely log in with PayPal to shop even faster!
-module_login_template_sandbox_alert = Test Mode: The Sandbox server is currently selected.
-module_login_language_locale = en-us
+module_login_template_title = Iniciar sesión con PayPal
+module_login_template_content = ¿Tiene una cuenta PayPal? Inicie sesión de forma segura con PayPal para comprar aún más rápido.
+module_login_template_sandbox_alert = Modo de prueba: El servidor "Sandbox" está actualmente seleccionado.
+module_login_language_locale = es-es
