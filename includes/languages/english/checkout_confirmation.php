8d268fa9e8004afbb9f2580ff25572095d7bd7e6
ArtcoInc <sales@artcoinc.com>
2018-03-18 16:05:56 -0700

    Update checkout_confirmation.php

--- includes/languages/english/checkout_confirmation.php
+++ includes/languages/english/checkout_confirmation.php
@@ -30,3 +30,9 @@ define('HEADING_ORDER_COMMENTS', 'Comments About Your Order');
 define('TEXT_EDIT', 'Edit');
 
 define('IMAGE_BUTTON_FINALISE_ORDER', 'Finalise and Pay For my Order');
+
+/* definations to display stock on hand quantity in shopping cart */
+define('TEXT_NOT_INSTOCK_CAN_CHECKOUT', ' At the moment this product is not in stock - it will be ordered for you!');
+define('TEXT_NOT_INSTOCK_CANT_CHECKOUT', ' At the moment this product is not in stock - please remove it from your shopping cart!');
+define('TEXT_NOT_ENOUGH_CAN_CHECKOUT', ' There are only %d pieces in stock at the moment - the rest will be ordered for you!');
+define('TEXT_NOT_ENOUGH_CANT_CHECKOUT', ' There are only %d pieces in stock at the moment - please reduce the quantity for your order!');
