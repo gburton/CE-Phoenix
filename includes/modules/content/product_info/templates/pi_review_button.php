3c82890e63dbb647e0ede849eb4d9f19d8abb0b4
gburton <oscshops@gmail.com>
2018-03-26 22:35:53 +0100

    Fix Button Display
    Default to XS so buttons sits side by side.  Clearfix the first button to solve long description or long gallery.

--- includes/modules/content/product_info/templates/pi_review_button.php
+++ includes/modules/content/product_info/templates/pi_review_button.php
@@ -1,4 +1,5 @@
-<div class="col-sm-<?php echo $content_width; ?> review-button">
+<div class="clearfix"></div>
+<div class="col-xs-<?php echo $content_width; ?> cm-pi-review-button">
   <?php 
   echo tep_draw_button(MODULE_CONTENT_PI_REVIEW_BUTTON_TEXT, 'fas fa-comment-alt', tep_href_link('product_reviews.php', tep_get_all_get_params()));
   ?>
