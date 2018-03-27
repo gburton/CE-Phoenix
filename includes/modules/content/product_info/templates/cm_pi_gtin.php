d80b5eaf0b601ed9cd0c4167163686592273c440
gburton <oscshops@gmail.com>
2018-03-27 10:03:24 +0100

    Remove Schema

--- includes/modules/content/product_info/templates/cm_pi_gtin.php
+++ includes/modules/content/product_info/templates/cm_pi_gtin.php
@@ -1,7 +1,7 @@
 <div class="col-sm-<?php echo $content_width; ?> cm-pi-gtin">
   <ul class="list-group">
     <li class="list-group-item">
-      <span itemprop="gtin<?php echo MODULE_CONTENT_PRODUCT_INFO_GTIN_LENGTH; ?>" class="badge"><?php echo $gtin; ?></span>
+      <span class="badge"><?php echo $gtin; ?></span>
       <?php echo MODULE_CONTENT_PRODUCT_INFO_GTIN_PUBLIC_TITLE; ?>
     </li>
   </ul>
