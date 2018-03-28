3da26d0acab7475ae435d4a5b31e6a22e32f7847
gburton <oscshops@gmail.com>
2018-03-28 11:43:27 +0100

    Class Call

--- includes/modules/content/index_nested/templates/cm_in_category_listing.php
+++ includes/modules/content/index_nested/templates/cm_in_category_listing.php
@@ -1,20 +1,19 @@
 <div class="col-sm-<?php echo $content_width; ?> cm-in-category-listing">
   <div itemscope itemtype="http://schema.org/ItemList">
     <meta itemprop="itemListOrder" content="http://schema.org/ItemListUnordered" />
-    <meta itemprop="name" content="<?php echo $category['categories_name']; ?>" />
+    <meta itemprop="name" content="<?php echo $category_name; ?>" />
     
     <?php
-    while ($categories = tep_db_fetch_array($categories_query)) {
-      $cPath_new = tep_get_path($categories['categories_id']);
+    foreach ($category_array as $k => $v) {
       echo '<div class="col-sm-' . $category_width . '">';
-      echo '  <div class="text-center">';
-      echo '    <a href="' . tep_href_link('index.php', $cPath_new) . '">' . tep_image('images/' . $categories['categories_image'], htmlspecialchars($categories['categories_name']), SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '</a>';
-      echo '    <div class="caption text-center">';
-      echo '      <h5 class="h3"><a href="' . tep_href_link('index.php', $cPath_new) . '"><span itemprop="itemListElement">' . $categories['categories_name'] . '</span></a></h5>';
-      echo '    </div>';
-      echo '  </div>';
+      echo   '<div class="text-center">';
+      echo     '<a href="' . tep_href_link('index.php', 'cPath=' . $v['id']) . '">' . tep_image('images/' . $v['image'], htmlspecialchars($v['title']), SUBCATEGORY_IMAGE_WIDTH, SUBCATEGORY_IMAGE_HEIGHT) . '</a>';
+      echo     '<div class="caption text-center">';
+      echo       '<h5 class="h3"><a href="' . tep_href_link('index.php', 'cPath=' . $v['id']) . '"><span itemprop="itemListElement">' . $v['title'] . '</span></a></h5>';
+      echo     '</div>';
+      echo   '</div>';
       echo '</div>';
     }
-    ?>    
-  </div>    
+    ?>
+  </div>
 </div>
