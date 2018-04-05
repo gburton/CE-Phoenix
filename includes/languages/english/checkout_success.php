86a34f44fcd6815b6b596e4c3ecc70baa177eec9
gburton <oscshops@gmail.com>
2018-04-05 13:38:13 +0100

    Modularise Page Heading
    Includes Quick Layout Fix for two existing modules.

--- includes/languages/english/checkout_success.php
+++ includes/languages/english/checkout_success.php
@@ -13,6 +13,4 @@
 define('NAVBAR_TITLE_1', 'Checkout');
 define('NAVBAR_TITLE_2', 'Success');
 
-define('HEADING_TITLE', 'Your Order Has Been Processed!');
-
 define('TABLE_HEADING_COMMENTS', 'Enter a comment for the order processed');
