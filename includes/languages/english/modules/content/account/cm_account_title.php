6cc52e33a327de6dae3ac00c42afb9d28822b8d6
gburton <oscshops@gmail.com>
2018-04-05 13:15:52 +0100

    Modularise Page Heading

---
+++ includes/languages/english/modules/content/account/cm_account_title.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2018 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  const MODULE_CONTENT_ACCOUNT_TITLE_TITLE        = 'Page Heading';
+  const MODULE_CONTENT_ACCOUNT_TITLE_DESCRIPTION  = 'Shows the Page Heading.';
+  
+  const MODULE_CONTENT_ACCOUNT_TITLE_PUBLIC_TITLE = 'My Account Information';
+  
