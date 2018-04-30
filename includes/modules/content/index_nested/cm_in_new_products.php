4f34ae8f49002752a6d4eda7866313acf780de4f
gburton <oscshops@gmail.com>
2018-04-30 21:00:26 +0100

    Update cm_in_new_products.php

--- includes/modules/content/index_nested/cm_in_new_products.php
+++ includes/modules/content/index_nested/cm_in_new_products.php
@@ -38,7 +38,7 @@
       $content_width = MODULE_CONTENT_IN_NEW_PRODUCTS_CONTENT_WIDTH;
       $product_width = MODULE_CONTENT_IN_NEW_PRODUCTS_DISPLAY_EACH;
      
-      $new_products_query = tep_db_query("select distinct p.products_id, p.products_image, p.products_tax_class_id, pd.products_name, if(s.status, s.specials_new_products_price, p.products_price) as products_price, p.products_quantity as in_stock, if(s.status, 1, 0) as is_special from products p left join specials s on p.products_id = s.products_id, products_description pd, products_to_categories p2c, categories c where p.products_id = p2c.products_id and p2c.categories_id = c.categories_id and c.parent_id = '" . (int)$current_category_id . "' and p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by p.products_date_added desc limit " . (int)MODULE_CONTENT_IN_NEW_PRODUCTS_MAX_DISPLAY);
+      $new_products_query = tep_db_query("select distinct p.products_id, p.products_image, p.products_tax_class_id, pd.products_name, if(s.status, s.specials_new_products_price, p.products_price) as products_price, p.products_quantity as in_stock, if(s.status, 1, 0) as is_special from products p left join specials s on p.products_id = s.products_id, products_description pd, products_to_categories p2c, categories c where p.products_id = p2c.products_id and p2c.categories_id = c.categories_id and c.parent_id = '" . (int)$current_category_id . "' and p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by p.products_id desc limit " . (int)MODULE_CONTENT_IN_NEW_PRODUCTS_MAX_DISPLAY);
       $num_new_products = tep_db_num_rows($new_products_query);
 
       if ($num_new_products > 0) {
