623c9cf0be4e03c121cf0cdba19bff139850b4b8
gburton <oscshops@gmail.com>
2018-04-30 20:50:13 +0100

    Update cm_pi_price.php

--- includes/languages/english/modules/content/product_info/cm_pi_price.php
+++ includes/languages/english/modules/content/product_info/cm_pi_price.php
@@ -13,6 +13,6 @@
   const MODULE_CONTENT_PI_PRICE_TITLE = 'Price';
   const MODULE_CONTENT_PI_PRICE_DESCRIPTION = 'Shows the Products Price on the product_info Page.';
   
-  const MODULE_CONTENT_PI_PRICE_DISPLAY_SPECIAL = '<del>%1$s</del> <span class="productPrice text-danger productSpecialPrice">%2$s</span>';
+  const MODULE_CONTENT_PI_PRICE_DISPLAY_SPECIAL = '<del>%2$s</del> <span class="productPrice text-danger productSpecialPrice">%1$s</span>';
   const MODULE_CONTENT_PI_PRICE_DISPLAY = '<span class="productPrice">%s</span>';
   
