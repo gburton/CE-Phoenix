5367beea4c758c7513a0f985dfe1330dcd0d7a61
gburton <oscshops@gmail.com>
2018-05-08 12:57:19 +0100

    Undefined Constant Warning
    This is from the language file.
    Also inserted a Helper Warning which shows when the module is uninstalled or is installed with the PayTo not set.

--- includes/languages/english/modules/payment/moneyorder.php
+++ includes/languages/english/modules/payment/moneyorder.php
@@ -11,6 +11,7 @@
 */
 
   define('MODULE_PAYMENT_MONEYORDER_TEXT_TITLE', 'Check/Money Order');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Make Payable To:&nbsp;' . MODULE_PAYMENT_MONEYORDER_PAYTO . '<br /><br />Send To:<br />' . STORE_NAME . '<br />' . nl2br(STORE_ADDRESS) . '<br /><br />' . 'Your order will not ship until we receive payment.');
-  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Make Payable To: ". MODULE_PAYMENT_MONEYORDER_PAYTO . "\n\nSend To:\n" . STORE_NAME . "\n" . STORE_ADDRESS . "\n\n" . 'Your order will not ship until we receive payment.');
-?>
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_DESCRIPTION', 'Make Payable To:&nbsp;' . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '') . '<br /><br />Send To:<br />' . STORE_NAME . '<br />' . nl2br(STORE_ADDRESS) . '<br /><br />' . 'Your order will not ship until we receive payment.');
+  define('MODULE_PAYMENT_MONEYORDER_TEXT_EMAIL_FOOTER', "Make Payable To: " . (defined('MODULE_PAYMENT_MONEYORDER_PAYTO') ? MODULE_PAYMENT_MONEYORDER_PAYTO : '') . "\n\nSend To:\n" . STORE_NAME . "\n" . STORE_ADDRESS . "\n\n" . 'Your order will not ship until we receive payment.');
+
+  define('MODULE_PAYMENT_MONEYORDER_WARNING_SETUP', 'Please ensure you set up the \'Make Payable to\' parameter.');
