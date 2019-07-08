f0838ab870b970e3108f5b947a87f95ae40bfa52
gburton <oscshops@gmail.com>
2019-07-08 21:23:51 +0100

    Close p tag

--- includes/languages/english.php
+++ includes/languages/english.php
@@ -226,4 +226,4 @@ define('PRODUCT_REMOVED', '%s has been removed from your Cart');
 define('MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION', '');
 
 // noscript helper
-define('TEXT_NOSCRIPT', '<p><strong>JavaScript seems to be disabled in your browser.</strong></p><p>You must have JavaScript enabled in your browser to utilize the functionality of this website.<br><a class="alert-link" href="https://www.enable-javascript.com/" target="_blank" rel="nofollow">Click here for instructions on enabling javascript in your browser</a>.');
+define('TEXT_NOSCRIPT', '<p><strong>JavaScript seems to be disabled in your browser.</strong></p><p>You must have JavaScript enabled in your browser to utilize the functionality of this website.<br><a class="alert-link" href="https://www.enable-javascript.com/" target="_blank" rel="nofollow">Click here for instructions on enabling javascript in your browser</a>.</p>');
