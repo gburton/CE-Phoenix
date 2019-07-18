2d653fa21723fb5b4b0416315b085ffbb24cd471
raiwa <info@oscaddons.com.com>
2019-07-18 10:38:39 +0200

    Update shopping_cart.php

--- includes/languages/english/shopping_cart.php
+++ includes/languages/english/shopping_cart.php
@@ -11,24 +11,3 @@
 */
 
 define('NAVBAR_TITLE', 'Cart Contents');
-define('HEADING_TITLE', 'What\'s In My Cart?');
-define('TEXT_CART_EMPTY', 'Your Shopping Cart is empty!');
-define('SUB_TITLE_SUB_TOTAL', 'Sub-Total:');
-define('SUB_TITLE_TOTAL', 'Total:');
-
-define('OUT_OF_STOCK_CANT_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' don\'t exist in desired quantity in our stock.<br />Please alter the quantity of products marked with (' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . '). Thank you');
-define('OUT_OF_STOCK_CAN_CHECKOUT', 'Products marked with ' . STOCK_MARK_PRODUCT_OUT_OF_STOCK . ' don\'t exist in desired quantity in our stock.<br />You can buy them anyway and check the quantity we have in stock for immediate delivery in the checkout process.');
-
-define('TEXT_ALTERNATIVE_CHECKOUT_METHODS', '- OR -');
-
-define('CART_BUTTON_UPDATE', '<i title="Update" class="fas fa-sync fa-fw"></i>');
-define('CART_BUTTON_REMOVE', '<i title="Remove" class="fas fa-times fa-fw"></i>');
-
-define('TABLE_SC_HEADING_PRODUCT', 'Product');
-define('TABLE_SC_HEADING_AVAILABILITY', 'Stock');
-define('TABLE_SC_HEADING_QUANTITY', 'Qty');
-define('TABLE_SC_HEADING_REMOVE', 'Remove');
-define('TABLE_SC_HEADING_PRICE', 'Price');
-
-define('TEXT_IN_STOCK', '<i class="fas fa-check fa-2x text-success"></i>');
-
