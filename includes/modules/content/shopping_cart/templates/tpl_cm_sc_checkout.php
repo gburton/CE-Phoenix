891271d02cfb9de70e0c26463941e826f160bf03
raiwa <info@oscaddons.com.com>
2019-07-18 08:47:52 +0200

    1.0.1.0

---
+++ includes/modules/content/shopping_cart/templates/tpl_cm_sc_checkout.php
@@ -0,0 +1,48 @@
+<div class="col-sm-<?php echo $content_width ?> cm-sc-checkout">
+
+  <div class="buttonSet">
+    <?php echo tep_draw_button(MODULE_CONTENT_SC_CHECKOUT_BUTTON_CHECKOUT, 'fas fa-angle-right', tep_href_link('checkout_shipping.php', '', 'SSL'), 'primary', NULL, 'btn-success btn-lg btn-block'); ?>
+  </div>
+  
+<?php
+    if (!empty($initialize_checkout_methods)) {
+?>
+  <div class="w-100"></div>
+  <p class="text-right"><?php echo MODULE_CONTENT_SC_CHECKOUT_ALTERNATIVE_CHECKOUT_METHODS; ?></p>
+
+<?php
+      foreach($initialize_checkout_methods as $value) {
+?>
+
+  <p class="text-right"><?php echo $value; ?></p>
+
+<?php
+      }
+    }
+?>
+
+</div>
+
+<?php
+/*
+  $Id$
+
+  Copyright (c) 2016:
+    Dan Cole - @Dan Cole
+    James Keebaugh - @kymation
+    Lambros - @Tsimi
+    Rainer Schmied - @raiwa
+
+  All rights reserved.﻿﻿
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+?>
