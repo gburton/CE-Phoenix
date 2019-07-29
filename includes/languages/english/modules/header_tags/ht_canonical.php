7545cb4bbd7a02c9f797b5c405bd8a89f7bd9962
gburton <oscshops@gmail.com>
2019-07-29 09:16:22 +0100

    Revert Remove Canonical

---
+++ includes/languages/english/modules/header_tags/ht_canonical.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2019 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_HEADER_TAGS_CANONICAL_TITLE', 'Canonical Links');
+  define('MODULE_HEADER_TAGS_CANONICAL_DESCRIPTION', 'Add canonical links to category/product pages.  <div class="secWarning">Please note that hreflang attributes are not supported in this module which will affect you if you have more than one language in your shop.<br><br>You should seek the advice of a good SEO advisor (eg Jack_mcs in the main osCommerce forum).</div>');
+  
