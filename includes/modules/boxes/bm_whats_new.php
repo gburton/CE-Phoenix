6b208db6f9a743fc9514257d1b495197825161fc
gburton <oscshops@gmail.com>
2019-08-06 16:20:08 +0100

    Standardise Layout
    Boxes to show limited data (no buttons).

--- includes/modules/boxes/bm_whats_new.php
+++ includes/modules/boxes/bm_whats_new.php
@@ -25,25 +25,37 @@
       if ( defined('MODULE_BOXES_WHATS_NEW_STATUS') ) {
         $this->sort_order = MODULE_BOXES_WHATS_NEW_SORT_ORDER;
         $this->enabled = (MODULE_BOXES_WHATS_NEW_STATUS == 'True');
-        
+
         $this->group = ((MODULE_BOXES_WHATS_NEW_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
       }
     }
 
     function execute() {
-      global $currencies, $oscTemplate;
+      global $currencies, $oscTemplate, $languages_id;
 
-      if ($random_product = tep_random_select("select products_id, products_image, products_tax_class_id, products_price from products where products_status = '1' order by products_date_added desc limit " . MAX_RANDOM_SELECT_NEW)) {
-        $random_product['products_name'] = tep_get_products_name($random_product['products_id']);
-        $random_product['specials_new_products_price'] = tep_get_products_special_price($random_product['products_id']);
+      $data = array();
 
-        if (tep_not_null($random_product['specials_new_products_price'])) {
-          $whats_new_price = '<del>' . $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])) . '</del><br />';
-          $whats_new_price .= '<span class="productPrice text-danger productSpecialPrice">' . $currencies->display_price($random_product['specials_new_products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])) . '</span>';
-        } else {
-          $whats_new_price = $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id']));
+      if ($random_product = tep_random_select("select p.*, pd.*, m.*, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price, p.products_quantity as in_stock, if(s.status, 1, 0) as is_special from products_description pd, products p left join manufacturers m on p.manufacturers_id = m.manufacturers_id left join specials s on p.products_id = s.products_id where p.products_status = '1' and p.products_id = pd.products_id and pd.language_id = '" . (int)$languages_id . "' order by products_date_added desc limit " . MAX_RANDOM_SELECT_NEW)) {
+        $data['data-is-special'] = (int)$random_product['is_special'];
+        $data['data-product-price'] = $currencies->display_raw($random_product['final_price'], tep_get_tax_rate($random_product['products_tax_class_id']));
+        $data['data-product-manufacturer'] = max(0, (int)$random_product['manufacturers_id']);
+
+        // data attributes
+        $box_attr = '';
+        foreach ( $data as $key => $value ) {
+          $box_attr .= ' ' . tep_output_string_protected($key) . '="' . tep_output_string_protected($value) . '"';
         }
-                 
+        // product title
+        $box_title = '<a href="' . tep_href_link('product_info.php', 'products_id=' . (int)$random_product['products_id']) . '">' . $random_product['products_name'] . '</a>';
+        // product image
+        $box_image = '<a href="' . tep_href_link('product_info.php', 'products_id=' . $random_product['products_id']) . '">' . tep_image('images/' . $random_product['products_image'], htmlspecialchars($random_product['products_name']), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '', true, 'card-img-top') . '</a>';
+        // product price
+        if ($random_product['is_special'] == 1) {
+          $box_price = sprintf(IS_PRODUCT_SHOW_PRICE_SPECIAL, $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])), $currencies->display_price($random_product['specials_new_products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])));
+        } else {
+          $box_price = sprintf(IS_PRODUCT_SHOW_PRICE, $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])));
+        }
+
         ob_start();
         include('includes/modules/boxes/templates/tpl_' . basename(__FILE__));
         $data = ob_get_clean();
@@ -74,4 +86,4 @@
       return array('MODULE_BOXES_WHATS_NEW_STATUS', 'MODULE_BOXES_WHATS_NEW_CONTENT_PLACEMENT', 'MODULE_BOXES_WHATS_NEW_SORT_ORDER');
     }
   }
-
+  
