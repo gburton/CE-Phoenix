95a6409306477b2af59b1a495abe97c61f8c8dea
gburton <oscshops@gmail.com>
2019-09-03 12:25:38 +0100

    Introduce Password Forgotten Module

---
+++ includes/modules/content/login/cm_forgot_password.php
@@ -0,0 +1,72 @@
+<?php
+/*
+  Copyright (c) 2019, G Burton
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+  class cm_forgot_password {
+    var $code;
+    var $group;
+    var $title;
+    var $description;
+    var $sort_order;
+    var $enabled = false;
+
+    function __construct() {
+      $this->code = get_class($this);
+      $this->group = basename(dirname(__FILE__));
+
+      $this->title = MODULE_CONTENT_FORGOT_PASSWORD_TITLE;
+      $this->description = MODULE_CONTENT_FORGOT_PASSWORD_DESCRIPTION;
+
+      if ( defined('MODULE_CONTENT_FORGOT_PASSWORD_STATUS') ) {
+        $this->sort_order = MODULE_CONTENT_FORGOT_PASSWORD_SORT_ORDER;
+        $this->enabled = (MODULE_CONTENT_FORGOT_PASSWORD_STATUS == 'True');
+      }
+    }
+
+    function execute() {
+      global $oscTemplate;
+
+      $content_width = (int)MODULE_CONTENT_FORGOT_PASSWORD_CONTENT_WIDTH;
+
+      ob_start();
+      include('includes/modules/content/' . $this->group . '/templates/tpl_' . basename(__FILE__));
+      $template = ob_get_clean();
+
+      $oscTemplate->addContent($template, $this->group);
+    }
+
+    function isEnabled() {
+      return $this->enabled;
+    }
+
+    function check() {
+      return defined('MODULE_CONTENT_FORGOT_PASSWORD_STATUS');
+    }
+
+    function install() {
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Forgot Password Module', 'MODULE_CONTENT_FORGOT_PASSWORD_STATUS', 'True', 'Do you want to enable this module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_FORGOT_PASSWORD_CONTENT_WIDTH', '6', 'What width container should the content be shown in? (12 = full width, 6 = half width).', '6', '1', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
+      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_FORGOT_PASSWORD_SORT_ORDER', '3000', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+    }
+
+    function remove() {
+      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
+    }
+
+    function keys() {
+      return array('MODULE_CONTENT_FORGOT_PASSWORD_STATUS', 'MODULE_CONTENT_FORGOT_PASSWORD_CONTENT_WIDTH', 'MODULE_CONTENT_FORGOT_PASSWORD_SORT_ORDER');
+    }
+  }
+  
