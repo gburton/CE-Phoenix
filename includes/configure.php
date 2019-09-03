9772a1b4c38e6775f37641863afae2ced3615bc1
raiwa <info@oscaddons.com.com>
2019-09-03 09:57:21 +0200

    revert config files

--- includes/configure.php
+++ includes/configure.php
@@ -1,22 +1,37 @@
 <?php
-  define('HTTP_SERVER', 'http://localhost');
-  define('HTTPS_SERVER', 'http://localhost');
-  define('ENABLE_SSL', false);
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+// Define the webserver and path parameters
+// * DIR_FS_* = Filesystem directories (local/physical)
+// * DIR_WS_* = Webserver directories (virtual/URL)
+  define('HTTP_SERVER', ''); // eg, http://localhost - should not be empty for productive servers
+  define('HTTPS_SERVER', ''); // eg, https://localhost - should not be empty for productive servers
+  define('ENABLE_SSL', false); // secure webserver for checkout procedure?
   define('HTTP_COOKIE_DOMAIN', '');
   define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '/Phoenix_Git/Responsive-osCommerce/');
-  define('HTTPS_COOKIE_PATH', '/Phoenix_Git/Responsive-osCommerce/');
-  define('DIR_WS_HTTP_CATALOG', '/Phoenix_Git/Responsive-osCommerce/');
-  define('DIR_WS_HTTPS_CATALOG', '/Phoenix_Git/Responsive-osCommerce/');
+  define('HTTP_COOKIE_PATH', '');
+  define('HTTPS_COOKIE_PATH', '');
+  define('DIR_WS_HTTP_CATALOG', '');
+  define('DIR_WS_HTTPS_CATALOG', '');
 
-  define('DIR_FS_CATALOG', 'C:/xampp_php_7_3_2/htdocs/Phoenix_Git/Responsive-osCommerce/');
+  define('DIR_FS_CATALOG', dirname($_SERVER['SCRIPT_FILENAME']) . '/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
-  define('DB_SERVER', 'localhost');
-  define('DB_SERVER_USERNAME', 'root');
+// define our database connection
+  define('DB_SERVER', ''); // eg, localhost - should not be empty for productive servers
+  define('DB_SERVER_USERNAME', '');
   define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'phoenix_git_spanish_lng');
-  define('USE_PCONNECT', 'false');
-  define('STORE_SESSIONS', 'mysql');
-  define('CFG_TIME_ZONE', 'Europe/Berlin');
+  define('DB_DATABASE', 'osCommerce');
+  define('USE_PCONNECT', 'false'); // use persistent connections?
+  define('STORE_SESSIONS', ''); // leave empty '' for default handler or set to 'mysql'
+?>
