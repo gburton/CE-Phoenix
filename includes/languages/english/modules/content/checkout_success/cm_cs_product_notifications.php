a275dc19e58caaf2bcc973d4091154e99e70065c
gburton <oscshops@gmail.com>
2019-09-05 09:43:47 +0100

    Modernise output of notifications

--- includes/languages/english/modules/content/checkout_success/cm_cs_product_notifications.php
+++ includes/languages/english/modules/content/checkout_success/cm_cs_product_notifications.php
@@ -12,6 +12,8 @@
 
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TITLE', 'Product Notifications');
   define('MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_DESCRIPTION', 'Add product notification to checkout success page');
+  
+  define('MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS_TITLE', 'Product Notifications');
 
-  define('MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS', '<strong>Please notify me of updates to the products I have selected below</strong>');
-?>
+  define('MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS', 'Please notify me of updates on...');
+  
--- includes/modules/content/checkout_success/templates/tpl_cm_cs_product_notifications.php
+++ includes/modules/content/checkout_success/templates/tpl_cm_cs_product_notifications.php
@@ -1,12 +1,17 @@
 <div class="col-sm-12 cm-cs-product-notifications">
   <div class="card mb-2">
     <div class="card-header">
-      <?php echo MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS; ?>
+      <?php echo MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS_TITLE; ?>
     </div>
     <div class="card-body">
-      <p class="productsNotifications">
-        <?php echo $products_notifications; ?>
-      </p>
+      <div class="row">
+        <div class="col-sm-4">
+          <?php echo MODULE_CONTENT_CHECKOUT_SUCCESS_PRODUCT_NOTIFICATIONS_TEXT_NOTIFY_PRODUCTS; ?>
+        </div>
+        <div class="col-sm-8">
+          <?php echo $products_notifications; ?>
+        </div>
+      </div>
     </div>
   </div>
 </div>
