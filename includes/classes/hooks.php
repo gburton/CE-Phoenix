0ac0c1f84db3f6591a4c7a8059c45793bea6acdd
BrockleyJohn - John Ferguson <github@yourmontage.co.uk>
2019-09-17 22:27:05 +0100

    one-liner sort

--- includes/classes/hooks.php
+++ includes/classes/hooks.php
@@ -25,40 +25,36 @@
 
       $directory = DIR_FS_CATALOG . 'includes/hooks/' . $this->_site . '/' . $group;
 
-      $files = [];
-	  if ( file_exists($directory) ) {
+      if ( file_exists($directory) ) {
         if ( $dir = @dir($directory) ) {
           while ( $file = $dir->read() ) {
             if ( !is_dir($directory . '/' . $file) ) {
-              $files[] = $file;
+              if ( substr($file, strrpos($file, '.')) == '.php' ) {
+                $code = substr($file, 0, strrpos($file, '.'));
+                $class = 'hook_' . $this->_site . '_' . $group . '_' . $code;
+
+                include($directory . '/' . $file);
+                $GLOBALS[$class] = new $class();
+
+                foreach ( get_class_methods($GLOBALS[$class]) as $method ) {
+                  if ( substr($method, 0, 7) == 'listen_' ) {
+                    $this->_hooks[$this->_site][$group][substr($method, 7)][] = $code;
+                  }
+                }
+              }
             }
           }
 
           $dir->close();
         }
-		natsort($files);
-		foreach ($files as $file) {
-		  if ( substr($file, strrpos($file, '.')) == '.php' ) {
-            $code = substr($file, 0, strrpos($file, '.'));
-            $class = 'hook_' . $this->_site . '_' . $group . '_' . $code;
-
-            include($directory . '/' . $file);
-            $GLOBALS[$class] = new $class();
-
-            foreach ( get_class_methods($GLOBALS[$class]) as $method ) {
-              if ( substr($method, 0, 7) == 'listen_' ) {
-                $this->_hooks[$this->_site][$group][substr($method, 7)][] = $code;
-              }
-            }
-          }
-		}
       }
     }
 
     function call($group, $action, $arr = array()) {
       $result = '';
 
-      foreach ( @(array)$this->_hooks[$this->_site][$group][$action] as $hook ) {
+      @asort($this->_hooks[$this->_site][$group][$action]); // new sorting
+	  foreach ( (array)$this->_hooks[$this->_site][$group][$action] as $hook ) {
         $result .= call_user_func(array($GLOBALS['hook_' . $this->_site . '_' . $group . '_' . $hook], 'listen_' . $action), $arr);
       }
 
