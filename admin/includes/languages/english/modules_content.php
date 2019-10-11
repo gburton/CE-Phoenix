542ded86e8bcbec1cbcb6af4263d5e94ef8e5260
gburton <oscshops@gmail.com>
2019-10-11 12:42:42 +0100

    Add (in)active flag
    TY @bonbec for the idea

--- admin/includes/languages/english/modules_content.php
+++ admin/includes/languages/english/modules_content.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2014 osCommerce
+  Copyright (c) 2019 osCommerce
 
   Released under the GNU General Public License
 */
@@ -15,6 +15,7 @@ define('HEADING_TITLE', 'Content Modules');
 define('TABLE_HEADING_MODULES', 'Modules');
 define('TABLE_HEADING_GROUP', 'Group');
 define('TABLE_HEADING_SORT_ORDER', 'Sort Order');
+define('TABLE_HEADING_MODULE_ACTIVE', 'Active');
 define('TABLE_HEADING_ACTION', 'Action');
 
 define('TEXT_INFO_VERSION', 'Version:');
@@ -22,4 +23,3 @@ define('TEXT_INFO_ONLINE_STATUS', 'online status');
 define('TEXT_INFO_API_VERSION', 'API Version:');
 
 define('TEXT_MODULE_DIRECTORY', 'Module Directory:');
-?>
