0103e1e369d2e2ea61e228560c2e5bd1d4535131
Rainer Schmied <info@oscaddons.com>
2019-11-21 16:21:28 +0100

    catalog box
    (cherry picked from commit 3041ea60e4a50c56f4a4f9bbe5eb904a9a482b15)

---
+++ admin/includes/boxes/catalog_products_attributes.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2019 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  foreach ( $cl_box_groups as &$group ) {
+    if ( $group['heading'] == BOX_HEADING_CATALOG ) {
+      $group['apps'][] = array('products_attributes.php',
+                               'title' => BOX_CATALOG_CATEGORIES_PRODUCTS_ATTRIBUTES,
+                               'link' => tep_href_link('products_attributes.php'));
+
+      break;
+    }
+  }
+
---
+++ admin/includes/boxes/catalog_reviews.php
@@ -0,0 +1,22 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2010 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  foreach ( $cl_box_groups as &$group ) {
+    if ( $group['heading'] == BOX_HEADING_CATALOG ) {
+      $group['apps'][] = array('code' => 'reviews.php',
+                               'title' => BOX_CATALOG_REVIEWS,
+                               'link' => tep_href_link('reviews.php'));
+
+      break;
+    }
+  }
+  
