e58d36fe6e65dd4196bbb624c3ccff01cb6cc2ae
gburton <oscshops@gmail.com>
2019-11-28 14:15:38 +0000

    Fix glitches in Testimonials Modules

--- includes/languages/english/testimonials.php
+++ includes/languages/english/testimonials.php
@@ -16,7 +16,3 @@
 
 define('NAVBAR_TITLE', 'Testimonials');
 define('HEADING_TITLE', 'Customer Testimonials');
-
-define('TEXT_NO_TESTIMONIALS', 'There are no Testimonials to display.');
-
-define('TEXT_DISPLAY_NUMBER_OF_TESTIMONIALS', 'Displaying <b>%s</b> to <b>%s</b> (of <b>%s</b> Testimonials)');
