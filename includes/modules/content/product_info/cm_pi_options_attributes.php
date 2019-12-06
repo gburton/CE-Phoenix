cff8520bd1f29daa8820dccc4d823c1d740ab2ea
Rainer Schmied <info@oscaddons.com>
2019-12-06 14:46:45 +0100

    Update cm_pi_options_attributes.php

--- includes/modules/content/product_info/cm_pi_options_attributes.php
+++ includes/modules/content/product_info/cm_pi_options_attributes.php
@@ -42,7 +42,7 @@
       $content_width = (int)MODULE_CONTENT_PI_OA_CONTENT_WIDTH;
       $options_output = null;
         
-      $products_options_name_query = tep_db_query("select distinct popt.products_options_id, popt.products_options_name from products_options popt, products_attributes patrib where patrib.products_id='" . (int)$_GET['products_id'] . "' and patrib.options_id = popt.products_options_id and popt.language_id = '" . (int)$languages_id . "' order by popt.products_options_name");
+      $products_options_name_query = tep_db_query("select distinct popt.* from products_options popt, products_attributes patrib where patrib.products_id='" . (int)$_GET['products_id'] . "' and patrib.options_id = popt.products_options_id and popt.language_id = '" . (int)$languages_id . "' order by popt.products_options_name");
 
       if (tep_db_num_rows($products_options_name_query)) {
         while ($products_options_name = tep_db_fetch_array($products_options_name_query)) {
@@ -57,7 +57,7 @@
             $products_options_array[] = array('id' => '', 'text' => MODULE_CONTENT_PI_OA_ENFORCE_SELECTION);            
           }
           
-          $products_options_query = tep_db_query("select pov.products_options_values_id, pov.products_options_values_name, pa.options_values_price, pa.price_prefix from products_attributes pa, products_options_values pov where pa.products_id = '" . (int)$_GET['products_id'] . "' and pa.options_id = '" . (int)$products_options_name['products_options_id'] . "' and pa.options_values_id = pov.products_options_values_id and pov.language_id = '" . (int)$languages_id . "'");
+          $products_options_query = tep_db_query("select pov.*, pa.* from products_attributes pa, products_options_values pov where pa.products_id = '" . (int)$_GET['products_id'] . "' and pa.options_id = '" . (int)$products_options_name['products_options_id'] . "' and pa.options_values_id = pov.products_options_values_id and pov.language_id = '" . (int)$languages_id . "'");
           while ($products_options = tep_db_fetch_array($products_options_query)) {
             $products_options_array[] = array('id' => $products_options['products_options_values_id'], 'text' => $products_options['products_options_values_name']);
             if ($products_options['options_values_price'] != '0') {
