2da27fc1464127eb21d45c1d461889e7c02de84f
gburton <oscshops@gmail.com>
2019-12-23 17:42:13 +0000

    Bootstrapped Page

--- admin/includes/languages/english/modules.php
+++ admin/includes/languages/english/modules.php
@@ -5,13 +5,14 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2008 osCommerce
+  Copyright (c) 2019 osCommerce
 
   Released under the GNU General Public License
 */
 
 define('TABLE_HEADING_MODULES', 'Modules');
 define('TABLE_HEADING_SORT_ORDER', 'Sort Order');
+define('TABLE_HEADING_ENABLED', 'Active');
 define('TABLE_HEADING_ACTION', 'Action');
 
 define('TEXT_INFO_VERSION', 'Version:');
@@ -19,4 +20,4 @@ define('TEXT_INFO_ONLINE_STATUS', 'online status');
 define('TEXT_INFO_API_VERSION', 'API Version:');
 
 define('TEXT_MODULE_DIRECTORY', 'Module Directory:');
-?>
+
