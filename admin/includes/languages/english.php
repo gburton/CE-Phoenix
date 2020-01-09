cb0bbfdb5d144aa8d0631436d37cd29dc0724b5f
gburton <oscshops@gmail.com>
2020-01-09 18:08:23 +0000

    Update Certified Addons

--- admin/includes/languages/english.php
+++ admin/includes/languages/english.php
@@ -47,11 +47,9 @@ define('CHARSET', 'utf-8');
 define('TITLE', 'OSCOM CE Phoenix Administration Tool');
 
 // header text in includes/header.php
-define('HEADER_TITLE_TOP', 'Administration');
-define('HEADER_TITLE_SUPPORT_SITE', '<span class="border-bottom border-primary">Support Site</span>');
-define('HEADER_TITLE_ONLINE_CATALOG', '<span class="border-bottom border-primary">Shop</span>');
-define('HEADER_TITLE_ADMINISTRATION', '<span class="border-bottom border-primary">Admin</span>');
+define('HEADER_TITLE_ONLINE_CATALOG', '<i class="fas fa-shopping-cart text-primary"></i> <span class="border-bottom border-primary">Your Shop</span>');
 define('HEADER_TITLE_PHOENIX_CLUB', '<span class="border-bottom border-primary">Phoenix Club</span>');
+define('HEADER_TITLE_CERTIFIED_ADDONS', '<span class="border-bottom border-primary">Certified Addons</span>');
 define('HEADER_TITLE_LOGOFF', '<i class="fas fa-lock"></i> <span class="border-bottom border-danger"> %s, securely logoff</span>');
 
 // text for gender
---
+++ admin/includes/languages/english/certified_addons.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+define('HEADING_TITLE', 'Certified Addons');
+
+define('TABLE_CERTIFIED_ADDONS_TITLE', 'Addon');
+define('TABLE_CERTIFIED_ADDONS_OWNER', 'Created By');
+define('TABLE_CERTIFIED_ADDONS_RATING', 'Rating');
+define('TABLE_CERTIFIED_ADDONS_DATE', 'Release Date');
+
+define('TEXT_CERTIFIED_ADDONS', 'All Addons listed here have been <b>certified</b> by a Phoenix Core Team member.');
+define('NUM_CERTIFIED_ADDONS', 'There are currently <b>%s</b> Certified Addons.');
+
+define('NUM_FILTERED_ADDONS', '<span class="filtered-result"><br>Filtered to <b>{X}</b> Addon(s).</span>');
