dd059fd71a6bf6dc5e5a62b21056c4a05eee0d57
Matt Fletcher <github@ecartz.biz>
2020-02-15 16:54:59 +0000

    Define missing constant

--- admin/includes/languages/english/customers.php
+++ admin/includes/languages/english/customers.php
@@ -30,3 +30,5 @@ const TYPE_BELOW = 'Type below';
 const PLEASE_SELECT = 'Select One';
 
 const PULL_DOWN_DEFAULT = PLEASE_SELECT;
+
+const FORM_REQUIRED_INPUT = '<span class="form-control-feedback text-danger"><i class="fas fa-asterisk"></i></span>';
