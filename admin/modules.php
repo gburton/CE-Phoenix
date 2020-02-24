1fbf7ee26dbaefb820927c49ceb5efcaebdb2aea
gburton <oscshops@gmail.com>
2020-02-24 14:47:16 +0000

    Revert "Update modules.php"
    This reverts commit 2b35de15d06db540d645292e7964ba822177e9c0.

--- admin/modules.php
+++ admin/modules.php
@@ -205,9 +205,9 @@
 
       if (isset($mInfo) && is_object($mInfo) && ($class == $mInfo->code) ) {
         if ($module->check() > 0) {
-          echo '          <tr class="table-active" onclick="document.location.href=\'' . tep_href_link('modules.php', 'set=' . $set . '&module=' . $class . '&action=edit') . '\'">';
+          echo '          <tr onclick="document.location.href=\'' . tep_href_link('modules.php', 'set=' . $set . '&module=' . $class . '&action=edit') . '\'">';
         } else {
-          echo '          <tr class="table-active">';
+          echo '          <tr>';
         }
 
         $icon = '<i class="fas fa-chevron-circle-right text-info"></i>';
