4012d34fe9341d02b95e7e27e0405e14edefb090
gburton <oscshops@gmail.com>
2020-03-02 16:00:07 +0000

    create_account_success modules
    with pre-installation

---
+++ includes/modules/content/create_account_success/templates/tpl_cm_cas_title.php
@@ -0,0 +1,17 @@
+<div class="col-sm-<?php echo $content_width; ?> cm-cas-title">
+  <h1 class="display-4"><?php echo MODULE_CONTENT_CAS_TITLE_PUBLIC_TITLE; ?></h1>
+</div>
+
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+?>
+   
