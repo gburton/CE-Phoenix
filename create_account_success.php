faae58e15c347475921b03ce29979804771d0688
gburton <oscshops@gmail.com>
2020-03-02 15:56:24 +0000

    Introduce modularisation of create_account_success

--- create_account_success.php
+++ create_account_success.php
@@ -17,27 +17,14 @@
   $breadcrumb->add(NAVBAR_TITLE_1);
   $breadcrumb->add(NAVBAR_TITLE_2);
 
-  if (count($navigation->snapshot) > 0) {
-    $origin_href = tep_href_link($navigation->snapshot['page'], tep_array_to_string($navigation->snapshot['get'], [session_name()]), $navigation->snapshot['mode']);
-    $navigation->clear_snapshot();
-  } else {
-    $origin_href = tep_href_link('index.php');
-  }
-
-  $contact_us_href = tep_href_link('contact_us.php', '', 'SSL');
+  $page_content = $oscTemplate->getContent('create_account_success');
 
   require 'includes/template_top.php';
 ?>
 
-<h1 class="display-4"><?php echo HEADING_TITLE; ?></h1>
-
 <div class="contentContainer">
-  <div class="alert alert-success" role="alert">
-    <?php echo sprintf(TEXT_ACCOUNT_CREATED, $contact_us_href, $contact_us_href); ?>
-  </div>
-
-  <div class="buttonSet">
-    <div class="text-right"><?php echo tep_draw_button(IMAGE_BUTTON_CONTINUE, 'fas fa-angle-right', $origin_href, null, null, 'btn-success btn-block btn-lg'); ?></div>
+  <div class="row">
+    <?php echo $page_content; ?>
   </div>
 </div>
 
