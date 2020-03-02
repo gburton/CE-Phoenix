9337580605dc333b6de5f648632e1887e664f048
gburton <oscshops@gmail.com>
2020-03-02 15:50:55 +0000

    Two GDPR modules

---
+++ includes/languages/english/modules/content/gdpr/cm_gdpr_personal_details.php
@@ -0,0 +1,28 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_TITLE', 'GDPR Personal Details');
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_DESCRIPTION', 'Show Customers Personal Details on the GDPR page.');
+  
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_PUBLIC_TITLE', 'Your Personal Details');
+  
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_GIVEN_NAME', 'First Name');
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_FAMILY_NAME', 'Surname');
+  
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_GENDER', 'Gender');
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_GENDER_M', 'Male');
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_GENDER_F', 'Female');
+  
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_UNKNOWN', 'Unknown');
+  
+  define('MODULE_CONTENT_GDPR_PERSONAL_DETAILS_DOB', 'Date of Birth');
+  
