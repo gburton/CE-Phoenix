e809d7bc4962691f5d369f23d480de72dc7365b8
gburton <oscshops@gmail.com>
2020-03-03 18:33:51 +0000

    Introduce Advert Manager
    This replaces the older banner_manager.  Addon makers of (eg) carousels can use this for a central location of adverts (both images and/or text).  This system will be subject to minor change based on feedback received over the next few iterations of Phoenix.

--- install/oscommerce.sql
+++ install/oscommerce.sql
@@ -63,6 +63,23 @@ CREATE TABLE administrators (
   PRIMARY KEY (id)
 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
+DROP TABLE IF EXISTS advert;
+CREATE TABLE advert (
+  advert_id int NOT NULL auto_increment,
+  advert_title varchar(64) NOT NULL,
+  advert_url varchar(255) NOT NULL,
+  advert_fragment varchar(255) NOT NULL,
+  advert_image varchar(64) NOT NULL,
+  advert_group varchar(64) NOT NULL,
+  advert_html_text text,
+  date_added datetime NOT NULL,
+  date_status_change datetime DEFAULT NULL,
+  sort_order int(3),
+  status int(1) DEFAULT '1' NOT NULL,
+  PRIMARY KEY (advert_id),
+  KEY idx_advert_group (advert_group)
+) CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
 DROP TABLE IF EXISTS categories;
 CREATE TABLE categories (
    categories_id int NOT NULL auto_increment,
