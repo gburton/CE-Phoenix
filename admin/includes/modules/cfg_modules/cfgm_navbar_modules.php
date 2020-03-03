a190b4aead0926ec580bba3262ea7fed3c84619d
Matt Fletcher <github@ecartz.biz>
2020-03-03 19:40:01 +0000

    Rename navbar modules to just navbar

--- admin/includes/modules/cfg_modules/cfgm_navbar_modules.php
+++ admin/includes/modules/cfg_modules/cfgm_navbar_modules.php
@@ -11,15 +11,12 @@
 */
 
   class cfgm_navbar_modules {
-    var $code = 'navbar_modules';
-    var $directory;
-    var $language_directory = DIR_FS_CATALOG_LANGUAGES;
-    var $key = 'MODULE_CONTENT_NAVBAR_INSTALLED';
-    var $title;
-    var $template_integration = false;
 
-    function __construct() {
-      $this->directory = DIR_FS_CATALOG_MODULES . 'navbar_modules/';
-      $this->title = MODULE_CFG_MODULE_CONTENT_NAVBAR_TITLE;
-    }
+    public $code = 'navbar';
+    public $directory = DIR_FS_CATALOG_MODULES . 'navbar/';
+    public $language_directory = DIR_FS_CATALOG_LANGUAGES;
+    public $key = 'MODULE_CONTENT_NAVBAR_INSTALLED';
+    public $title = MODULE_CFG_MODULE_CONTENT_NAVBAR_TITLE;
+    public $template_integration = false;
+
   }
