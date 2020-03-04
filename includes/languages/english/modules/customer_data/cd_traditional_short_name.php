681f2f073e3b9826ec7b70b0578e4bab79f8f971
Matt Fletcher <github@ecartz.biz>
2020-03-04 19:07:48 +0000

    Module specific language constants

--- includes/languages/english/modules/customer_data/cd_traditional_short_name.php
+++ includes/languages/english/modules/customer_data/cd_traditional_short_name.php
@@ -12,3 +12,7 @@
 
 const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_TEXT_TITLE = 'Traditional short name';
 const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_TEXT_DESCRIPTION = 'Use the title and last name if available or the first name if not.';
+
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_MR = 'Dear Mr. %s,' . "\n\n";
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_MS = 'Dear Ms. %s,' . "\n\n";
+const MODULE_CUSTOMER_DATA_TRADITIONAL_SHORT_NAME_GREET_NONE = 'Dear %s' . "\n\n";
