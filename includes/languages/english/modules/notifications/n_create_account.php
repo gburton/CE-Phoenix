39ccc8221b8beded944038fa1d3bd49827c1343c
Matt Fletcher <github@ecartz.biz>
2020-03-04 23:12:44 +0000

    Notification module files

---
+++ includes/languages/english/modules/notifications/n_create_account.php
@@ -0,0 +1,20 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_TITLE = 'Create Account';
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT_DESCRIPTION = 'Send a notification at the completion of account creation.';
+
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_SUBJECT = 'Welcome to ' . STORE_NAME;
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WELCOME = 'We welcome you to <strong>' . STORE_NAME . '</strong>.' . "\n\n";
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_TEXT = 'You can now take part in the <strong>various services</strong> we have to offer you. Some of these services include:' . "\n\n" . '<li><strong>Permanent Cart</strong> - Any products added to your online cart remain there until you remove them, or check them out.' . "\n" . '<li><strong>Address Book</strong> - We can now deliver your products to another address other than yours! This is perfect to send birthday gifts direct to the birthday-person themselves.' . "\n" . '<li><strong>Order History</strong> - View your history of purchases that you have made with us.' . "\n" . '<li><strong>Products Reviews</strong> - Share your opinions on products with our other customers.' . "\n\n";
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_CONTACT = 'For help with any of our online services, please email the store-owner: ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n\n";
+const MODULE_NOTIFICATIONS_CREATE_ACCOUNT_WARNING = '<strong>Note:</strong> This email address was given to us by one of our customers. If you did not sign up to be a member, please send an email to ' . STORE_OWNER_EMAIL_ADDRESS . '.' . "\n";
