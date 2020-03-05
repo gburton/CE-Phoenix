aeae80926043a80a141172a6f67c7365ffc1b5c8
gburton <oscshops@gmail.com>
2020-03-05 11:55:07 +0000

    GDPR Modules

--- includes/languages/english/modules/content/gdpr/cm_gdpr_intro.php
+++ includes/languages/english/modules/content/gdpr/cm_gdpr_intro.php
@@ -10,8 +10,8 @@
   Released under the GNU General Public License
 */
 
-  define('MODULE_CONTENT_GDPR_INTRO_TITLE', 'GDPR Intro');
-  define('MODULE_CONTENT_GDPR_INTRO_DESCRIPTION', 'Explain to Customers what GDPR is...');
+  const MODULE_CONTENT_GDPR_INTRO_TITLE = 'GDPR Intro';
+  const MODULE_CONTENT_GDPR_INTRO_DESCRIPTION = 'Explain to Customers what GDPR is...';
   
-  define('MODULE_CONTENT_GDPR_INTRO_PUBLIC_TEXT', '<p>The <strong>General Data Protection Regulation (GDPR)</strong> (Regulation (EU) 2016/679) is a regulation by which the European Parliament, the Council of the European Union and the European Commission intend to strengthen and unify data protection for all individuals within the European Union (EU).</p><p>The GDPR states that you are able to review your personal information that we hold.  If you are a EU Citizen, you can request deletion of some of this data.</p>');
+  const MODULE_CONTENT_GDPR_INTRO_PUBLIC_TEXT = '<p>The <strong>General Data Protection Regulation (GDPR)</strong> (Regulation (EU) 2016/679) is a regulation by which the European Parliament, the Council of the European Union and the European Commission intend to strengthen and unify data protection for all individuals within the European Union (EU).</p><p>The GDPR states that you are able to review your personal information that we hold.  If you are a EU Citizen, you can request deletion of some of this data.</p>';
   
