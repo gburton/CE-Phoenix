40ed83f0f3604e6248f489bf7b5c885320b9ec1a
Matt Fletcher <github@ecartz.biz>
2020-03-06 15:46:11 +0000

    Add hook to infobox

--- admin/includes/classes/box.php
+++ admin/includes/classes/box.php
@@ -5,31 +5,37 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2003 osCommerce
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 
   Example usage:
 
-  $heading = array();
-  $heading[] = array('params' => 'class="menuBoxHeading"',
-                     'text'  => BOX_HEADING_TOOLS,
-                     'link'  => tep_href_link(basename($PHP_SELF));
+  $heading = [];
+  $heading[] = [
+    'params' => 'class="menuBoxHeading"',
+    'text'  => BOX_HEADING_TOOLS,
+    'link'  => tep_href_link(basename($PHP_SELF),
+  ];
 
-  $contents = array();
-  $contents[] = array('text'  => SOME_TEXT);
+  $contents = [];
+  $contents[] = ['text'  => SOME_TEXT];
 
-  $box = new box;
+  $box = new box();
   echo $box->infoBox($heading, $contents);
 */
 
   class box extends tableBlock {
-	function __construct() {
-      $this->heading = array();
-      $this->contents = array();
+
+    function __construct() {
+      $this->heading = [];
+      $this->contents = [];
     }
 
     function infoBox($heading, $contents) {
+      $parameters = ['heading' => &$heading, 'contents' => &$contents];
+      echo $GLOBALS['OSCOM_Hooks']->call(pathinfo($GLOBALS['PHP_SELF'], PATHINFO_FILENAME), 'infoBox', $parameters);
+
       $this->table_row_parameters = 'class="infoBoxHeading"';
       $this->table_data_parameters = 'class="infoBoxHeading"';
       $this->heading = $this->tableBlock($heading);
@@ -41,20 +47,4 @@
       return $this->heading . $this->contents;
     }
 
-    function menuBox($heading, $contents) {
-      $this->table_data_parameters = 'class="menuBoxHeading"';
-      if (isset($heading[0]['link'])) {
-        $this->table_data_parameters .= ' onmouseover="this.style.cursor=\'hand\'" onclick="document.location.href=\'' . $heading[0]['link'] . '\'"';
-        $heading[0]['text'] = '&nbsp;<a href="' . $heading[0]['link'] . '" class="menuBoxHeadingLink">' . $heading[0]['text'] . '</a>&nbsp;';
-      } else {
-        $heading[0]['text'] = '&nbsp;' . $heading[0]['text'] . '&nbsp;';
-      }
-      $this->heading = $this->tableBlock($heading);
-
-      $this->table_data_parameters = 'class="menuBoxContent"';
-      $this->contents = (!empty($contents) ? $this->tableBlock($contents) : '');
-
-      return $this->heading . $this->contents;
-    }
   }
-?>
