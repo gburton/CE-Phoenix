fa2c8be1409c34db237d8cade5b5da716c6879f2
Matt Fletcher <github@ecartz.biz>
2020-03-07 22:02:26 +0000

    Prepare for switchable templates
        In order to switch templates, we need to be able to map to a
        separate template.  So we include cm_template.php to do the actual
        include that we want.
    
        Move all the HTML from the module file to the template file.
    
        While there update the G Burton copyrights and licenses, as per a
        separate conversation.  And extend abstract_executable_module to
        reduce boilerplate.  And update to PHP 7 standards.

--- includes/modules/content/shopping_cart/cm_sc_checkout.php
+++ includes/modules/content/shopping_cart/cm_sc_checkout.php
@@ -2,14 +2,14 @@
 /*
   $Id$
 
-  Copyright (c) 2016:
+  Copyright (c) 2020:
     Dan Cole - @Dan Cole
     James Keebaugh - @kymation
     Lambros - @Tsimi
     Rainer Schmied - @raiwa
 
   All rights reserved.
-  
+
   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
@@ -21,67 +21,47 @@
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-  class cm_sc_checkout {
-    var $code;
-    var $group;
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+  class cm_sc_checkout extends abstract_executable_module {
 
-    function __construct() {
-      $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+    const CONFIG_KEY_BASE = 'MODULE_CONTENT_SC_CHECKOUT_';
 
-      $this->title = MODULE_CONTENT_SC_CHECKOUT_TITLE;
-      $this->description = MODULE_CONTENT_SC_CHECKOUT_DESCRIPTION;
-
-      if ( defined('MODULE_CONTENT_SC_CHECKOUT_STATUS') ) {
-        $this->sort_order = MODULE_CONTENT_SC_CHECKOUT_SORT_ORDER;
-        $this->enabled = (MODULE_CONTENT_SC_CHECKOUT_STATUS == 'True');
-      }
+    public function __construct() {
+      parent::__construct(__FILE__);
     }
 
-    function execute() {
-      global $oscTemplate, $cart;
-
+    public function execute() {
       $content_width = (int)MODULE_CONTENT_SC_CHECKOUT_CONTENT_WIDTH;
 
-        if ($cart->count_contents() > 0) {
-          if (!class_exists('payment')) {
-            include('includes/classes/payment.php');
-          }
-          $payment_modules = new payment;
+      if ($GLOBALS['cart']->count_contents() > 0) {
+        $payment_modules = new payment();
 
-          $initialize_checkout_methods = $payment_modules->checkout_initialization_method();
+        $initialize_checkout_methods = $payment_modules->checkout_initialization_method();
 
-        ob_start();
-        include('includes/modules/content/' . $this->group . '/templates/tpl_' . basename(__FILE__));
-        $template = ob_get_clean();
-
-        $oscTemplate->addContent($template, $this->group);
+        $tpl_data = [ 'group' => $this->group, 'file' => __FILE__ ];
+        include 'includes/modules/content/cm_template.php';
       }
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_CONTENT_SC_CHECKOUT_STATUS' => [
+          'title' => 'Enable Shopping Cart Checkout Button',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_CONTENT_SC_CHECKOUT_CONTENT_WIDTH' => [
+          'title' => 'Content Width',
+          'value' => '12',
+          'desc' => 'What width container should the content be shown in?',
+          'set_func' => "tep_cfg_select_option(['12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'], ",
+        ],
+        'MODULE_CONTENT_SC_CHECKOUT_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '150',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_CONTENT_SC_CHECKOUT_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Shopping Cart Checkout Button', 'MODULE_CONTENT_SC_CHECKOUT_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_SC_CHECKOUT_CONTENT_WIDTH', '12', 'What width container should the content be shown in?', '6', '2', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_SC_CHECKOUT_SORT_ORDER', '150', 'Sort order of display. Lowest is displayed first.', '6', '3', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_CONTENT_SC_CHECKOUT_STATUS', 'MODULE_CONTENT_SC_CHECKOUT_CONTENT_WIDTH', 'MODULE_CONTENT_SC_CHECKOUT_SORT_ORDER');
-    }
   }
--- includes/modules/content/shopping_cart/cm_sc_stock_notice.php
+++ includes/modules/content/shopping_cart/cm_sc_stock_notice.php
@@ -2,14 +2,14 @@
 /*
   $Id$
 
-  Copyright (c) 2016:
+  Copyright (c) 2020:
     Dan Cole - @Dan Cole
     James Keebaugh - @kymation
     Lambros - @Tsimi
     Rainer Schmied - @raiwa
 
   All rights reserved.
-  
+
   Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
   1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
@@ -21,66 +21,43 @@
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-  class cm_sc_stock_notice {
-    public $code;
-    public $group;
-    public $title;
-    public $description;
-    public $sort_order;
-    public $enabled = false;
+  class cm_sc_stock_notice extends abstract_executable_module {
 
-    function __construct() {
-      $this->code = get_class($this);
-      $this->group = basename(dirname(__FILE__));
+    const CONFIG_KEY_BASE = 'MODULE_CONTENT_SC_STOCK_NOTICE_';
 
-      $this->title = MODULE_CONTENT_SC_STOCK_NOTICE_TITLE;
-      $this->description = MODULE_CONTENT_SC_STOCK_NOTICE_DESCRIPTION;
-
-      if ( defined('MODULE_CONTENT_SC_STOCK_NOTICE_STATUS') ) {
-        $this->sort_order = MODULE_CONTENT_SC_STOCK_NOTICE_SORT_ORDER;
-        $this->enabled = (MODULE_CONTENT_SC_STOCK_NOTICE_STATUS == 'True');
-      }
+    public function __construct() {
+      parent::__construct(__FILE__);
     }
 
     function execute() {
-      global $oscTemplate, $cart, $any_out_of_stock;
-
       $content_width = (int)MODULE_CONTENT_SC_STOCK_NOTICE_CONTENT_WIDTH;
 
-      if ($cart->count_contents() > 0 && $any_out_of_stock == 1) {
-        if (STOCK_ALLOW_CHECKOUT == 'true') {
-          $sc_stock_notice = '<div class="alert alert-warning" role="alert">' . MODULE_CONTENT_SC_STOCK_NOTICE_CAN_CHECKOUT . '</div>';
-        } else {
-          $sc_stock_notice = '<div class="alert alert-danger" role="alert">' . MODULE_CONTENT_SC_STOCK_NOTICE_CANT_CHECKOUT . '</div>';
-        }
-
-        ob_start();
-        include('includes/modules/content/' . $this->group . '/templates/tpl_' . basename(__FILE__));
-        $template = ob_get_clean();
-
-        $oscTemplate->addContent($template, $this->group);
+      if ($GLOBALS['cart']->count_contents() > 0 && isset($GLOBALS['any_out_of_stock']) && $GLOBALS['any_out_of_stock']) {
+        $tpl_data = [ 'group' => $this->group, 'file' => __FILE__ ];
+        include 'includes/modules/content/cm_template.php';
       }
     }
 
-    function  isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_CONTENT_SC_STOCK_NOTICE_STATUS' => [
+          'title' => 'Enable Shopping Cart Stock Notice',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_CONTENT_SC_STOCK_NOTICE_CONTENT_WIDTH' => [
+          'title' => 'Content Width',
+          'value' => '12',
+          'desc' => 'What width container should the content be shown in?',
+          'set_func' => "tep_cfg_select_option(['12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'], ",
+        ],
+        'MODULE_CONTENT_SC_STOCK_NOTICE_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '140',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_CONTENT_SC_STOCK_NOTICE_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Shopping Cart Stock Notice', 'MODULE_CONTENT_SC_STOCK_NOTICE_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'MODULE_CONTENT_SC_STOCK_NOTICE_CONTENT_WIDTH', '12', 'What width container should the content be shown in?', '6', '2', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_SC_STOCK_NOTICE_SORT_ORDER', '140', 'Sort order of display. Lowest is displayed first.', '6', '3', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_CONTENT_SC_STOCK_NOTICE_STATUS', 'MODULE_CONTENT_SC_STOCK_NOTICE_CONTENT_WIDTH', 'MODULE_CONTENT_SC_STOCK_NOTICE_SORT_ORDER');
-    }
   }
