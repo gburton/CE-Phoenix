e28f6493bc6bfe127248ea5e6dd33935a9afbd1b
gburton <oscshops@gmail.com>
2020-03-15 22:12:16 +0000

    Update TableBlock
    matches table for admin pages.

---
+++ admin/includes/classes/templates/tpl_box.php
@@ -0,0 +1,25 @@
+<div class="table-responsive">
+  <table class="table table-striped table-hover">
+    <thead class="thead-light">
+      <tr>
+        <th><?php echo $heading ?></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php echo $contents ?>
+    </tbody>
+  </table>
+</div>
+
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2020 osCommerce
+
+  Released under the GNU General Public License
+*/
+?>
