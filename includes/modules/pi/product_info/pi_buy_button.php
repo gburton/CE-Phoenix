265b933ee875a71f275716056a7403b4cd39a3ce
gburton <oscshops@gmail.com>
2020-04-05 22:32:39 +0100

    New architecture
    Plug these modules into the new architecture

--- includes/modules/pi/product_info/pi_buy_button.php
+++ includes/modules/pi/product_info/pi_buy_button.php
@@ -10,32 +10,27 @@
   Released under the GNU General Public License
 */
 
-  class pi_buy_button {
-    var $code = 'pi_buy_button';
-    var $group = 'pi_modules_c';
-    var $title;
-    var $description;
-    var $content_width;
-    var $sort_order;
-    var $api_version;
-    var $enabled = false;
+  class pi_buy_button extends abstract_module {
+
+    const CONFIG_KEY_BASE = 'PI_BUY_';
+
+    public $group = 'pi_modules_c';
+    public $content_width;
 
     function __construct() {
+      parent::__construct();
+
       $this->group = basename(dirname(__FILE__));
 
-      $this->title = PI_BUY_TITLE;
-      $this->description = PI_BUY_DESCRIPTION;
       $this->description .= '<div class="secWarning">' . MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION . '</div>';
-      $this->description .= '<div class="secInfo">' . $this->display_layout() . '</div>';
+      $this->description .= '<div class="secInfo">' . cm_pi_modular::display_layout() . '</div>';
 
-      if ( defined('PI_BUY_STATUS') ) {
+      if ( $this->enabled ) {
         $this->group = 'pi_modules_' . strtolower(PI_BUY_GROUP);
-        $this->sort_order = PI_BUY_SORT_ORDER;
         $this->content_width = (int)PI_BUY_CONTENT_WIDTH;
-        $this->enabled = (PI_BUY_STATUS == 'True');
       }
     }
-
+    
     function getOutput() {
       global $product_info;
       
@@ -48,34 +43,33 @@
       include 'includes/modules/block_template.php';
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'PI_BUY_STATUS' => [
+          'title' => 'Enable Buy Button',
+          'value' => 'True',
+          'desc' => 'Should this module be shown on the product info page?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'PI_BUY_GROUP' => [
+          'title' => 'Module Display',
+          'value' => 'C',
+          'desc' => 'Where should this module display on the product info page?',
+          'set_func' => "tep_cfg_select_option(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], ",
+        ],
+        'PI_BUY_CONTENT_WIDTH' => [
+          'title' => 'Content Width',
+          'value' => '12',
+          'desc' => 'What width container should the content be shown in?',
+          'set_func' => "tep_cfg_select_option(['12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'], ",
+        ],
+        'PI_BUY_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '320',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('PI_BUY_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Buy Button', 'PI_BUY_STATUS', 'True', 'Should this module be shown on the product info page?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Module Display', 'PI_BUY_GROUP', 'C', 'Where should this module display on the product info page?', '6', '2', 'tep_cfg_select_option(array(\'A\', \'B\', \'C\', \'D\', \'E\', \'F\', \'G\', \'H\', \'I\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'PI_BUY_CONTENT_WIDTH', '12', 'What width container should the content be shown in?', '6', '3', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'PI_BUY_SORT_ORDER', '320', 'Sort order of display. Lowest is displayed first.', '6', '4', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('PI_BUY_STATUS', 'PI_BUY_GROUP', 'PI_BUY_CONTENT_WIDTH', 'PI_BUY_SORT_ORDER');
-    }
-    
-    function display_layout() {
-      include_once(DIR_FS_CATALOG . 'includes/modules/content/product_info/cm_pi_modular.php');
-       
-      return call_user_func(array('cm_pi_modular', 'display_layout'));
-    }
-    
   }
   
--- includes/modules/pi/product_info/pi_img_disclaimer.php
+++ includes/modules/pi/product_info/pi_img_disclaimer.php
@@ -10,35 +10,29 @@
   Released under the GNU General Public License
 */
 
-  class pi_img_disclaimer {
-    var $code = 'pi_img_disclaimer';
-    var $group = 'pi_modules_b';
-    var $title;
-    var $description;
-    var $content_width;
-    var $sort_order;
-    var $api_version;
-    var $enabled = false;
+  class pi_img_disclaimer extends abstract_module {
+
+    const CONFIG_KEY_BASE = 'PI_IMG_DISCLAIMER_';
+
+    public $group = 'pi_modules_b';
+    public $content_width;
 
     function __construct() {
-      $this->code = get_class($this);
+      parent::__construct();
+
       $this->group = basename(dirname(__FILE__));
 
-      $this->title = PI_IMG_DISCLAIMER_TITLE;
-      $this->description = PI_IMG_DISCLAIMER_DESCRIPTION;
       $this->description .= '<div class="secWarning">' . MODULE_CONTENT_BOOTSTRAP_ROW_DESCRIPTION . '</div>';
-      $this->description .= '<div class="secInfo">' . $this->display_layout() . '</div>';
-      
-      if ( defined('PI_IMG_DISCLAIMER_STATUS') ) {
+      $this->description .= '<div class="secInfo">' . cm_pi_modular::display_layout() . '</div>';
+
+      if ( $this->enabled ) {
         $this->group = 'pi_modules_' . strtolower(PI_IMG_DISCLAIMER_GROUP);
-        $this->sort_order = PI_IMG_DISCLAIMER_SORT_ORDER;
         $this->content_width = (int)PI_IMG_DISCLAIMER_CONTENT_WIDTH;
-        $this->enabled = (PI_IMG_DISCLAIMER_STATUS == 'True');
       }
     }
-
+    
     function getOutput() {
-      global $oscTemplate, $product_info;
+      global $product_info;
       
       $content_width = $this->content_width;
 
@@ -46,34 +40,33 @@
       include 'includes/modules/block_template.php';
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'PI_IMG_DISCLAIMER_STATUS' => [
+          'title' => 'Enable Image Disclaimer Module',
+          'value' => 'True',
+          'desc' => 'Should this module be shown on the product info page?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'PI_IMG_DISCLAIMER_GROUP' => [
+          'title' => 'Module Display',
+          'value' => 'B',
+          'desc' => 'Where should this module display on the product info page?',
+          'set_func' => "tep_cfg_select_option(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], ",
+        ],
+        'PI_IMG_DISCLAIMER_CONTENT_WIDTH' => [
+          'title' => 'Content Width',
+          'value' => '12',
+          'desc' => 'What width container should the content be shown in?',
+          'set_func' => "tep_cfg_select_option(['12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2', '1'], ",
+        ],
+        'PI_IMG_DISCLAIMER_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '230',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('PI_IMG_DISCLAIMER_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Image Disclaimer Module', 'PI_IMG_DISCLAIMER_STATUS', 'True', 'Should this module be shown on the product info page?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Module Display', 'PI_IMG_DISCLAIMER_GROUP', 'B', 'Where should this module display on the product info page?', '6', '2', 'tep_cfg_select_option(array(\'A\', \'B\', \'C\', \'D\', \'E\', \'F\', \'G\', \'H\', \'I\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Width', 'PI_IMG_DISCLAIMER_CONTENT_WIDTH', '12', 'What width container should the content be shown in?', '6', '3', 'tep_cfg_select_option(array(\'12\', \'11\', \'10\', \'9\', \'8\', \'7\', \'6\', \'5\', \'4\', \'3\', \'2\', \'1\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'PI_IMG_DISCLAIMER_SORT_ORDER', '230', 'Sort order of display. Lowest is displayed first.', '6', '4', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('PI_IMG_DISCLAIMER_STATUS', 'PI_IMG_DISCLAIMER_GROUP',  'PI_IMG_DISCLAIMER_CONTENT_WIDTH', 'PI_IMG_DISCLAIMER_SORT_ORDER');
-    }
-    
-    function display_layout() {
-      include_once(DIR_FS_CATALOG . 'includes/modules/content/product_info/cm_pi_modular.php');
-       
-      return call_user_func(array('cm_pi_modular', 'display_layout'));
-    }
-    
   }
   
