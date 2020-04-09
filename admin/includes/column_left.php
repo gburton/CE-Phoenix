2d73d1c010414b71582f87983702a254da57cdb8
gemi254 <gemi254@hotmail.com>
2020-04-09 22:40:14 +0300

    Added administrator access restrictions

--- admin/includes/column_left.php
+++ admin/includes/column_left.php
@@ -9,7 +9,8 @@
 
   Released under the GNU General Public License
 */
-
+  global $cl_box_groups;
+  //global $files;
   if (tep_session_is_registered('admin')) {
     $cl_box_groups = array();
 
@@ -50,22 +51,53 @@
     foreach ( $cl_box_groups as &$group ) {
       usort($group['apps'], 'tep_sort_admin_boxes_links');
     }
+    
+    //$aID = $admin['id'];
+    $aID = $_SESSION['admin']['id'];
+    
+    $sql_selected = 'SELECT * FROM administrators_acl WHERE aID="'.$aID.'" group by menu_heading,page_name ';
+    $result = tep_db_query( $sql_selected );
+    $blocked_urls = array();
+    if( tep_db_num_rows( $result ) > 0 )
+    {
+        $menu_heading = '';
+        $h = 0;
+        while( $row = tep_db_fetch_array( $result ) )
+        {
+            $path = explode('&',$row['blocked_url']);
+            $menu_heading = $row['menu_heading'];
+            $blocked_urls[$menu_heading][] = $path[0];
+        }
+    }
 ?>
 
 <div class="col-md-3 col-lg-2 pt-2">
   <div id="adminAppMenu">
 
   <?php
-      foreach ($cl_box_groups as $groups) {
-        echo '<h3><a href="#">' . $groups['heading'] . '</a></h3>' .
-             '<div><ul>';
-
-        foreach ($groups['apps'] as $app) {
-          echo '<li><a href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
-        }
-
-        echo '</ul></div>';
+  
+     foreach ($cl_box_groups as $groups)
+      {
+         $title = explode('</i>',$groups['heading']);
+          $heading = $title[1];	
+          
+          if(!isset($blocked_urls[$heading])) $blocked_urls[$heading] = array();
+          
+          if( sizeof($groups['apps']) != sizeof($blocked_urls[$heading]) )
+          {
+              echo '<h3><a href="#">' . $groups['heading'] . '</a></h3>' .
+                  '<div><ul>';
+              
+              foreach ($groups['apps'] as $app)
+              {
+                  if(!(in_array($app['link'], $blocked_urls[$heading], true)) )
+                      echo '<li><a href="' . $app['link'] . '">' . $app['title'] . '</a></li>';
+              }
+              
+              echo '</ul></div>';
+          }
       }
+          
   ?>
 
   </div>
