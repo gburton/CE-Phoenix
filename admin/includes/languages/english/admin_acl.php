4ab0eae4f26b4722e718bbb91f19091dbe86ae2f
gemi254 <gemi254@hotmail.com>
2020-04-09 22:43:17 +0300

    Admin access restrictions

---
+++ admin/includes/languages/english/admin_acl.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * coupons_exclusions.php
+ * September 26, 2006
+ * author: Kristen G. Thorson
+ * ot_discount_coupon_codes version 3.0
+ *
+ *
+ * Released under the GNU General Public License
+ *
+ */
+
+define('HEADING_TITLE', 'Rules for %s');
+define('HEADING_AVAILABLE', 'Available');
+define('HEADING_SELECTED', 'Blocked');
+
+define('MESSAGE_ADMIN_ACL_SAVED', 'New access control rules saved.');
+
+define('ERROR_ADMIN_ACL_SAVE', 'Error saving new access rules.');
+
+?>
