b76f8f0584ede98a7c3e08b6dfde059c4a586b5a
gburton <oscshops@gmail.com>
2020-04-29 09:12:28 +0100

    Profile Text

--- includes/languages/english/modules/content/index/cm_i_customer_greeting.php
+++ includes/languages/english/modules/content/index/cm_i_customer_greeting.php
@@ -1,21 +1,17 @@
 <?php
 /*
-  Copyright (c) 2018, G Burton
-  All rights reserved.
+  $Id$
 
-  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
 
-  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+  Copyright (c) 2020 osCommerce
 
-  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-
-  3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  Released under the GNU General Public License
 */
 
   const MODULE_CONTENT_CUSTOMER_GREETING_TITLE       = 'Customer Greeting';
   const MODULE_CONTENT_CUSTOMER_GREETING_DESCRIPTION = 'Shows the "Customer Greeting" module on your Index page.';
 
   const MODULE_CONTENT_CUSTOMER_GREETING_PERSONAL    = '<h4 class="alert-heading">Welcome back %s!</h4> Would you like to see which <a class="alert-link" href="%s">new products</a> are available to purchase?';
-  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = '<h4 class="alert-heading">Welcome Guest!</h4> Would you like to <a class="alert-link" href="%s">log yourself in</a> or would you prefer to <a class="alert-link" href="%s">create an account</a>';
+  const MODULE_CONTENT_CUSTOMER_GREETING_GUEST       = '<h4 class="alert-heading">Welcome Guest!</h4> Would you like to <a class="alert-link" href="%s">log yourself in</a> or would you prefer to <a class="alert-link" href="%s">create a profile</a> for easy checkout';
