d5a8171c6437d10aa200937ebaf3825ba9729341
Matt Fletcher <github@ecartz.biz>
2020-05-29 23:24:20 +0000

    External templates

---
+++ templates/default/includes/ext/modules/payment/sage_pay/checkout.php
@@ -0,0 +1,99 @@
+<?php
+/*
+  $Id$
+
+  osCommerce, Open Source E-Commerce Solutions
+  http://www.oscommerce.com
+
+  Copyright (c) 2014 osCommerce
+
+  Released under the GNU General Public License
+*/
+
+  chdir('../../../../');
+  require 'includes/application_top.php';
+
+// if the customer is not logged on, redirect them to the login page
+  if (!isset($_SESSION['customer_id'])) {
+    $_SESSION['navigation']->set_snapshot(['mode' => 'SSL', 'page' => 'checkout_payment.php']);
+    tep_redirect(tep_href_link('login.php', '', 'SSL'));
+  }
+
+// if there is nothing in the customers cart, redirect them to the shopping cart page
+  if ($_SESSION['cart']->count_contents() < 1) {
+    tep_redirect(tep_href_link('shopping_cart.php'));
+  }
+
+// avoid hack attempts during the checkout procedure by checking the internal cartID
+  if (isset($_SESSION['cart']->cartID, $_SESSION['cartID'])) {
+    if ($_SESSION['cart']->cartID != $_SESSION['cartID']) {
+      tep_redirect(tep_href_link('checkout_shipping.php', '', 'SSL'));
+    }
+  }
+
+// if no shipping method has been selected, redirect the customer to the shipping method selection page
+  if (!isset($_SESSION['shipping'])) {
+    tep_redirect(tep_href_link('checkout_shipping.php', '', 'SSL'));
+  }
+
+  if (!isset($_SESSION['payment']) || (($_SESSION['payment'] != 'sage_pay_direct') && ($_SESSION['payment'] != 'sage_pay_server')) || (($_SESSION['payment'] == 'sage_pay_server') && !isset($_SESSION['sage_pay_server_nexturl']))) {
+    tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
+  }
+
+// load the selected payment module
+  $payment_modules = new payment($_SESSION['payment']);
+
+  $order = new order;
+
+  $payment_modules->update_status();
+
+  if ( ( is_array($payment_modules->modules) && (count($payment_modules->modules) > 1) && !is_object(${$_SESSION['payment']}) ) || (is_object(${$_SESSION['payment']}) && (${$_SESSION['payment']}->enabled == false)) ) {
+    tep_redirect(tep_href_link('checkout_payment.php', 'error_message=' . urlencode(ERROR_NO_PAYMENT_MODULE_SELECTED), 'SSL'));
+  }
+
+  if (is_array($payment_modules->modules)) {
+    $payment_modules->pre_confirmation_check();
+  }
+
+// load the selected shipping module
+  $shipping_modules = new shipping($shipping);
+
+  $order_total_modules = new order_total;
+  $order_total_modules->process();
+
+// Stock Check
+  $any_out_of_stock = false;
+  if (STOCK_CHECK == 'true') {
+    foreach ($order->products as $product) {
+      if (tep_check_stock($order->product['id'], $order->product['qty'])) {
+        $any_out_of_stock = true;
+      }
+    }
+    // Out of Stock
+    if ( (STOCK_ALLOW_CHECKOUT != 'true') && ($any_out_of_stock == true) ) {
+      tep_redirect(tep_href_link('shopping_cart.php'));
+    }
+  }
+
+  require "includes/languages/$language/checkout_confirmation.php";
+
+  $breadcrumb->add(NAVBAR_TITLE_1, tep_href_link('checkout_shipping.php', '', 'SSL'));
+  $breadcrumb->add(NAVBAR_TITLE_2);
+
+  if ($_SESSION['payment'] == 'sage_pay_direct') {
+    $iframe_url = tep_href_link('ext/modules/payment/sage_pay/direct_3dauth.php', '', 'SSL');
+  } else {
+    $iframe_url = $sage_pay_server_nexturl;
+  }
+
+  require 'includes/template_top.php';
+?>
+
+    <iframe src="<?php echo $iframe_url; ?>" width="100%" height="600" frameborder="0">
+      <p>Your browser does not support iframes.</p>
+    </iframe>
+
+<?php
+  require 'includes/template_bottom.php';
+  require 'includes/application_bottom.php';
+?>
