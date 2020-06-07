747338764d96097af0824dfe1bd7e9b2db5e5a0d
Matt Fletcher <github@ecartz.biz>
2020-06-07 01:52:04 +0000

    Default attributes to null on update

--- includes/actions/update_product.php
+++ includes/actions/update_product.php
@@ -13,14 +13,14 @@
   class osC_Actions_update_product {
 
     public static function execute() {
-      global $PHP_SELF, $messageStack, $goto, $parameters;
+      global $messageStack, $goto, $parameters;
 
       foreach (($_POST['products_id'] ?? []) as $i => $product_id) {
         if (in_array($product_id, ($_POST['cart_delete'] ?? []))) {
           $_SESSION['cart']->remove($product_id);
           $messageStack->add_session('product_action', sprintf(PRODUCT_REMOVED, tep_get_products_name($product_id)), 'warning');
         } else {
-          $attributes = $_POST['id'][$product_id] ?? '';
+          $attributes = $_POST['id'][$product_id] ?? null;
           $_SESSION['cart']->add_cart($product_id, $_POST['cart_quantity'][$i], $attributes, false);
         }
       }
