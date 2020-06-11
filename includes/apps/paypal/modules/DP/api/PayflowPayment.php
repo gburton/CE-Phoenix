3b5f689dc05695dade571f7923e68339fe7d3724
Matt Fletcher <github@ecartz.biz>
2020-06-11 09:51:11 +0000

    Fix missing getIpAddress error

--- includes/apps/paypal/modules/DP/api/PayflowPayment.php
+++ includes/apps/paypal/modules/DP/api/PayflowPayment.php
@@ -5,7 +5,7 @@
   osCommerce, Open Source E-Commerce Solutions
   http://www.oscommerce.com
 
-  Copyright (c) 2014 osCommerce
+  Copyright (c) 2020 osCommerce
 
   Released under the GNU General Public License
 */
@@ -17,21 +17,22 @@
       $api_url = 'https://pilot-payflowpro.paypal.com';
     }
 
-    $params = array('USER' => $OSCOM_PayPal->hasCredentials('DP', 'payflow_user') ? $OSCOM_PayPal->getCredentials('DP', 'payflow_user') : $OSCOM_PayPal->getCredentials('DP', 'payflow_vendor'),
-                    'VENDOR' => $OSCOM_PayPal->getCredentials('DP', 'payflow_vendor'),
-                    'PARTNER' => $OSCOM_PayPal->getCredentials('DP', 'payflow_partner'),
-                    'PWD' => $OSCOM_PayPal->getCredentials('DP', 'payflow_password'),
-                    'TENDER' => 'C',
-                    'TRXTYPE' => (OSCOM_APP_PAYPAL_DP_TRANSACTION_METHOD == '1') ? 'S' : 'A',
-                    'CUSTIP' => $OSCOM_PayPal->getIpAddress(),
-                    'BUTTONSOURCE' => $OSCOM_PayPal->getIdentifier());
+    $params = [
+      'USER' => $OSCOM_PayPal->getCredentials('DP', ($OSCOM_PayPal->hasCredentials('DP', 'payflow_user') ? 'payflow_user' : 'payflow_vendor')),
+      'VENDOR' => $OSCOM_PayPal->getCredentials('DP', 'payflow_vendor'),
+      'PARTNER' => $OSCOM_PayPal->getCredentials('DP', 'payflow_partner'),
+      'PWD' => $OSCOM_PayPal->getCredentials('DP', 'payflow_password'),
+      'TENDER' => 'C',
+      'TRXTYPE' => (OSCOM_APP_PAYPAL_DP_TRANSACTION_METHOD == '1') ? 'S' : 'A',
+      'CUSTIP' => tep_get_ip_address(),
+      'BUTTONSOURCE' => $OSCOM_PayPal->getIdentifier(),
+    ];
 
-    if ( is_array($extra_params) && !empty($extra_params) ) {
+    if ( !empty($extra_params) && is_array($extra_params) ) {
       $params = array_merge($params, $extra_params);
     }
 
-    $headers = array();
-
+    $headers = [];
     if ( isset($params['_headers']) ) {
       $headers = $params['_headers'];
 
@@ -44,13 +45,14 @@
       $post_string .= $key . '[' . strlen(trim($value)) . ']=' . trim($value) . '&';
     }
 
-    $post_string = substr($post_string, 0, -1);
+    $post_string = substr($post_string, 0, -strlen('&'));
 
     $response = $OSCOM_PayPal->makeApiCall($api_url, $post_string, $headers);
     parse_str($response, $response_array);
 
-    return array('res' => $response_array,
-                 'success' => ($response_array['RESULT'] == '0'),
-                 'req' => $params);
+    return [
+      'res' => $response_array,
+      'success' => ($response_array['RESULT'] == '0'),
+      'req' => $params,
+    ];
   }
-?>
