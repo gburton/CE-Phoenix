10a9d043e5793b3f158e4858f4fb92478dfe4bb3
gburton <oscshops@gmail.com>
2020-06-13 17:13:29 +0100

    Admin Dropdowns get their own stylesheet file.

--- includes/hooks/admin/siteWide/styleSheets.php
+++ includes/hooks/admin/siteWide/styleSheets.php
@@ -14,7 +14,7 @@ class hook_admin_siteWide_styleSheets {
   
   function listen_injectSiteStart() {
     $admin_css = '<!-- stylesheets hooked -->' . PHP_EOL;
-    $admin_css .= '<style>* {min-height: 0.01px;} .form-control-feedback { position: absolute; width: auto; top: 7px; right: 45px; margin-top: 0; } @media (min-width: 768px) { .w-md-25 { width: 25% !important; } .w-md-50 { width: 50% !important; } .w-md-75 { width: 75% !important; } .w-md-100 { width: 100% !important; } .w-md-auto { width: auto !important; } } @media (min-width: 768px) { #navbarAdmin > ul.navbar-nav > li.dropdown:hover > div.dropdown-menu { display: block; } }</style>' . PHP_EOL;
+    $admin_css .= '<style>* {min-height: 0.01px;} .form-control-feedback { position: absolute; width: auto; top: 7px; right: 45px; margin-top: 0; } @media (min-width: 768px) { .w-md-25 { width: 25% !important; } .w-md-50 { width: 50% !important; } .w-md-75 { width: 75% !important; } .w-md-100 { width: 100% !important; } .w-md-auto { width: auto !important; } }</style>' . PHP_EOL;
     $admin_css .= '<link href="includes/stylesheet.css" rel="stylesheet">' . PHP_EOL;
 
     return $admin_css;
