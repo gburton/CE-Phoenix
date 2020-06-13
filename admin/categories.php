dc2202bc78217f7e099c8b72ec8e1d3ee5f9f5db
Matt Fletcher <github@ecartz.biz>
2020-06-13 22:03:47 +0000

    Inject columns into duplicate product

--- admin/categories.php
+++ admin/categories.php
@@ -324,7 +324,7 @@
         tep_redirect(tep_href_link('categories.php', 'cPath=' . $cPath . '&pID=' . $products_id));
         break;
       case 'copy_to_confirm':
-        if (isset($_POST['products_id']) && isset($_POST['categories_id'])) {
+        if (isset($_POST['products_id'], $_POST['categories_id'])) {
           $products_id = tep_db_prepare_input($_POST['products_id']);
           $categories_id = tep_db_prepare_input($_POST['categories_id']);
 
@@ -339,24 +339,43 @@
               $messageStack->add_session(ERROR_CANNOT_LINK_TO_SAME_CATEGORY, 'error');
             }
           } elseif ($_POST['copy_as'] == 'duplicate') {
-            $product_query = tep_db_query("select products_quantity, products_model, products_image, products_price, products_date_available, products_weight, products_tax_class_id, manufacturers_id, products_gtin from products where products_id = '" . (int)$products_id . "'");
-            $product = tep_db_fetch_array($product_query);
+            $db = [
+              'products' => [
+                'products_quantity' => null,
+                'products_model' => null,
+                'products_image' => null,
+                'products_price' => null,
+                'products_date_added' => 'NOW()',
+                'products_date_available' => null,
+                'products_weight' => null,
+                'products_status' => 0,
+                'products_tax_class_id' => null,
+                'manufacturers_id' => null,
+                'products_gtin' => null,
+              ],
+              'products_description' => [
+                'products_id' => null,
+                'language_id' => null,
+                'products_name' => null,
+                'products_description' => null,
+                'products_url' => null,
+                'products_viewed' => 0,
+                'products_seo_title' => null,
+                'products_seo_description' => null,
+                'products_seo_keywords' => null,
+              ],
+              'products_images' => [
+                'products_id' => null,
+                'image' => null,
+                'htmlcontent' => null,
+                'sort_order' => null,
+              ],
+            ];
 
-            tep_db_query("insert into products (products_quantity, products_model,products_image, products_price, products_date_added, products_date_available, products_weight, products_status, products_tax_class_id, manufacturers_id, products_gtin) values ('" . tep_db_input($product['products_quantity']) . "', '" . tep_db_input($product['products_model']) . "', '" . tep_db_input($product['products_image']) . "', '" . tep_db_input($product['products_price']) . "',  now(), " . (empty($product['products_date_available']) ? "null" : "'" . tep_db_input($product['products_date_available']) . "'") . ", '" . tep_db_input($product['products_weight']) . "', '0', '" . (int)$product['products_tax_class_id'] . "', '" . (int)$product['manufacturers_id'] . "', '" . tep_db_input($product['products_gtin']) . "')");
-            $dup_products_id = tep_db_insert_id();
-
-            $description_query = tep_db_query("select language_id, products_name, products_description, products_url, products_seo_title, products_seo_description, products_seo_keywords from products_description where products_id = '" . (int)$products_id . "'");
-            while ($description = tep_db_fetch_array($description_query)) {
-              tep_db_query("insert into products_description (products_id, language_id, products_name, products_description, products_url, products_viewed, products_seo_title, products_seo_description, products_seo_keywords) values ('" . (int)$dup_products_id . "', '" . (int)$description['language_id'] . "', '" . tep_db_input($description['products_name']) . "', '" . tep_db_input($description['products_description']) . "', '" . tep_db_input($description['products_url']) . "', '0', '" . tep_db_input($description['products_seo_title']) . "', '" . tep_db_input($description['products_seo_description']) . "', '" . tep_db_input($description['products_seo_keywords']) . "')");
-            }
-
-            $product_images_query = tep_db_query("select image, htmlcontent, sort_order from products_images where products_id = '" . (int)$products_id . "'");
-            while ($product_images = tep_db_fetch_array($product_images_query)) {
-              tep_db_query("insert into products_images (products_id, image, htmlcontent, sort_order) values ('" . (int)$dup_products_id . "', '" . tep_db_input($product_images['image']) . "', '" . tep_db_input($product_images['htmlcontent']) . "', '" . tep_db_input($product_images['sort_order']) . "')");
-            }
-
-            tep_db_query("insert into products_to_categories (products_id, categories_id) values ('" . (int)$dup_products_id . "', '" . (int)$categories_id . "')");
-            $products_id = $dup_products_id;
+            $parameters = ['db' => &$db];
+            $OSCOM_Hooks->call('categories', 'preDuplicateCopyToConfirmAction', $parameters);
+            $products_id = tep_db_copy($db, 'products_id', (int)$products_id);
+            tep_db_query("INSERT INTO products_to_categories (products_id, categories_id) VALUES (" . (int)$products_id . ", " . (int)$categories_id . ")");
           }
         }
 
