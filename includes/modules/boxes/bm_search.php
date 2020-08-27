ca96d92ca64d178993797c0bccecfb6857ff9864
Matt Fletcher <github@ecartz.biz>
2020-08-27 20:56:23 +0000

    Eliminate notice and update to latest form

--- includes/modules/boxes/bm_search.php
+++ includes/modules/boxes/bm_search.php
@@ -10,63 +10,45 @@
   Released under the GNU General Public License
 */
 
-  class bm_search {
-    var $code = 'bm_search';
-    var $group = 'boxes';
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+  class bm_search extends abstract_block_module {
 
-    function __construct() {
-      $this->title = MODULE_BOXES_SEARCH_TITLE;
-      $this->description = MODULE_BOXES_SEARCH_DESCRIPTION;
-
-      if ( defined('MODULE_BOXES_SEARCH_STATUS') ) {
-        $this->sort_order = MODULE_BOXES_SEARCH_SORT_ORDER;
-        $this->enabled = (MODULE_BOXES_SEARCH_STATUS == 'True');
-        
-        $this->group = ((MODULE_BOXES_SEARCH_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
-      }
-    }
+    const CONFIG_KEY_BASE = 'MODULE_BOXES_SEARCH_';
 
     function execute() {
-      global $request_type, $oscTemplate;
-
-      $form_output .= tep_draw_form('quick_find', tep_href_link('advanced_search_result.php', '', $request_type, false), 'get');
+      $form_output = tep_draw_form('quick_find', tep_href_link('advanced_search_result.php', '', null, false), 'get');
         $form_output .= '<div class="input-group">';
-          $form_output .= tep_draw_input_field('keywords', '', 'required aria-required="true" autocomplete="off" list="search" placeholder="' . TEXT_SEARCH_PLACEHOLDER . '"', 'search');
+          $form_output .= tep_draw_input_field('keywords', '', 'required="required" aria-required="true" autocomplete="off" list="search" placeholder="' . TEXT_SEARCH_PLACEHOLDER . '"', 'search');
           $form_output .= '<div class="input-group-append">';
             $form_output .= '<button type="submit" class="btn btn-info btn-search"><i class="fas fa-search"></i></button>';
           $form_output .= '</div>';
         $form_output .= '</div>';
         $form_output .= tep_draw_hidden_field('search_in_description', '0') . tep_hide_session_id();
       $form_output .= '</form>';
-              
+
       $tpl_data = ['group' => $this->group, 'file' => __FILE__];
       include 'includes/modules/block_template.php';
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_BOXES_SEARCH_STATUS' => [
+          'title' => 'Enable Search Module',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_BOXES_SEARCH_CONTENT_PLACEMENT' => [
+          'title' => 'Content Placement',
+          'value' => 'Right Column',
+          'desc' => 'Should the module be loaded in the left or right column?',
+          'set_func' => "tep_cfg_select_option(['Left Column', 'Right Column'], ",
+        ],
+        'MODULE_BOXES_SEARCH_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '5025',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_BOXES_SEARCH_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Search Module', 'MODULE_BOXES_SEARCH_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_SEARCH_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column?', '6', '1', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_SEARCH_SORT_ORDER', '5025', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_BOXES_SEARCH_STATUS', 'MODULE_BOXES_SEARCH_CONTENT_PLACEMENT', 'MODULE_BOXES_SEARCH_SORT_ORDER');
-    }
   }
-?>
