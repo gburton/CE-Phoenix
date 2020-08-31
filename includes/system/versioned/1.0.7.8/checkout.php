90e67522b610d72a9bb83134d10553207ec775ad
Matt Fletcher <github@ecartz.biz>
2020-08-31 16:22:53 +0000

    Fix payment module handling
    Finished the migration of checkout_confirmation to use the modular
    checkout.
    
    Changed the payment validation to use global rather than local
    variables.  This fixes a bug when there were multiple payment methods
    that prevented checkout getting past the payment page.
    
    Adjusted the order of payment module initialization on the checkout
    payment page to match the old order.  This fixed a bug where
    disabling a module for all but specific zones was not working.
    
    Changed the functions to static so as not to bother instantiating a
    Checkout object.  Done at this time to facilitate making a call from
    one method to another.

--- includes/system/versioned/1.0.7.8/checkout.php
+++ includes/system/versioned/1.0.7.8/checkout.php
@@ -12,7 +12,7 @@
 
   class Checkout {
 
-    public function register_stages() {
+    public static function register_stages() {
       $stages = [];
 
       switch (pathinfo($GLOBALS['PHP_SELF'], PATHINFO_FILENAME)) {
@@ -29,7 +29,7 @@
       }
     }
 
-    public function require_login() {
+    public static function require_login() {
 // if the customer is not logged on, redirect to the login page
       $parameters = [
         'page' => 'checkout_payment.php',
@@ -38,14 +38,14 @@
       $GLOBALS['hooks']->register_pipeline('loginRequired', $parameters);
     }
 
-    public function guarantee_cart() {
+    public static function guarantee_cart() {
 // if there is nothing in the customer's cart, redirect to the shopping cart page
       if ($_SESSION['cart']->count_contents() <= 0) {
         tep_redirect(tep_href_link('shopping_cart.php'));
       }
     }
 
-    public function guarantee_cart_id() {
+    public static function guarantee_cart_id() {
 // register a random ID in the session to check throughout the checkout procedure
 // against alterations in the shopping cart contents
       if (isset($_SESSION['cartID']) && ($_SESSION['cartID'] != $_SESSION['cart']->cartID)) {
@@ -55,7 +55,7 @@
       $_SESSION['cartID'] = $_SESSION['cart']->cartID = $_SESSION['cart']->generate_cart_id();
     }
 
-    public function validate_sendto() {
+    public static function validate_sendto() {
       global $customer;
 
       if (isset($_SESSION['sendto'])) {
@@ -69,7 +69,7 @@
       }
     }
 
-    public function validate() {
+    public static function validate() {
 // if no shipping method has been selected, redirect the customer to the shipping method selection page
 // avoid hack attempts during the checkout procedure by checking the internal cartID
       if (!isset($_SESSION['shipping'], $_SESSION['sendto'], $_SESSION['cart']->cartID, $_SESSION['cartID'])
@@ -79,7 +79,7 @@
       }
     }
 
-    public function validate_billto() {
+    public static function validate_billto() {
       global $customer;
 
       if (isset($_SESSION['billto'])) {
@@ -99,13 +99,29 @@
       }
     }
 
-    public function validate_payment() {
+    public static function validate_payment() {
       if ( (tep_not_null(MODULE_PAYMENT_INSTALLED)) && (!isset($_SESSION['payment'])) ) {
         tep_redirect(tep_href_link('checkout_payment.php', '', 'SSL'));
       }
     }
 
-    public function skip_shipping() {
+    public static function guarantee_payment() {
+      if (isset($_POST['payment'])) {
+        $_SESSION['payment'] = $_POST['payment'];
+      } elseif (!isset($_SESSION['payment']) && !array_key_exists('payment', $_SESSION)) {
+        $_SESSION['payment'] = null;
+      }
+
+      if (isset($_POST['comments']) && tep_not_null($_POST['comments'])) {
+        $_SESSION['comments'] = tep_db_prepare_input($_POST['comments']);
+      } elseif (!array_key_exists('comments', $_SESSION)) {
+        $_SESSION['comments'] = null;
+      }
+
+      static::validate_payment();
+    }
+
+    public static function skip_shipping() {
 // if the order contains only virtual products, forward the customer to the billing page as
 // a shipping address is not needed
       if ('virtual' === $GLOBALS['order']->content_type) {
@@ -115,60 +131,61 @@
       }
     }
 
-    public function initialize_payment_modules() {
+    public static function initialize_payment_modules() {
       $GLOBALS['payment_modules'] = new payment();
     }
 
-    public function initialize_payment_module() {
+    public static function initialize_payment_module() {
       $GLOBALS['payment_modules'] = new payment($_SESSION['payment']);
     }
 
-    public function initialize_shipping_module() {
+    public static function initialize_shipping_module() {
 // load the selected shipping module
       $GLOBALS['shipping_modules'] = new shipping($_SESSION['shipping']);
     }
 
-    public function update_payment_module() {
+    public static function update_payment_module() {
       global $payment_modules;
 
       $payment_modules->update_status();
+      $payment_module = isset($_SESSION['payment'], $GLOBALS[$_SESSION['payment']]) ? $GLOBALS[$_SESSION['payment']] : null;
 
       if ( ($payment_modules->selected_module != $_SESSION['payment'])
-        || ( is_array($payment_modules->modules) && (count($payment_modules->modules) > 1) && !is_object(${$_SESSION['payment']}) )
-        || (is_object(${$_SESSION['payment']}) && (!${$_SESSION['payment']}->enabled)) )
+        || ( is_array($payment_modules->modules) && (count($payment_modules->modules) > 1) && !is_object($payment_module) )
+        || (is_object($payment_module) && (!$payment_module->enabled)) )
       {
         tep_redirect(tep_href_link('checkout_payment.php', 'error_message=' . urlencode(ERROR_NO_PAYMENT_MODULE_SELECTED), 'SSL'));
       }
     }
 
-    public function preconfirm_payment() {
+    public static function preconfirm_payment() {
       if (is_array($GLOBALS['payment_modules']->modules)) {
         $GLOBALS['payment_modules']->pre_confirmation_check();
       }
     }
 
-    public function set_order_totals() {
+    public static function set_order_totals() {
       $GLOBALS['order_total_modules'] = new order_total();
       $GLOBALS['order']->totals = $GLOBALS['order_total_modules']->process();
     }
 
-    public function prepare_payment() {
+    public static function prepare_payment() {
       $GLOBALS['payment_modules']->before_process();
     }
 
-    public function notify() {
+    public static function notify() {
       $GLOBALS['customer_notification'] = tep_notify('checkout', $GLOBALS['order']) ? 1 : 0;
     }
 
-    public function conclude_payment() {
+    public static function conclude_payment() {
       $GLOBALS['payment_modules']->after_process();
     }
 
-    public function reset_cart() {
+    public static function reset_cart() {
       $_SESSION['cart']->reset(true);
     }
 
-    public function redirect_success() {
+    public static function redirect_success() {
       tep_redirect(tep_href_link('checkout_success.php', '', 'SSL'));
     }
 
