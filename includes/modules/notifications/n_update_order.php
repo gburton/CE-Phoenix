6b72565dca9b1bd24c4aece6dc24eb5a8704488e
Matt Fletcher <github@ecartz.biz>
2020-10-08 11:53:43 +0000

    Update title to match this module

--- includes/modules/notifications/n_update_order.php
+++ includes/modules/notifications/n_update_order.php
@@ -34,7 +34,7 @@
     protected function get_parameters() {
       return [
         static::CONFIG_KEY_BASE . 'STATUS' => [
-          'title' => 'Enable Checkout Notification module',
+          'title' => 'Enable Order Update Notification module',
           'value' => 'True',
           'desc' => 'Do you want to add the module to your shop?',
           'set_func' => "tep_cfg_select_option(['True', 'False'], ",
