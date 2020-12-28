654e507c4c2451d162fa9d34c178efe1e5e5e632
Matt Fletcher <github@ecartz.biz>
2020-12-28 23:03:00 +0000

    Use product class builders
    This makes the reviews and specials boxes join What's New in using
    product builders.  In these cases, the product is randomly selected
    directly rather than fetching a product ID and then fetching that.
    
    They also switch to the new box and product card components.

--- includes/modules/boxes/bm_reviews.php
+++ includes/modules/boxes/bm_reviews.php
@@ -10,83 +10,53 @@
   Released under the GNU General Public License
 */
 
-  class bm_reviews {
-    var $code = 'bm_reviews';
-    var $group = 'boxes';
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+  class bm_reviews extends abstract_block_module {
 
-    function __construct() {
-      $this->title = MODULE_BOXES_REVIEWS_TITLE;
-      $this->description = MODULE_BOXES_REVIEWS_DESCRIPTION;
-
-      if ( defined('MODULE_BOXES_REVIEWS_STATUS') ) {
-        $this->sort_order = MODULE_BOXES_REVIEWS_SORT_ORDER;
-        $this->enabled = (MODULE_BOXES_REVIEWS_STATUS == 'True');
-
-        $this->group = ((MODULE_BOXES_REVIEWS_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
-      }
-    }
+    const CONFIG_KEY_BASE = 'MODULE_BOXES_REVIEWS_';
 
     function execute() {
-      global $currencies, $oscTemplate;
+      if ($product = random_review::build()) {
+        $card = [
+          'extra' => tep_draw_stars($product->get('reviews_rating')) . '<br>'
+                   . htmlspecialchars($product->get('reviews_text')) . '...',
+        ];
 
-      $random_select = "select r.*, p.*, pd.*, IF(s.status, s.specials_new_products_price, NULL) as specials_new_products_price, IF(s.status, s.specials_new_products_price, p.products_price) as final_price, p.products_quantity as in_stock, if(s.status, 1, 0) as is_special, substring(reviews_text, 1, 60) as reviews_text ";
-      $random_select .= "from reviews r, reviews_description rd, products p left join specials s on p.products_id = s.products_id, products_description pd ";
-      $random_select .= "where p.products_status = '1' and p.products_id = r.products_id and r.reviews_id = rd.reviews_id and rd.languages_id = '" . (int)$_SESSION['languages_id'] . "' and p.products_id = pd.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and r.reviews_status = 1 ";
-      $random_select .= "order by r.reviews_id desc limit " . (int)MODULE_BOXES_REVIEWS_MAX_RANDOM_SELECT_REVIEWS;
-      $random_product = tep_random_select($random_select);
-      
-      $box_attr = $box_title = $box_image = $box_price = $box_review_text = '';
-      $data = array();
+        $box = [
+          'parameters' => ['product_card.php', 'component'],
+          'classes' => 'is-product bm-reviews',
+          'title' => MODULE_BOXES_REVIEWS_BOX_TITLE,
+          'attributes' => $product->get('data_attributes'),
+        ];
 
-      if ($random_product) {
-        $data['data-is-special'] = (int)$random_product['is_special'];
-        $data['data-product-price'] = $currencies->display_raw($random_product['final_price'], tep_get_tax_rate($random_product['products_tax_class_id']));
-        $data['data-product-manufacturer'] = max(0, (int)$random_product['manufacturers_id']);
-
-        foreach ( $data as $key => $value ) {
-          $box_attr .= ' ' . tep_output_string_protected($key) . '="' . tep_output_string_protected($value) . '"';
-        }
-        
-        $box_title = '<a href="' . tep_href_link('product_info.php', 'products_id=' . (int)$random_product['products_id']) . '">' . $random_product['products_name'] . '</a>';
-        $box_image = '<a href="' . tep_href_link('product_info.php', 'products_id=' . $random_product['products_id']) . '">' . tep_image('images/' . $random_product['products_image'], htmlspecialchars($random_product['products_name']), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '', true, 'card-img-top') . '</a>';
-        if ($random_product['is_special'] == 1) {
-          $box_price = sprintf(IS_PRODUCT_SHOW_PRICE_SPECIAL, $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])), $currencies->display_price($random_product['specials_new_products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])));
-        } else {
-          $box_price = sprintf(IS_PRODUCT_SHOW_PRICE, $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])));
-        }
-        $box_review_text .= tep_draw_stars($random_product['reviews_rating']) . '<br>';
-        $box_review_text .= tep_output_string_protected($random_product['reviews_text']) . '...';
-        
-        $tpl_data = ['group' => $this->group, 'file' => __FILE__];
-        include('includes/modules/block_template.php');
+        $tpl_data = [
+          'group' => $this->group,
+          'file' => 'box.php',
+          'type' => 'component',
+        ];
+        include 'includes/modules/block_template.php';
       }
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_BOXES_REVIEWS_STATUS' => [
+          'title' => 'Enable Reviews Module',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_BOXES_REVIEWS_CONTENT_PLACEMENT' => [
+          'title' => 'Content Placement',
+          'value' => 'Right Column',
+          'desc' => 'Should the module be loaded in the left or right column?',
+          'set_func' => "tep_cfg_select_option(['Left Column', 'Right Column'], ",
+        ],
+        'MODULE_BOXES_REVIEWS_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '0',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_BOXES_REVIEWS_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Reviews Module', 'MODULE_BOXES_REVIEWS_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Selection of Random Reviews', 'MODULE_BOXES_REVIEWS_MAX_RANDOM_SELECT_REVIEWS', '10', 'How many records to select from to choose one random product review', '6', '2', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_REVIEWS_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column?', '6', '3', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_REVIEWS_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '4', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_BOXES_REVIEWS_STATUS', 'MODULE_BOXES_REVIEWS_MAX_RANDOM_SELECT_REVIEWS', 'MODULE_BOXES_REVIEWS_CONTENT_PLACEMENT', 'MODULE_BOXES_REVIEWS_SORT_ORDER');
-    }
   }
-  
--- includes/modules/boxes/bm_specials.php
+++ includes/modules/boxes/bm_specials.php
@@ -10,71 +10,48 @@
   Released under the GNU General Public License
 */
 
-  class bm_specials {
-    var $code = 'bm_specials';
-    var $group = 'boxes';
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+  class bm_specials extends abstract_block_module {
 
-    function __construct() {
-      $this->title = MODULE_BOXES_SPECIALS_TITLE;
-      $this->description = MODULE_BOXES_SPECIALS_DESCRIPTION;
-
-      if ( defined('MODULE_BOXES_SPECIALS_STATUS') ) {
-        $this->sort_order = MODULE_BOXES_SPECIALS_SORT_ORDER;
-        $this->enabled = (MODULE_BOXES_SPECIALS_STATUS == 'True');
-
-        $this->group = ((MODULE_BOXES_SPECIALS_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
-      }
-    }
+    const CONFIG_KEY_BASE = 'MODULE_BOXES_SPECIALS_';
 
     function execute() {
-      global $currencies, $oscTemplate;
+      if ($product = random_special::build()) {
+        $box = [
+          'parameters' => ['product_card.php', 'component'],
+          'classes' => 'is-product bm-specials',
+          'title' => sprintf(MODULE_BOXES_SPECIALS_BOX_TITLE, tep_href_link('specials.php')),
+          'attributes' => $product->get('data_attributes'),
+        ];
 
-      if ($random_product = tep_random_select("select p.*, pd.*, s.*, p.products_quantity as in_stock, s.specials_new_products_price as final_price from products p, products_description pd, specials s where p.products_status = '1' and p.products_id = s.products_id and pd.products_id = s.products_id and pd.language_id = '" . (int)$_SESSION['languages_id'] . "' and s.status = '1' order by s.specials_date_added desc limit " . MODULE_BOXES_SPECIALS_MAX_RANDOM_SELECT_SPECIALS)) {
-        $data['data-is-special'] = 1;
-        $data['data-product-price'] = $currencies->display_raw($random_product['final_price'], tep_get_tax_rate($random_product['products_tax_class_id']));
-        $data['data-product-manufacturer'] = max(0, (int)$random_product['manufacturers_id']);
-
-        // data attributes
-        $box_attr = '';
-        foreach ( $data as $key => $value ) {
-          $box_attr .= ' ' . tep_output_string_protected($key) . '="' . tep_output_string_protected($value) . '"';
-        }
-        // product title
-        $box_title = '<a href="' . tep_href_link('product_info.php', 'products_id=' . (int)$random_product['products_id']) . '">' . $random_product['products_name'] . '</a>';
-        // product image
-        $box_image = '<a href="' . tep_href_link('product_info.php', 'products_id=' . $random_product['products_id']) . '">' . tep_image('images/' . $random_product['products_image'], htmlspecialchars($random_product['products_name']), SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT, '', true, 'card-img-top') . '</a>';
-        // product price
-        $box_price = sprintf(IS_PRODUCT_SHOW_PRICE_SPECIAL, $currencies->display_price($random_product['products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])), $currencies->display_price($random_product['specials_new_products_price'], tep_get_tax_rate($random_product['products_tax_class_id'])));
-
-        $tpl_data = ['group' => $this->group, 'file' => __FILE__];
+        $tpl_data = [
+          'group' => $this->group,
+          'file' => 'box.php',
+          'type' => 'component',
+        ];
         include 'includes/modules/block_template.php';
       }
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_BOXES_SPECIALS_STATUS' => [
+          'title' => 'Enable Specials Module',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_BOXES_SPECIALS_CONTENT_PLACEMENT' => [
+          'title' => 'Content Placement',
+          'value' => 'Right Column',
+          'desc' => 'Should the module be loaded in the left or right column?',
+          'set_func' => "tep_cfg_select_option(['Left Column', 'Right Column'], ",
+        ],
+        'MODULE_BOXES_SPECIALS_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '0',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_BOXES_SPECIALS_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Specials Module', 'MODULE_BOXES_SPECIALS_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Selection of Products on Special', 'MODULE_BOXES_SPECIALS_MAX_RANDOM_SELECT_SPECIALS', '10', 'How many records to select from to choose one random product special to display', '6', '2', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_SPECIALS_CONTENT_PLACEMENT', 'Right Column', 'Should the module be loaded in the left or right column?', '6', '3', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_SPECIALS_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '4', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_BOXES_SPECIALS_STATUS', 'MODULE_BOXES_SPECIALS_MAX_RANDOM_SELECT_SPECIALS', 'MODULE_BOXES_SPECIALS_CONTENT_PLACEMENT', 'MODULE_BOXES_SPECIALS_SORT_ORDER');
-    }
   }
