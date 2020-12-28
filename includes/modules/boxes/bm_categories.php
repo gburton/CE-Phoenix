ed7a5c8cae3313f8abc851e02eabea4b36927643
Matt Fletcher <github@ecartz.biz>
2020-12-28 07:09:54 +0000

    Separate category tree display, data, and structure
    The current category tree class mixes several things.  It creates a
    tree structure to hold the category data.  It loads and exposes the
    data.  And it has specific displays of the data.
    
    This separates into multiple classes.  A generic tree structure with
    tree operations.  A category tree implementation of that, which loads
    and exposes the data.  A display class.  And a controller class for
    the display.
    
    This also simplifies accesses of the tree structure.  Both because
    some could be simpler and through the addition of a parent_of data
    structure.  This saves iterating to find a category in the tree.
    We can simply look up the location of any category.  And we can
    find its ancestors through iteration of a simple lookup.
    
    The separation of data and display requires modification of uses of
    the category tree.  In particular, the categories box.

--- includes/modules/boxes/bm_categories.php
+++ includes/modules/boxes/bm_categories.php
@@ -10,66 +10,48 @@
   Released under the GNU General Public License
 */
 
-  class bm_categories {
-    var $code = 'bm_categories';
-    var $group = 'boxes';
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+  class bm_categories extends abstract_block_module {
 
-    function __construct() {
-      $this->title = MODULE_BOXES_CATEGORIES_TITLE;
-      $this->description = MODULE_BOXES_CATEGORIES_DESCRIPTION;
-
-      if ( defined('MODULE_BOXES_CATEGORIES_STATUS') ) {
-        $this->sort_order = MODULE_BOXES_CATEGORIES_SORT_ORDER;
-        $this->enabled = (MODULE_BOXES_CATEGORIES_STATUS == 'True');
-
-        $this->group = ((MODULE_BOXES_CATEGORIES_CONTENT_PLACEMENT == 'Left Column') ? 'boxes_column_left' : 'boxes_column_right');
-      }
-    }
+    const CONFIG_KEY_BASE = 'MODULE_BOXES_CATEGORIES_';
 
     function execute() {
-      global $oscTemplate, $cPath;
-      
-      $max_nesting_level = (int)MODULE_BOXES_CATEGORIES_MAX_LEVEL;
+      $display = new tree_display(Guarantor::ensure_global('category_tree'));
 
-      $OSCOM_CategoryTree = new category_tree();
-      $OSCOM_CategoryTree->setCategoryPath($cPath, '<strong>', '</strong>');
-      $OSCOM_CategoryTree->setMaximumLevel($max_nesting_level);
-      
-      $OSCOM_CategoryTree->setParentGroupString('<div class="list-group list-group-flush">', '</div>', false);
-      $OSCOM_CategoryTree->setChildString('', '');
-      $OSCOM_CategoryTree->setSpacerString('<i class="fas fa-angle-right ml-2 mr-1 text-muted"></i>', 1);
-      
-      $category_tree = $OSCOM_CategoryTree->getTree();
-      
+      $display->setPath($GLOBALS['cPath'], '<strong>', '</strong>');
+      $display->setMaximumLevel((int)MODULE_BOXES_CATEGORIES_MAX_LEVEL);
+
+      $display->setChildString('', '');
+
       $tpl_data = ['group' => $this->group, 'file' => __FILE__];
       include 'includes/modules/block_template.php';
     }
 
-    function isEnabled() {
-      return $this->enabled;
+    protected function get_parameters() {
+      return [
+        'MODULE_BOXES_CATEGORIES_STATUS' => [
+          'title' => 'Enable Categories Module',
+          'value' => 'True',
+          'desc' => 'Do you want to add the module to your shop?',
+          'set_func' => "tep_cfg_select_option(['True', 'False'], ",
+        ],
+        'MODULE_BOXES_CATEGORIES_CONTENT_PLACEMENT' => [
+          'title' => 'Content Placement',
+          'value' => 'Left Column',
+          'desc' => 'Should the module be loaded in the left or right column?',
+          'set_func' => "tep_cfg_select_option(['Left Column', 'Right Column'], ",
+        ],
+        'MODULE_BOXES_CATEGORIES_MAX_LEVEL' => [
+          'title' => 'Maximum Level of Nesting',
+          'value' => '1',
+          'desc' => 'If you increase this number, subcategories will show in the module output.',
+        ],
+        'MODULE_BOXES_CATEGORIES_SORT_ORDER' => [
+          'title' => 'Sort Order',
+          'value' => '0',
+          'desc' => 'Sort order of display. Lowest is displayed first.',
+        ],
+      ];
     }
 
-    function check() {
-      return defined('MODULE_BOXES_CATEGORIES_STATUS');
-    }
-
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable Categories Module', 'MODULE_BOXES_CATEGORIES_STATUS', 'True', 'Do you want to add the module to your shop?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Content Placement', 'MODULE_BOXES_CATEGORIES_CONTENT_PLACEMENT', 'Left Column', 'Should the module be loaded in the left or right column?', '6', '2', 'tep_cfg_select_option(array(\'Left Column\', \'Right Column\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Maximum Level of Nesting', 'MODULE_BOXES_CATEGORIES_MAX_LEVEL', '1', 'If you increase this number, subcategories will show in the module output.', '6', '3', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_BOXES_CATEGORIES_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '4', now())");
-    }
-
-    function remove() {
-      tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
-    }
-
-    function keys() {
-      return array('MODULE_BOXES_CATEGORIES_STATUS', 'MODULE_BOXES_CATEGORIES_CONTENT_PLACEMENT', 'MODULE_BOXES_CATEGORIES_MAX_LEVEL', 'MODULE_BOXES_CATEGORIES_SORT_ORDER');
-    }
   }
 
---
+++ includes/system/versioned/1.0.7.other/1.0.7.12/tree.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * osCommerce Online Merchant
+ *
+ * @copyright Copyright (c) 2020 osCommerce; http://www.oscommerce.com
+ * @license GNU General Public License; http://www.oscommerce.com/gpllicense.txt
+ */
+
+  abstract class Tree {
+
+    protected static $_parents = [];
+
+    protected $_data = [];
+    protected $root_id = 0;
+
+    public function exists($id) {
+      return isset(static::$_parents[$id]);
+    }
+
+    public function get_children($id) {
+      return array_keys($this->_data[$id] ?? []);
+    }
+
+    public function get_descendants($id, &$results = []) {
+      foreach (array_keys($this->_data[$id]) as $child_id) {
+        $results[] = $child_id;
+        $this->get_descendants($child_id, $results);
+      }
+
+      return $results;
+    }
+
+    public function get_ancestors($id, &$results = []) {
+      while (($id = $this->get_parent_id($id)) && ($id != $this->root_id)) {
+        $results[] = $id;
+      }
+
+      return $results;
+    }
+
+    public function find_path($id, $glue = '_') {
+      $nodes = array_reverse($this->get_ancestors($id));
+      $nodes[] = $id;
+
+      return implode($glue, $nodes);
+    }
+
+    public function parse_path($path) {
+      return array_unique(array_map('intval', explode('_', $path)), SORT_NUMERIC);
+    }
+
+/**
+ * Return node information
+ *
+ * @param int $id Return information for this node ID
+ * @param string $key The key information to return
+ * @return mixed
+ */
+    public function get($id, $key = null) {
+      if ( isset(static::$_parents[$id]) ) {
+        $data = $this->_data[static::$_parents[$id]][$id];
+
+        $data['id'] = $id;
+        $data['parent_id'] = static::$_parents[$id];
+
+        return ( is_null($key) ? $data : $data[$key] );
+      }
+
+      return null;
+    }
+
+/**
+ * Return the parent ID of a node
+ *
+ * @param int $id Return the parent ID of this ID
+ * @return int
+ */
+    public function get_parent_id($id) {
+      return static::$_parents[$id] ?? null;
+    }
+
+    public function get_root_id() {
+      return $this->root_id;
+    }
+
+    public function set_root_id($root_id) {
+      $this->root_id = $root_id;
+    }
+
+/**
+ * Return a formated string representation of the category structure relationship data
+ *
+ * @access public
+ * @return string
+ */
+    public function getTree() {
+      $display = new tree_display($this);
+
+      return $display->_buildBranch($this->root_id);
+    }
+
+/**
+ * Magic function; return a formated string representation of the category structure relationship data
+ *
+ * This is used when echoing the class object, eg:
+ *
+ * echo $tree;
+ *
+ * @access public
+ * @return string
+ */
+    public function __toString() {
+      return $this->getTree();
+    }
+
+  }
