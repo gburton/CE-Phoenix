d0c8a1a2eeeeb453d7c5ee96b66ca1338be8269b
Matt Fletcher <github@ecartz.biz>
2021-01-27 10:56:00 +0000

    Only allow modules that are available
    This removes the module GET parameter if it doesn't match any of
    the installed modules.
    
    https://github.com/gburton/CE-Phoenix/issues/1039

--- admin/action_recorder.php
+++ admin/action_recorder.php
@@ -122,8 +122,12 @@
             <?php
             $filter = [];
 
-            if (isset($_GET['module']) && in_array($_GET['module'], $modules)) {
-              $filter[] = " module = '" . tep_db_input($_GET['module']) . "' ";
+            if (isset($_GET['module'])) {
+              if (in_array($_GET['module'], $modules)) {
+                $filter[] = " module = '" . tep_db_input($_GET['module']) . "' ";
+              } else {
+                unset($_GET['module']);
+              }
             }
 
             if (!empty($_GET['search'])) {
